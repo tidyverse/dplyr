77aae20b30b368ce934ea701bb1bea9439796900
hadley <h.wickham@gmail.com>
2012-11-03 09:52:58 -0500

    Data.table hack, summarise fix and timing info

--- bench/summarise-by.r
+++ bench/summarise-by.r
@@ -4,6 +4,15 @@
   baseball_s <- sqlite_source("inst/db/baseball.sqlite3", "baseball")
 }
 
+ddply(baseball, "id", summarise, g = mean(g))
+#:    user  system elapsed
+#:   0.499   0.003   0.503
 summarise_by(baseball_s, group("id"), list(g = quote(mean(g))))
+#:    user  system elapsed
+#:   0.038   0.002   0.039
 summarise_by(baseball_dt, group("id"), list(g = quote(mean(g))))
+#:    user  system elapsed
+#:   0.008   0.000   0.007
 summarise_by(baseball_df, group("id"), list(g = quote(mean(g))))
+#:    user  system elapsed
+#:   0.028   0.000   0.029
