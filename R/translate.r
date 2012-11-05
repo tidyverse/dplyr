5e330fdf377e89459d2336ebd86483b1a9c9a0cd
hadley <h.wickham@gmail.com>
2012-11-05 17:00:40 -0600

    Move partial eval to own file

--- R/translate.r
+++ R/translate.r
@@ -16,27 +16,3 @@ translate <- function(source, call, env = parent.frame()) {
   call <- partial_eval(source, call, env)
   eval(call, translator, emptyenv())
 }
-
-partial_eval <- function(source, call, env = parent.frame()) {
-  if (is.atomic(call)) return(call)
-
-  if (is.symbol(call)) {
-    # Symbols must be resolveable either locally or remotely
-
-    name <- as.character(call)
-    if (name %in% source_vars(source)) {
-      substitute(remote_var(var), list(var = as.character(call)))
-    } else if (exists(name, env)) {
-      substitute(local_value(x), list(x = get(name, env)))
-    } else {
-      stop(name, " not defined locally or in data source")
-    }
-  } else if (is.call(call)) {
-    # Process call arguments recursively
-
-    call[-1] <- lapply(call[-1], partial_eval, source = source, env = env)
-    call
-  } else {
-    stop("Unknown input type: ", class(call))
-  }
-}
