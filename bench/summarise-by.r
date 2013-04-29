47a240771b6c499c2c7db19f752bd74e4aa10e61
hadley <h.wickham@gmail.com>
2013-04-29 16:43:37 -0500

    Fix var name

--- bench/summarise-by.r
+++ bench/summarise-by.r
@@ -6,16 +6,16 @@
   baseball_s <- sqlite_source("inst/db/baseball.sqlite3", "baseball")
 }
 
-ddply(baseball, "id", summarise, g = mean(g))
+ddply(baseball, "id", summarise, avg_games = mean(g))
 #:    user  system elapsed
 #:   0.471   0.002   0.473
-summarise_by(baseball_s, "id", g = mean(g))
+summarise_by(baseball_s, "id", avg_games = mean(g))
 #:    user  system elapsed
 #:   0.037   0.002   0.039
-summarise_by(baseball_dt, "id", g = mean(g))
+summarise_by(baseball_dt, "id", avg_games = mean(g))
 #:    user  system elapsed
 #:   0.007   0.000   0.007
-summarise_by(baseball_df, "id", g = mean(g))
+summarise_by(baseball_df, "id", avg_games = mean(g))
 #:    user  system elapsed
 #:   0.028   0.000   0.028
 aggregate(g ~ id, baseball, mean)
