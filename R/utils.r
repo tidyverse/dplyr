a1363ea0f6d87e1666f0df38e6054a7affb7928b
hadley <h.wickham@gmail.com>
2013-04-29 20:01:20 -0500

    Use R names by default. And escape them in sql

--- R/utils.r
+++ R/utils.r
@@ -2,6 +2,21 @@ dots <- function(...) {
   eval(substitute(alist(...)))
 }
 
+named_dots <- function(...) {
+  args <- dots(...)
+
+  nms <- names(args) %||% rep("", length(args))
+  missing <- nms == ""
+  if (all(!missing)) return(args)
+
+  deparse2 <- function(x) paste(deparse(x, 500L), collapse = "")
+  defaults <- vapply(args[missing], deparse2, character(1), USE.NAMES = FALSE)
+
+  names(args)[missing] <- defaults
+  args
+}
+
+
 names2 <- function(x) {
   names(x) %||% rep("", length(x))
 }
