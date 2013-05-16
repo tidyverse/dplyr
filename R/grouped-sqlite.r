9e8e1dc8fd3038dacb6b111f1f220588283990e6
hadley <h.wickham@gmail.com>
2013-05-16 11:30:58 -0500

    Document sqlite grouping

--- R/grouped-sqlite.r
+++ R/grouped-sqlite.r
@@ -1,16 +1,39 @@
-#' @S3method group_by source_sqlite
-group_by.source_sqlite <- function(x, ...) {
-  vars <- named_dots(...)
-  group_by <- partial_eval(x, vars, parent.frame())
-
-  grouped_sqlite(x, vars, group_by)
-}
-
+#' A grouped sqlite database.
+#'
+#' Typically you will create a grouped data table is to call the \code{group_by}
+#' method on a sqlite data source: this will take care of capturing
+#' the unevalated expressions for you.
+#'
+#' @param source a data source or data frame.
+#' @param vars a list of quoted variables.
+#' @param group_by \code{vars} partially evaluated in the correct environment
+#' @export
+#' @examples
+#' db_path <- system.file("db", "baseball.sqlite3", package = "dplyr")
+#' baseball_s <- source_sqlite(db_path, "baseball")
+#'
+#' by_year_lg <- group_by(baseball_s, year, lg)
+#' summarise(by_year_lg, players = count(), avg_g = mean(g))
+#'
+#' by_team <- group_by(baseball_s, team)
+#' summarise(by_team, players = count())
 grouped_sqlite <- function(source, vars, group_by) {
   source$vars <- vars
   source$group_by <- group_by
 
   structure(source, class = c("grouped_sqlite", "source_sqlite", "source"))
+}
+
+#' @export
+#' @rdname grouped_sqlite
+#' @method group_by source_sqlite
+#' @param x an existing sqlite data source
+#' @param ... expressions describing how to group data
+group_by.source_sqlite <- function(x, ...) {
+  vars <- named_dots(...)
+  group_by <- partial_eval(x, vars, parent.frame())
+
+  grouped_sqlite(x, vars, group_by)
 }
 
 #' @S3method print grouped_sqlite
