dcca52f463c9a46f1af234ef1631a6bac9d7e75e
hadley <h.wickham@gmail.com>
2013-05-16 09:54:48 -0500

    Document and export ungroup

--- R/group-by.r
+++ R/group-by.r
@@ -1,5 +1,7 @@
 #' Group a data source by one or more variables.
 #'
+#' See \code{\link{ungroup}} for the opposite operation.
+#'
 #' @param x a data source
 #' @param ... variables to group by. All data sources accept variable names,
 #'   some will also accept functons of variables.
@@ -8,6 +10,12 @@ group_by <- function(x, ...) {
   UseMethod("group_by")
 }
 
+#' Ungroup a grouped data source
+#'
+#' The inverse of \code{\link{group_by}}
+#'
+#' @param x a grouped data source
+#' @export
 ungroup <- function(x) {
   UseMethod("ungroup")
 }
