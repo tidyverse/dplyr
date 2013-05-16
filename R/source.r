2ddb88a6b3b80968bdf4031a6e287c1c0ccc2aec
hadley <h.wickham@gmail.com>
2013-05-16 09:53:05 -0500

    Document as.source

--- R/source.r
+++ R/source.r
@@ -4,7 +4,15 @@ new_source <- function(...) {
 
 source_vars <- function(x) UseMethod("source_vars")
 
+#' Convert an existing object to a data source.
+#'
+#' @param x an object to convert
+#' @param ... other arguments passed to methods
+#' @export
+#' @examples
+#' as.source(mtcars)
 as.source <- function(x, ...) UseMethod("as.source")
+
 #' @S3method as.source source
 as.source.source <- function(x, ...) x
 
