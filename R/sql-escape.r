0c10ba795938196700ad608824c68863a00db998
hadley <h.wickham@gmail.com>
2013-09-04 07:21:02 -0500

    Add support for escaping identifiers. Ensure escape always returns an sql string

--- R/sql-escape.r
+++ R/sql-escape.r
@@ -1,20 +1,31 @@
 #' SQL escaping.
 #'
-#' These two functions are critical when writing functions that translate R
+#' These functions are critical when writing functions that translate R
 #' functions to sql functions. Typically a conversion function should escape
 #' all it's inputs and return an sql object.
 #'
 #' @param ... Character vectors that will be combined into a single SQL
-#'   expression
+#'   expression. \code{ident} flags its input as a identifier, to ensure that
+#'   it gets the correct quoting.
 #' @param x An object to escape. Existing sql vectors will be left as is,
 #'   character vectors are escaped with single quotes, numeric vectors have
-#'   trailing .0 added if they're whole numbers.
+#'   trailing \code{.0} added if they're whole numbers. 
 #' @keywords internal
 #' @export
 #' @examples
+#' # Doubles vs. integers
 #' escape(1:5)
 #' escape(c(1, 5.4))
+#' 
+#' # String vs known sql vs. sql identifier 
+#' escape("X")
 #' escape(sql("X"))
+#' escape(ident("X"))
+#' 
+#' # Escaping is idempotent
+#' escape("X")
+#' escape(escape("X"))
+#' escape(escape(escape("X")))
 #'
 #' # You can use these functions to make your own R wrappers for SQL functions.
 #' # The following is a more sophisticated version of round that have more
@@ -37,6 +48,12 @@ sql <- function(...) {
   structure(paste0(...), class = "sql")
 }
 
+#' @export 
+#' @rdname sql
+ident <- function(...) {
+  structure(c(...), class = c("ident", "sql"))
+}
+
 #' @rdname sql
 #' @export
 is.sql <- function(x) inherits(x, "sql")
@@ -48,27 +65,37 @@ print.sql <- function(x, ...) cat("<SQL> ", x, "\n", sep = "")
 #' @export
 escape <- function(x) UseMethod("escape")
 
+#' @S3method escape ident
+escape.ident <- function(x) {
+  sql_vector(paste0('"', x, '"'))
+}
+
 #' @S3method escape character
 escape.character <- function(x) {
   sql_vector(paste0("'", x, "'"))
 }
+
 #' @S3method escape double
 escape.double <- function(x) {
   x <- ifelse(is.wholenumber(x), sprintf("%.1f", x), as.character(x))
   sql_vector(x)
 }
+
 #' @S3method escape integer
 escape.integer <- function(x) {
   sql_vector(x)
 }
+
 #' @S3method escape NULL
-escape.NULL <- function(x) "NULL"
+escape.NULL <- function(x) sql("NULL")
+
 #' @S3method escape sql
 escape.sql <- function(x) x
+
 #' @S3method escape list
-escape.list <- function(x) vapply(x, escape, character(1))
+escape.list <- function(x) sql(vapply(x, escape, character(1)))
 
 sql_vector <- function(x) {
-  if (length(x) == 1) return(x)
+  if (length(x) == 1) return(sql(x))
   sql("(", paste(x, collapse = ", "), ")")
 }
