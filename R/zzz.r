ae8e54998b1644bb944bd05d73bf0b90a09fc7ed
hadley <h.wickham@gmail.com>
2013-09-05 07:17:06 -0500

    Add startup message

--- R/zzz.r
+++ R/zzz.r
@@ -1,4 +1,11 @@
 .onAttach <- function(libname, pkgname) {
+  packageStartupMessage("The dplyr API is currently rapidly evolving. ",
+    "I recommend that you don't rely on this for production, but ",
+    "feel free to explore. If you encounter a clear bug, please file a ",
+    "minimal reproducible example at https://github.com/hadley/dplyr/issues.",
+    "For questions and other discussion, please use ", 
+    "https://groups.google.com/group/manipulatr")
+
   op <- options()
   op.dplyr <- list(
     dplyr.show_sql = FALSE
