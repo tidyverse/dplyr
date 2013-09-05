b1b19ce61218fd7e46fb12cbbfcea10f0b47e31a
hadley <h.wickham@gmail.com>
2013-09-05 11:07:30 -0500

    Add collapse method

--- R/compute-collect.r
+++ R/compute-collect.r
@@ -2,7 +2,9 @@
 #' 
 #' \code{compute} forces computation of lazy tbls, leaving data in the remote
 #' source. \code{collect} also forces computation, but will bring data back into
-#' an R data.frame (stored in a \code{\link{tbl_df}}).  
+#' an R data.frame (stored in a \code{\link{tbl_df}}). \code{collapse} doesn't
+#' force computation, but collapses a complex tbl into a form that additional
+#' restrictions can be placed on.
 #' 
 #' @param x a data tbl
 #' @param ... other arguments passed on to methods
@@ -11,7 +13,8 @@
 #' @export
 #' @examples
 #' batting <- tbl(lahman(), "Batting")
-#' remote <- select(filter(tbl(l, "Batting"), year > 2010 && stint == 1), playerID:H)
+#' remote <- select(filter(batting, year > 2010 && stint == 1), playerID:H)
+#' remote2 <- collapse(remote)
 #' cached <- compute(remote)
 #' local  <- collect(remote)
 compute <- function(x, name = random_table_name(), ...) {
@@ -24,7 +27,18 @@ collect <- function(x, ...) {
   UseMethod("collect")
 }
 
+#' @export
+#' @rdname compute
+collapse <- function(x, ...) {
+  UseMethod("collapse")
+}
+
 # Methods ---------------------------------------------------------------------
+
+#' @S3method collapse tbl_sqlite
+collapse.tbl_sqlite <- function(x, ...) {
+  tbl(x$src, qry_select(x)$sql)
+}
 
 #' @S3method compute tbl_sqlite
 compute.tbl_sqlite <- function(x, name = random_table_name(), ...) {
@@ -37,6 +51,11 @@ collect.tbl_sqlite <- function(x, ...) {
   tbl_df(qry_select(x)$fetch_df())
 }
 
+
+#' @S3method collapse tbl_df
+collapse.tbl_df <- function(x, ...) x
+#' @S3method collapse tbl_dt
+collapse.tbl_dt <- function(x, ...) x
 #' @S3method compute tbl_df
 compute.tbl_df <- function(x, ...) x
 #' @S3method compute tbl_dt
