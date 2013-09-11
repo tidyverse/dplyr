9982ad8454bd444288dddb7b4e0ca51c3295bd25
hadley <h.wickham@gmail.com>
2013-09-11 08:38:40 -0500

    Better tbl_sqlite internals. Always use lists of language objects

--- R/utils.r
+++ R/utils.r
@@ -16,6 +16,43 @@ named_dots <- function(...) {
   args
 }
 
+is.lang <- function(x) {
+  is.name(x) || is.atomic(x) || is.call(x)
+}
+is.lang.list <- function(x) {
+  if (is.null(x)) return(TRUE)
+  
+  is.list(x) && all_apply(x, is.lang)
+}
+on_failure(is.lang.list) <- function(call, env) {
+  paste0(call$x, " is not a list containing only names, calls and atomic vectors")
+}
+
+only_has_names <- function(x, nms) {
+  all(names(x) %in% nms)
+}
+on_failure(all_names) <- function(call, env) {
+  x_nms <- names(eval(call$x, env))
+  nms <- eval(call$nms, env)
+  extra <- setdiff(x_nms, nms)
+  
+  paste0(call$x, " has named components: ", paste0(extra, collapse = ", "), ".", 
+    "Should only have names: ", paste0(nms, collapse = ","))
+}
+
+all_apply <- function(xs, f) {
+  for (x in xs) {
+    if (!f(x)) return(FALSE)
+  }
+  TRUE
+}
+any_apply <- function(xs, f) {
+  for (x in xs) {
+    if (f(x)) return(TRUE)
+  }
+  FALSE
+}
+
 drop_last <- function(x) {
   if (length(x) <= 1L) return(NULL)
   x[-length(x)]
