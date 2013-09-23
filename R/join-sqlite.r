b03871a7f572b9a7faf9ca8f5470e576d5202240
hadley <h.wickham@gmail.com>
2013-09-23 16:21:11 -0500

    One function per join type: inner_join, left_join, semi_join, anti_join

--- R/join-sqlite.r
+++ R/join-sqlite.r
@@ -1,61 +1,6 @@
 #' Join SQLite tbls.
 #' 
-#' @inheritParams join
-#' @param copy If \code{x} and \code{y} are not from the same data source,
-#'   and \code{copy} is \code{TRUE}, then \code{y} will be copied into a 
-#'   temporary table in same database as \code{x}. \code{join} will automatically 
-#'   run \code{ANALYSE} on the created table in the hope that this will make
-#'   you queries as efficient as possible by giving more data to the query
-#'   planner.
-#'   
-#'   This allows you to join tables across srcs, but it's potentially expensive 
-#'   operation so you must opt into it.
-#' @param auto_index if \code{copy} is \code{TRUE}, automatically create 
-#'   indices for the variables in \code{by}. This may speed up the join if
-#'   there are matching indices in \code{x}.
-#' @method join tbl_sqlite
-#' @export
-#' @examples
-#' batting <- tbl(src_lahman(), "Batting")
-#' team_info <- select(tbl(src_lahman(), "Teams"), yearID, lgID, teamID, G, R:H)
-#'
-#' # Combine player and whole team statistics
-#' first_stint <- select(filter(batting, stint == 1), playerID:H)
-#' both <- join(first_stint, team_info, type = "inner", by = c("yearID", "teamID", "lgID"))
-#' head(both)
-#' explain_tbl(both)
-#' 
-#' # Join with a local data frame
-#' grid <- expand.grid(
-#'   teamID = c("WAS", "ATL", "PHI", "NYA"), 
-#'   yearID = 2010:2012)
-#' top4a <- join(batting, grid, copy = TRUE, type = "inner")
-#' explain_tbl(top4a)
-#' 
-#' # Indices don't really help here because there's no matching index on
-#' # batting
-#' top4b <- join(batting, grid, copy = TRUE, type = "inner", auto_index = TRUE)
-#' explain_tbl(top4b)
-join.tbl_sqlite <- function(x, y, by = NULL, type = "left", copy = FALSE, 
-  auto_index = FALSE, ...) {
-  type <- match.arg(type, c("left", "right", "inner", "full"))
-  by <- by %||% common_by(x, y)
-  
-  y <- auto_copy(x, y, copy, indexes = if (auto_index) list(by))
-  
-  join <- switch(type, left = sql("LEFT"), inner = sql("INNER"),
-    right = stop("Right join not supported", call. = FALSE),
-    full = stop("Full join not supported", call. = FALSE))
-  
-  from <- build_sql(from(x), "\n\n", 
-    join, " JOIN \n\n" , 
-    from(y), "\n\n",
-    "USING ", lapply(by, ident))
-  
-  update(tbl(x$src, from), group_by = groups(x))
-}
-
-#' Semi-join for SQLite tbls.
+#' @section Implementation notes:
 #' 
 #' Semi-joins are implemented using \code{WHERE EXISTS}, and anti-joins with
 #' \code{WHERE NOT EXISTS}. Support for semi-joins is somewhat partial: you 
@@ -63,11 +8,50 @@ join.tbl_sqlite <- function(x, y, by = NULL, type = "left", copy = FALSE,
 #' compared with \code{=} not with more general operators.
 #' 
 #' @inheritParams join
-#' @inheritParams join.tbl_sqlite
-#' @param anti If \code{TRUE}, performs an anti join instead of a semi join.
-#'   Anti joins use \code{WHERE NOT EXISTS} rather than \code{WHERE EXISTS}
+#' @param x,y tbls to join
+#' @param by a character vector of variables to join by.  If \code{NULL}, the
+#'   default, \code{join} will do a natural join, using all variables with 
+#'   common names across the two tables. A message lists the variables so
+#'   that you can check they're right - to suppress the message, supply
+#'   a character vector.
+#' @param copy If \code{x} and \code{y} are not from the same data source,
+#'   and \code{copy} is \code{TRUE}, then \code{y} will be copied into a 
+#'   temporary table in same database as \code{x}. \code{join} will automatically 
+#'   run \code{ANALYZE} on the created table in the hope that this will make
+#'   you queries as efficient as possible by giving more data to the query
+#'   planner.
+#'   
+#'   This allows you to join tables across srcs, but it's potentially expensive 
+#'   operation so you must opt into it.
+#' @param auto_index if \code{copy} is \code{TRUE}, automatically create 
+#'   indices for the variables in \code{by}. This may speed up the join if
+#'   there are matching indexes in \code{x}.
 #' @export
 #' @examples
+#' # Left joins ----------------------------------------------------------------
+#' batting <- tbl(src_lahman(), "Batting")
+#' team_info <- select(tbl(src_lahman(), "Teams"), yearID, lgID, teamID, G, R:H)
+#'
+#' # Combine player and whole team statistics
+#' first_stint <- select(filter(batting, stint == 1), playerID:H)
+#' both <- left_join(first_stint, team_info, type = "inner", by = c("yearID", "teamID", "lgID"))
+#' head(both)
+#' explain_tbl(both)
+#' 
+#' # Join with a local data frame
+#' grid <- expand.grid(
+#'   teamID = c("WAS", "ATL", "PHI", "NYA"), 
+#'   yearID = 2010:2012)
+#' top4a <- left_join(batting, grid, copy = TRUE, type = "inner")
+#' explain_tbl(top4a)
+#' 
+#' # Indices don't really help here because there's no matching index on
+#' # batting
+#' top4b <- left_join(batting, grid, copy = TRUE, type = "inner", auto_index = TRUE)
+#' explain_tbl(top4b)
+#' 
+#' # Semi-joins ----------------------------------------------------------------
+#' 
 #' people <- tbl(src_lahman(), "Master")
 #' 
 #' # All people in half of fame
@@ -85,7 +69,70 @@ join.tbl_sqlite <- function(x, y, by = NULL, type = "left", copy = FALSE,
 #' famous_manager <- semi_join(semi_join(people, manager), hof)
 #' famous_manager
 #' explain_tbl(famous_manager)
-semi_join.tbl_sqlite <- function(x, y, by = NULL, anti = FALSE, copy = FALSE, 
+#' 
+#' # Anti-joins ----------------------------------------------------------------
+#' 
+#' # batters without person covariates
+#' anti_join(batting, people)
+#' 
+#' @name join.tbl_sqlite
+NULL
+
+#' @rdname join.tbl_sqlite
+#' @method inner_join tbl_sqlite
+#' @export
+inner_join.tbl_sqlite <- function(x, y, by = NULL, copy = FALSE, 
+                                  auto_index = FALSE, ...) {
+  join_sqlite(x, y, "inner", by = by, copy = copy, auto_index = auto_index, ...)
+}
+
+#' @rdname join.tbl_sqlite
+#' @method left_join tbl_sqlite
+#' @export
+left_join.tbl_sqlite <- function(x, y, by = NULL, copy = FALSE, 
+                                 auto_index = FALSE, ...) {
+  join_sqlite(x, y, "left", by = by, copy = copy, auto_index = auto_index, ...)
+}
+
+
+#' @rdname join.tbl_sqlite
+#' @method semi_join tbl_sqlite
+#' @export
+semi_join.tbl_sqlite <- function(x, y, by = NULL, copy = FALSE, 
+                                 auto_index = FALSE, ...) {
+  semi_join_sqlite(x, y, FALSE, by = by, copy = copy, auto_index = auto_index, 
+    ...)  
+}
+  
+#' @rdname join.tbl_sqlite
+#' @method anti_join tbl_sqlite
+#' @export
+anti_join.tbl_sqlite <- function(x, y, by = NULL, copy = FALSE, 
+                                 auto_index = FALSE, ...) {
+  semi_join_sqlite(x, y, TRUE, by = by, copy = copy, auto_index = auto_index, 
+    ...)  
+}
+
+join_sqlite <- function(x, y, type, by = NULL, copy = FALSE, auto_index = FALSE, 
+  ...) {
+  type <- match.arg(type, c("left", "right", "inner", "full"))
+  by <- by %||% common_by(x, y)
+  
+  y <- auto_copy(x, y, copy, indexes = if (auto_index) list(by))
+  
+  join <- switch(type, left = sql("LEFT"), inner = sql("INNER"),
+    right = stop("Right join not supported", call. = FALSE),
+    full = stop("Full join not supported", call. = FALSE))
+  
+  from <- build_sql(from(x), "\n\n", 
+    join, " JOIN \n\n" , 
+    from(y), "\n\n",
+    "USING ", lapply(by, ident))
+  
+  update(tbl(x$src, from), group_by = groups(x))
+}
+
+semi_join_sqlite <- function(x, y, anti = FALSE, by = NULL, copy = FALSE, 
   auto_index = FALSE, ...) {
   by <- by %||% common_by(x, y)
   y <- auto_copy(x, y, copy, indexes = if (auto_index) list(by))
@@ -94,20 +141,20 @@ semi_join.tbl_sqlite <- function(x, y, by = NULL, anti = FALSE, copy = FALSE,
   join <- sql(paste0('"_LEFT".', by_escaped, ' = "_RIGHT".', by_escaped, 
     collapse = ' AND '))
   
-  from <- build_sql('(',
+  from <- build_sql(
     'SELECT * FROM ', from(x), ' as "_LEFT"\n\n', 
     'WHERE ', if (anti) sql('NOT '), 'EXISTS (\n',
     '  SELECT 1 FROM ', from(y), ' AS "_RIGHT"\n',
-    '  WHERE ', join, ')',
-    ')')
+    '  WHERE ', join, ')'
+  )
   
   update(tbl(x$src, from), group_by = groups(x))
 }
-
+  
 from <- function(x) {
   if (is_table(x)) {
-    x$table
+    x$from
   } else {
     build_sql("(", x$query$sql, ")")
-  }  
+  } 
 }
--- R/join.r
+++ R/join.r
@@ -1,4 +1,33 @@
-#' Join two tbls together
+#' Join two tbls together.
+#' 
+#' These are generic functions that dispatch to individual tbl methods - see the 
+#' method documentation for details of individual data sources. \code{x} and 
+#' \code{y} should usually be from the same data source, but if \code{copy} is 
+#' \code{TRUE}, \code{y} will automatically be copied to the same source as 
+#' \code{x} - this may be an expensive operation.
+#' 
+#' @section Join types:
+#' 
+#' Currently dplyr supports four join types:
+#' 
+#' \itemize{
+#'    \code{inner_join}: return all rows from x where there are matching values
+#'    in y, and all columns from x and y
+#'    
+#'    \code{left_join}: return all rows from x, and all columns from x and y
+#'    
+#'    \code{semi_join}: return all rows from x where there are matching values 
+#'    in y, keeping just columns from x.
+#'    
+#'    \code{anti_join}: return all rows from x where there are not matching 
+#'    values in y, keeping just columns from x
+#' }
+#'
+#' A semi join differs from an inner join (with just the \code{by} variables)
+#' because an inner join will return one row of \code{x} for each matching row
+#' of \code{y}, where a semi join will never duplicated rows in \code{x}.
+#'
+#' @section Grouping:
 #' 
 #' Groups are ignored for the purpose of joining, but the result preserves
 #' the grouping of \code{x}.
@@ -8,40 +37,37 @@
 #'   default, \code{join} will do a natural join, using all variables with 
 #'   common names across the two tables. A message lists the variables so
 #'   that you can check they're right. 
-#' @param type a string giving the join type. Possible values are left (the 
-#'   default), right, full and inner. Not all types will be supported by all
-#'   tbls.
 #' @param copy If \code{x} and \code{y} are not from the same data source,
 #'   and \code{copy} is \code{TRUE}, then \code{y} will be copied into the 
 #'   same src as \code{x}.  This allows you to join tables across srcs, but
 #'   it is a potentially expensive operation so you must opt into it.
-#' @param ... other parameters passed onto individual methods
+#' @param ... other parameters passed onto methods
+#' @name join
+NULL
+
+#' @rdname join
 #' @export
-join <- function(x, y, by = NULL, type = "left", copy = FALSE, ...) {
-  UseMethod("join")
+inner_join <- function(x, y, by = NULL, copy = FALSE, ...) {
+  UseMethod("inner_join")
 }
 
-#' Semi joins and anti joins.
-#' 
-#' Groups are ignored for the purpose of joining, but the result preserves
-#' the grouping of \code{x}.
-#' 
-#' A semi join keeps all records from \code{x} that have matching rows in 
-#' \code{y}. An anti-join does the opposite: it preserves all records from 
-#' \code{x} that don't have matching values in \code{y}. Both keep the columns 
-#' of \code{x}, and don't include any from \code{y}.
-#' 
-#' A semi join differs from an inner join (with just the \code{by} variables)
-#' because an inner join will return one row of \code{x} for each matching row
-#' of \code{y}, where a semi join will never duplicated rows in \code{x}.
-#' 
-#' @inheritParams join
-#' @param anti If \code{TRUE}, performs an anti join instead of a semi join.
+#' @rdname join
 #' @export
-semi_join <- function(x, y, by = NULL, anti = FALSE, copy = FALSE, ...) {
+left_join <- function(x, y, by = NULL, copy = FALSE, ...) {
+  UseMethod("left_join")
+}
+
+#' @rdname join
+#' @export
+semi_join <- function(x, y, by = NULL, copy = FALSE, ...) {
   UseMethod("semi_join")
 }
 
+#' @rdname join
+#' @export
+anti_join <- function(x, y, by = NULL, copy = FALSE, ...) {
+  UseMethod("anti_join")
+}
 
 common_by <- function(x, y) {
   by <- intersect(tbl_vars(x), tbl_vars(y))
