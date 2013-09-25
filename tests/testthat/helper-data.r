e5885a4bc17ac58b069f47d68778c7774e412dd5
hadley <h.wickham@gmail.com>
2013-09-25 08:24:50 -0500

    Use copy_to in test helper

--- tests/testthat/helper-data.r
+++ tests/testthat/helper-data.r
@@ -1,18 +1,6 @@
 library(data.table)
 library(RSQLite)
 
-make_sql_table <- function(x, name = deparse(substitute(x)), ...) {
-  path <- file.path(tempdir(), paste0(name, ".sqlite3"))
-
-  test_con <- dbConnect(dbDriver("SQLite"), dbname = path)
-  on.exit(dbDisconnect(test_con))
-
-  object <- as.data.frame(x)
-  dbWriteTable(test_con, name, x, row.names = FALSE, overwrite = TRUE)
-
-  tbl_sqlite(path, name)
-}
-
 strip <- function(x) {
   x <- as.data.frame(x)
   rownames(x) <- NULL
@@ -23,7 +11,7 @@ clone_tbls <- function(df) {
   list(
     df = tbl_df(df),
     dt = tbl_dt(df),
-    sqlite = make_sql_table(df)
+    sqlite = copy_to(src_tmp(), df, name = random_table_name())
   )
 }
 
