30035f7d8b2f22d713f2bab41357f05360c58f73
hadley <h.wickham@gmail.com>
2013-10-02 14:46:05 -0500

    One file each for data sets

---
+++ R/data-hflights.r
@@ -0,0 +1,65 @@
+#' Houston flights data
+#'
+#' This dataset contains all flights departing from Houston airports IAH
+#' (George Bush Intercontinental) and HOU (Houston Hobby). The data comes
+#' from the Research and Innovation Technology Administration at the
+#' Bureau of Transporation statistics:
+#' \url{http://www.transtats.bts.gov/DatabaseInfo.asp?DB_ID=120&Link=0}
+#'
+#' \code{src_hflights} caches a SQLite version of the data in a standard
+#' location for use in examples.
+#'
+#' @section Variables:
+#'
+#' \itemize{
+#'   \item \code{Year}, \code{Month}, \code{DayofMonth}: date of departure
+#'   \item \code{DayOfWeek}: day of week of departure (useful for removing
+#'     weekend effects)
+#'  \item \code{DepTime}, \code{ArrTime}: departure and arrival times
+#'    (in local time, hhmm)
+#'  \item \code{UniqueCarrier}: unique abbreviation for a carrier
+#'  \item \code{FlightNum}: flight number
+#'  \item \code{TailNum}: airplane tail number
+#'  \item \code{ActualElapsedTime}: elapsed time of flight, in minutes
+#'  \item \code{AirTime}: flight time, in minutes
+#'  \item \code{ArrDelay}, \code{DepDelay}: arrival and departure delays,
+#'    in minutes
+#'  \item \code{Origin}, \code{Dest} origin and destination airport codes
+#'  \item \code{Distance}: distance of flight, in miles
+#'  \item \code{TaxiIn}, \code{TaxiOut}: taxi in and out times in minutes
+#'  \item \code{Cancelled}: cancelled indicator: 1 = Yes, 0 = No
+#'  \item \code{CancellationCode}: reason for cancellation: A = carrier,
+#'    B = weather, C = national air system, D = security
+#'  \item \code{Diverted}: diverted indicator: 1 = Yes, 0 = No
+#' }
+#' @docType data
+#' @name hflights
+#' @usage hflights
+#' @format A data frame with 227,496 rows and 21 columns.
+#' @examples
+#' head(hflights)
+#'
+#' hflight_db <- tbl(src_hflights(), "hflights")
+#' hflight_db
+NULL
+
+#' @export
+#' @rdname hflights
+src_hflights <- function(path = NULL) {
+  if (!is.null(cache$hflights)) return(cache$hflights)
+  
+  path <- db_location(path, "hflights.sqlite")
+  
+  if (!file.exists(path)) {
+    message("Caching hflights db at ", path)
+    
+    src <- src_sqlite(path, create = TRUE)
+    copy_to(src, hflights, temporary = FALSE,
+      indexes = list("Dest", c("Year", "Month", "DayofMonth"), "UniqueCarrier"))
+  } else {
+    src <- src_sqlite(path)
+  }
+  
+  cache$hflights <- src
+  src
+}
---
+++ R/data-lahman.r
@@ -0,0 +1,101 @@
+#' Cache and retrieve an \code{src_sqlite} of the Lahman baseball database.
+#'
+#' This creates an interesting database using data from the Lahman baseball
+#' data source, provided by Sean Lahman at
+#' \url{http://www.seanlahman.com/baseball-archive/statistics/}, and
+#' made easily available in R through the \pkg{Lahman} package by
+#' Michael Friendly, Dennis Murphy and Martin Monkman. See the documentation
+#' for that package for documentation of the inidividual tables.
+#'
+#' @param path location to look for and cache SQLite database. If \code{NULL},
+#'   the default, will first try storing in the installed package directory, and
+#'   if that isn't writeable, a temporary directory.
+#' @param dbname,... Arguments passed to \code{\link{src_postgresql}} on first
+#'   load. The defaults assume you have a local postgresql install with
+#'   \code{lahman} database already created.
+#' @export
+#' @examples
+#' lahman_sqlite()
+#' batting <- tbl(lahman_sqlite(), "Batting")
+#' batting
+#'
+#' # Connect to a local postgres database with lahman database, if available
+#' if (has_lahman_postgres()) {
+#'   lahman_postgres()
+#'   batting <- tbl(lahman_postgres(), "Batting")
+#' }
+#' @name lahman
+NULL
+
+#' @export
+#' @rdname lahman
+lahman_sqlite <- function(path = NULL) {
+  if (!is.null(cache$lahman_sqlite)) return(cache$lahman_sqlite)
+  
+  path <- db_location(path, "lahman.sqlite")
+  
+  if (!file.exists(path)) {
+    message("Caching Lahman db at ", path)
+    src <- src_sqlite(path, create = TRUE)
+    cache_lahman(src, quiet = FALSE)
+  } else {
+    src <- src_sqlite(path)
+  }
+  
+  cache$lahman_sqlite <- src
+  src
+}
+
+#' @export
+#' @rdname lahman
+lahman_postgres <- function(dbname = "lahman", ...) {
+  if (!is.null(cache$lahman_postgres)) return(cache$lahman_postgres)
+  
+  src <- src_postgres(dbname, ...)
+  
+  missing <- setdiff(lahman_tables(), src_tbls(src))
+  if (length(missing) > 0) {
+    cache_lahman(src, quiet = FALSE)
+  }
+  
+  cache$lahman_postgres <- src
+  src
+}
+
+#' @name lahman
+#' @export
+has_lahman_postgres <- function(dbname = "lahman", ...) {
+  if (!require("RPostgreSQL")) return(FALSE)
+  
+  ok <- FALSE
+  try({
+    src_postgres(dbname, ...)
+    ok <- TRUE
+  }, silent = TRUE)
+  
+  ok
+}
+
+
+cache_lahman <- function(src, index = TRUE, quiet = FALSE) {
+  if (!require("Lahman")) {
+    stop("Please install the Lahman package", call. = FALSE)
+  }
+  
+  tables <- setdiff(lahman_tables(), src_tbls(src))
+  for(table in tables) {
+    df <- get(table, "package:Lahman")
+    if (!quiet) message("Creating table ", table)
+    
+    ids <- as.list(names(df)[grepl("ID$", names(df))])
+    copy_to(src, df, table, indexes = if (index) ids, temporary = FALSE)
+  }
+  
+  invisible(TRUE)
+}
+
+# Get list of all non-label data frames in package
+lahman_tables <- function() {
+  tables <- data(package = "Lahman")$results[, 3]
+  tables[!grepl("Labels", tables)]
+}
