b884f3778a4030b4ff46aaa46112efc7047d6dc3
hadley <h.wickham@gmail.com>
2013-10-02 14:41:08 -0500

    Sort tbls when printing src

--- R/src-sql.r
+++ R/src-sql.r
@@ -28,7 +28,7 @@ src_tbls.src_sql <- function(x, ...) {
 #' @S3method format src_sql
 format.src_sql <- function(x, ...) {
   paste0("src:  ", brief_desc(x), "\n",
-    wrap("tbls: ", paste0(src_tbls(x), collapse = ", ")))
+    wrap("tbls: ", paste0(sort(src_tbls(x)), collapse = ", ")))
 }
 
 begin_transaction <- function(x) UseMethod("begin_transaction")
