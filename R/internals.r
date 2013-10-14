9c72190311a2c6da626626946ce08589cd3db3ca
hadley <h.wickham@gmail.com>
2013-10-14 08:33:43 -0500

    Fix indenting and strip srcref from generated f

--- R/internals.r
+++ R/internals.r
@@ -1,23 +1,24 @@
 order_ <- function(..., data){
-    parent_frame <- parent.frame()
-    if( missing(data) ) {
-        env <- parent_frame
-    } else {
-        env <- as.environment(data)
-        parent.env(env) <- parent_frame
-    }
-    order_impl( dots(...) , env )
+  parent_frame <- parent.frame()
+  if(missing(data)) {
+    env <- parent_frame
+  } else {
+    env <- as.environment(data)
+    parent.env(env) <- parent_frame
+  }
+  order_impl(dots(...) , env)
 }  
 
 .data_dots <- function(fun, DOTS = dots){
-    f <- function(data, ...){}
-    body(f) <- substitute({
-        parent_frame <- parent.frame()
-        env <- as.environment(data)
-        parent.env(env) <- parent_frame
-        FUN( data, DOTS(...) , env )   
-    } , list( FUN = substitute(fun), DOTS = substitute(DOTS) ) )
-    f
+  f <- function(data, ...){}
+  body(f) <- substitute({
+    parent_frame <- parent.frame()
+    env <- as.environment(data)
+    parent.env(env) <- parent_frame
+    FUN( data, DOTS(...) , env )   
+  }, list( FUN = substitute(fun), DOTS = substitute(DOTS)))
+  attr(f, "srcref") <- NULL
+  f
 }
 
 arrange_ <- .data_dots(arrange_impl)
@@ -27,10 +28,10 @@ filter_grouped_df <- .data_dots(filter_grouped_df_impl)
 mutate_  <- .data_dots(mutate_impl, named_dots)
 
 equal_ <- function(x, y){
-    equal_data_frame(x, y) 
+  equal_data_frame(x, y) 
 }
 
-sort_ <- function( data ){
-    sort_impl(data)    
+sort_ <- function(data){
+  sort_impl(data)
 }
 
