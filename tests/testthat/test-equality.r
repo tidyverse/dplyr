45d20d6df410d46e9b86aa6eb125e92977ca51c9
hadley <h.wickham@gmail.com>
2013-10-23 09:54:21 -0500

    Make formal equality tests

---
+++ tests/testthat/test-equality.r
@@ -0,0 +1,42 @@
+context("Equality")
+
+set.seed(1014)
+# A data frame with all major types
+df_all <- data.frame(
+  a = c(1, 2.5),
+  b = 1:2,
+  c = c(T, F),
+  d = c("a", "b"),
+  e = factor(c("a", "b")),
+  f = Sys.Date() + 1:2,
+  g = Sys.time() + 1:2,
+  stringsAsFactors = FALSE
+)
+
+test_that("data frames equal to themselves", {
+  expect_true(equal_data_frame(mtcars, mtcars))
+  expect_true(equal_data_frame(iris, iris))
+  expect_true(equal_data_frame(df_all, df_all))
+})
+
+test_that("data frames equal to random permutations of themselves", {
+  scramble <- function(x){
+    x[sample(nrow(x)), sample(ncol(x)), drop = FALSE]
+  }
+  
+  expect_true(equal_data_frame(mtcars, scramble(mtcars)))
+  expect_true(equal_data_frame(iris, scramble(iris)))
+  expect_true(equal_data_frame(df_all, scramble(df_all)))
+})
+
+test_that("data frames not equal if missing row", {
+  expect_false(equal_data_frame(mtcars, mtcars[-1, ]))
+  expect_false(equal_data_frame(iris, iris[-1, ]))
+  expect_false(equal_data_frame(df_all, df_all[-1, ]))
+})
+
+test_that("factors equal only if levels equal", {
+  df1 <- data.frame(x = factor(c("a", "b")))
+  df2 <- data.frame(x = factor(c("a", "b")), levels = c("b", "a"))
+  expect_false(equal_data_frame(df1, df2))
+})
