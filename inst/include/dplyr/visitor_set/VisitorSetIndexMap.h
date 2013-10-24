39c9f14930ab8feb156ee7891970774925e6d25b
Romain François <romain@r-enthusiasts.com>
2013-10-24 09:39:01 +0200

    rm licence headers. licence is covered in DESCCRIPTION

--- inst/include/dplyr/visitor_set/VisitorSetIndexMap.h
+++ inst/include/dplyr/visitor_set/VisitorSetIndexMap.h
@@ -1,21 +1,3 @@
-// Copyright (C) 2013    Romain Francois
-// Copyright (C) 2013    Rice University
-//
-// This file is part of dplyr.
-//
-// dplyr is free software: you can redistribute it and/or modify it
-// under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 2 of the License, or
-// (at your option) any later version.
-//
-// dplyr is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with dplyr.  If not, see <http://www.gnu.org/licenses/>.
-
 #ifndef dplyr_VisitorSetIndexMap_H
 #define dplyr_VisitorSetIndexMap_H
 
