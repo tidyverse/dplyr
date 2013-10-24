8dcdf852e4314e1ae07b060664bbf1d7a7a9c17e
Romain François <romain@r-enthusiasts.com>
2013-10-24 09:46:50 +0200

    move DataFrameVisitors impl to headers

--- inst/include/dplyr/DataFrameVisitors.h
+++ inst/include/dplyr/DataFrameVisitors.h
@@ -1,3 +1,21 @@
+// Copyright (C) 2013    Romain Francois
+// Copyright (C) 2013    Rice University
+//
+// This file is part of dplyr.
+//
+// dplyr is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// dplyr is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with dplyr.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef dplyr_DataFrameVisitors_H
 #define dplyr_DataFrameVisitors_H
 
@@ -19,7 +37,25 @@ class DataFrameVisitors :
     public:
         typedef VectorVisitor visitor_type ;
         
-        DataFrameVisitors( const Rcpp::DataFrame& data_, const Rcpp::CharacterVector& names ) ;
+        DataFrameVisitors( const Rcpp::DataFrame& data_, const Rcpp::CharacterVector& names ) : 
+        data(data_), 
+        visitors()
+        {
+            std::string name ;
+            int n = names.size() ;
+            nvisitors = 0 ;
+            std::vector<std::string> visitor_names_ ;
+            for( int i=0; i<n; i++){
+                name = (String)names[i] ;
+                SEXP column = data[name] ;
+                if( column != R_NilValue ){
+                    nvisitors++ ;
+                    visitor_names_.push_back( name ) ;
+                    visitors.push_back( visitor( column ) ) ;
+                }
+            }
+            visitor_names = wrap( visitor_names_ );
+        } 
         
         ~DataFrameVisitors(){
             delete_all( visitors );
