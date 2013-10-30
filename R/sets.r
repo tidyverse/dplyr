7a90e11fd9b1f3dc4647885b29af231c89303546
hadley <h.wickham@gmail.com>
2013-10-30 14:14:11 -0500

    Provide generics for set operations. #93

---
+++ R/sets.r
@@ -0,0 +1,70 @@
+#' Set operations.
+#' 
+#' These functions override the set functions provided in base to make them
+#' generic so that efficient versions for data frames and other tables can be
+#' provided. The default methods call the base versions.
+#' 
+#' @param x,y objects to compare (ignoring order)
+#' @param .... other arguments passed on to methods
+#' @name setops
+NULL
+
+#' @rdname setops
+#' @export
+intersect <- function(x, y, ...) UseMethod("intersect")
+#' @rdname setops
+#' @export
+union <- function(x, y, ...) UseMethod("union")
+#' @rdname setops
+#' @export
+setdiff <- function(x, y, ...) UseMethod("setdiff")
+#' @rdname setops
+#' @export
+setequal <- function(x, y, ...) UseMethod("setequal")
+
+#' @S3method intersect default
+intersect.default <- function(x, y, ...) base::intersect(x, y)
+#' @S3method union default
+union.default <-     function(x, y, ...) base::union(x, y)
+#' @S3method setdiff default
+setdiff.default <-   function(x, y, ...) base::setdiff(x, y)
+#' @S3method setequal default
+setequal.default <-  function(x, y, ...) base::setequal(x, y)
+
+#' Set operations for data frames.
+#' 
+#' These set operations are implemented with an efficeint C++ backend.
+#' 
+#' @param x,y Two data frames to compare, igoring order of row and columns
+#' @param ... Needed for compatibility with generic. Otherwise ignored.
+#' 
+#' @name setops-data.frame
+#' @examples
+#' mtcars$model <- rownames(mtcars)
+#' first <- mtcars[1:20, ]
+#' second <- mtcars[10:32, ]
+#' 
+#' intersect(first, second)
+#' union(first, second)
+#' setdiff(first, second)
+#' setdiff(second, first)
+#' 
+#' setequal(mtcars, mtcars[32:1, ])
+NULL
+
+#' @rdname setops-data.frame
+#' @method intersect data.frame
+#' @export
+intersect.data.frame <- function(x, y, ...) intersect_data_frame(x, y)
+#' @rdname setops-data.frame
+#' @method union data.frame
+#' @export
+union.data.frame <-     function(x, y, ...) union_data_frame(x, y)
+#' @rdname setops-data.frame
+#' @method setdiff data.frame
+#' @export
+setdiff.data.frame <-   function(x, y, ...) setdiff_data_frame(x, y)
+#' @rdname setops-data.frame
+#' @method setequal data.frame
+#' @export
+setequal.data.frame <-  function(x, y, ...) equal_data_frame(x, y)
