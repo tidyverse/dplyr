485ceaa15be6506b5a24630173af29c9c4a375a7
Romain François <romain@r-enthusiasts.com>
2013-10-30 14:22:30 +0100

    added JoinFactorVisitor

--- inst/include/dplyr/JoinVisitorImpl.h
+++ inst/include/dplyr/JoinVisitorImpl.h
@@ -37,7 +37,7 @@ namespace dplyr{
             return res ;
         }
         
-    private:
+    protected:
         Vector<RTYPE> left, right ;
         hasher hash_fun ;
         
@@ -47,11 +47,116 @@ namespace dplyr{
         
     } ;
     
+    class StringLessPredicate : comparisons<STRSXP>{
+    public:
+        typedef SEXP value_type ;
+        inline bool operator()( SEXP x, SEXP y){
+            return comparisons::is_less(x, y) ;    
+        }
+    } ;
+    
+    class JoinFactorVisitor : public JoinVisitorImpl<INTSXP> {
+    public:
+        typedef JoinVisitorImpl<INTSXP> Parent ;
+        
+        JoinFactorVisitor( const IntegerVector& left, const IntegerVector& right ) : 
+            Parent(left, right), 
+            left_levels_ptr( internal::r_vector_start<STRSXP>( left.attr("levels") ) ) ,
+            right_levels_ptr( internal::r_vector_start<STRSXP>( right.attr("levels") ) )
+            {}
+        
+        inline size_t hash(int i){
+            return string_hash( get(i) ) ; 
+        }
+        
+        inline bool equal( int i, int j){
+            return string_compare.equal_or_both_na( get(i), get(j) ) ;
+        }
+        
+        inline SEXP subset( const VisitorSetIndexSet<DataFrameJoinVisitors>& set ){
+            int n = set.size() ;
+            Vec res = no_init(n) ;
+            
+            typedef std::set<SEXP, StringLessPredicate> LevelsSet ;
+            LevelsSet levels ;
+            std::vector<SEXP> strings(n) ;
+            VisitorSetIndexSet<DataFrameJoinVisitors>::const_iterator it = set.begin() ;
+            for( int i=0; i<n; i++, ++it){
+                SEXP s = get(*it) ;
+                levels.insert( s );
+                strings[i] = s ;
+            }
+            boost::unordered_map<SEXP,int> invmap ;
+            
+            int nlevels = levels.size() ;
+            LevelsSet::const_iterator lit = levels.begin() ;
+            CharacterVector levels_vector( nlevels );
+            for( int i=0; i<nlevels; i++, ++lit){
+                invmap[*lit] = i+1 ;
+                levels_vector[i] = *lit ;
+            }
+            
+            for( int i=0; i<n; i++){
+                res[i] = invmap[strings[i]] ;
+            }
+            res.attr( "class" )  = Parent::left.attr("class" ) ;
+            res.attr( "levels" ) = levels_vector ;
+            
+            return res ;
+        }
+        
+        inline SEXP subset( const std::vector<int>& indices ){
+            int n = indices.size() ;
+            Vec res = no_init(n) ;
+            
+            typedef std::set<SEXP, StringLessPredicate> LevelsSet ;
+            LevelsSet levels ;
+            std::vector<SEXP> strings(n) ;
+            for( int i=0; i<n; i++){
+                SEXP s = get(indices[i]) ;
+                levels.insert( s );
+                strings[i] = s ;
+            }
+            boost::unordered_map<SEXP,int> invmap ;
+            
+            int nlevels = levels.size() ;
+            LevelsSet::const_iterator lit = levels.begin() ;
+            CharacterVector levels_vector( nlevels );
+            for( int i=0; i<nlevels; i++, ++lit){
+                invmap[*lit] = i+1 ;
+                levels_vector[i] = *lit ;
+            }
+            
+            for( int i=0; i<n; i++){
+                res[i] = invmap[ strings[i] ] ;
+            }
+            res.attr( "class" )  = Parent::left.attr("class" ) ;
+            res.attr( "levels" ) = levels_vector ;
+            
+            return res ;
+        }
+        
+            
+    private:
+        SEXP* left_levels_ptr ;
+        SEXP* right_levels_ptr ;
+        comparisons<STRSXP> string_compare ;
+        boost::hash<SEXP> string_hash ;
+    
+        inline SEXP get(int i){
+            return i>=0 ? left_levels_ptr[left[i]] : right_levels_ptr[right[-i-1]] ;    
+        }
+        
+    } ;
+    
     inline JoinVisitor* join_visitor( SEXP left, SEXP right ){
         if( TYPEOF(left) != TYPEOF(right) ) 
             stop( "cannot create join visitor from incompatible types" ) ;
         switch( TYPEOF(left) ){
-            case INTSXP:  return new JoinVisitorImpl<INTSXP> ( left, right ) ;
+            case INTSXP:
+                if( Rf_inherits( left, "factor" ) )
+                    return new JoinFactorVisitor(left, right) ;
+                return new JoinVisitorImpl<INTSXP> ( left, right ) ;
             case REALSXP: return new JoinVisitorImpl<REALSXP>( left, right ) ;
             case LGLSXP:  return new JoinVisitorImpl<LGLSXP> ( left, right ) ;
             case STRSXP:  return new JoinVisitorImpl<STRSXP> ( left, right ) ;
