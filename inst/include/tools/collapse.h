9d917a422252253b60095816544425ff73d2ccec
Romain François <romain@r-enthusiasts.com>
2013-10-31 04:28:13 +0100

    added collapse

---
+++ inst/include/tools/collapse.h
@@ -0,0 +1,26 @@
+#ifndef dplyr_collapse_H
+#define dplyr_collapse_H
+
+namespace Rcpp {
+    
+    template <int RTYPE> 
+    const char* toString( typename ::Rcpp::traits::storage_type<RTYPE>::type from){
+        SEXP s = internal::r_coerce<RTYPE,STRSXP>( from ) ;
+        return CHAR(s) ;
+    }
+
+    template <int RTYPE>
+    std::string collapse( const Vector<RTYPE>& x, const char* sep = ", " ){
+        std::stringstream ss;
+        int n = x.size() ;
+        if( n > 0){
+            ss << toString<RTYPE>(x[0]) ;
+            for( int i=1; i<n; i++) 
+                ss << sep << toString<RTYPE>(x[i]) ;
+        }
+        
+        return ss.str(); 
+    }   
+          
+}    
+#endif
