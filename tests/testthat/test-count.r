7fb7c42399ae0787a8f45a7e72441197bd10484f
hadley <h.wickham@gmail.com>
2013-11-01 14:16:09 -0400

    More tests for count_distinct. Closes #102

--- tests/testthat/test-count.r
+++ tests/testthat/test-count.r
@@ -1,9 +1,25 @@
 context("Count")
 
-test_that("count_distinct gives the correct results", {
+test_that("count_distinct gives the correct results on iris", {
   expect_equal( 
-    sapply( iris, count_distinct ), 
-    sapply( iris, function(.) length(unique(.)) )
+    sapply(iris, count_distinct), 
+    sapply(iris, function(.) length(unique(.)))
   )
 })
 
+df_var <- data.frame(
+  l = c(T, F, F),
+  i = c(1, 1, 2), 
+  d = Sys.Date() + c(1, 1, 2),
+  f = factor(letters[c(1, 1, 2)]),
+  n = c(1, 1, 2) + 0.5, 
+  t = Sys.time() + c(1, 1, 2),
+  c = letters[c(1, 1, 2)],
+  stringsAsFactors = FALSE
+)
+test_that("count_distinct gives correct results for key types", {
+  expect_equal( 
+    sapply(df_var, count_distinct), 
+    sapply(df_var, function(.) length(unique(.)))
+  )
+})
