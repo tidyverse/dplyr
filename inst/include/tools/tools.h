4f1d7e0d269c619380e2223a25105a88323553bf
Romain François <romain@r-enthusiasts.com>
2013-11-03 12:03:59 +0100

    use Rcpp 0.10.6 and remove Shield/Shelter/Armor from dplyr

--- inst/include/tools/tools.h
+++ inst/include/tools/tools.h
@@ -5,14 +5,6 @@
 #include <tools/delete_all.h>
 #include <tools/ListOf.h>
 #include <tools/collapse.h>
- 
-// remove these lines and the files when Rcpp 0.10.6 is released
-#if !defined(Rcpp_protection_protection_H)
-    #include <tools/Shelter.h>
-    #include <tools/Shield.h>
-    #include <tools/Armor.h>
-#endif
-
 #include <tools/ShrinkableVector.h>
 #include <tools/wrap_subset.h>
 #include <tools/get_all_second.h>
