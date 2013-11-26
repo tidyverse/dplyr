073262eb492e0c63ad8bc6bd5294c2660c59115c
hadley <h.wickham@gmail.com>
2013-11-26 14:22:23 -0600

    Provide option to control default number of rows printed

--- R/zzz.r
+++ R/zzz.r
@@ -9,7 +9,8 @@
   op <- options()
   op.dplyr <- list(
     dplyr.show_sql = FALSE,
-    dplyr.explain_sql = FALSE
+    dplyr.explain_sql = FALSE,
+    dplyr.max_print = 10L
   )
   toset <- !(names(op.dplyr) %in% names(op))
   if(any(toset)) options(op.dplyr[toset])
