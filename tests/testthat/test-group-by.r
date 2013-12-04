a37a8dc6d51ebab4c77368e86b13d35e607cb153
Romain François <romain@r-enthusiasts.com>
2013-12-04 01:42:16 +0100

    'n =' made test fail

--- tests/testthat/test-group-by.r
+++ tests/testthat/test-group-by.r
@@ -53,7 +53,7 @@ df_var <- data.frame(
   i = 1:2, 
   d = Sys.Date() + 1:2,
   f = factor(letters[1:2]),
-  n = 1:2 + 0.5, 
+  num = 1:2 + 0.5, 
   t = Sys.time() + 1:2,
   c = letters[1:2],
   stringsAsFactors = FALSE
