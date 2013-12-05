ea010d365edac003cde094700e876d2460dd0967
hadley <h.wickham@gmail.com>
2013-12-05 09:43:19 -0600

    Convert tbl_cpp class to tbl_df

--- tests/testthat/test-tbl-df.r
+++ tests/testthat/test-tbl-df.r
@@ -2,5 +2,5 @@ context("tbl_df/grouped_df")
 
 test_that("grouped_df returns tbl_df if no groups", {
   df <- grouped_df(mtcars, list())
-  expect_equal(class(df), c("tbl_cpp", "tbl", "data.frame"))
+  expect_equal(class(df), c("tbl_df", "tbl", "data.frame"))
 })
