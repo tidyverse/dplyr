3d9967e1be468614249056c72b81c2a57d11c087
hadley <h.wickham@gmail.com>
2013-12-06 15:45:45 -0600

    Only create if _doesn't_ exist

--- R/data-hflights.r
+++ R/data-hflights.r
@@ -36,7 +36,7 @@ hflights_postgres <- function(dbname = "hflights", ...) {
   if (!is.null(cache$hflights_postgres)) return(cache$hflights_postgres)
 
   src <- src_postgres(dbname, ...)
-  if (db_has_table(src$con, "hflights")) {
+  if (!db_has_table(src$con, "hflights")) {
     copy_to(src, hflights, temporary = FALSE,
       indexes = list("Dest", c("Year", "Month", "DayofMonth"), "UniqueCarrier"))
   }
