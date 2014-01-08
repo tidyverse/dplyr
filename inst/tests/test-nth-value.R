320061b1c59a39a6f93350df7e7bb66c7159ced2
hadley <h.wickham@gmail.com>
2014-01-08 11:44:33 -0600

    Better nth value behaviour (and tests)

---
+++ inst/tests/test-nth-value.R
@@ -0,0 +1,27 @@
+context("Nth value")
+
+test_that("nth works with lists", {
+  x <- list(1, 2, 3)
+  
+  expect_equal(nth(x, 1), 1)
+  expect_equal(nth(x, 4), NULL)
+  expect_equal(nth(x, 4, default = 1), 1)
+})
+
+test_that("first and last use default value for 0 length inputs", {
+  expect_equal(first(numeric()), NA_real_)
+  expect_equal(last(numeric()), NA_real_)
+})
+
+test_that("default value returns appropriate missing for basic vectors", {
+  expect_equal(default_missing(TRUE), NA)
+  expect_equal(default_missing(1), NA_real_)
+  expect_equal(default_missing(1L), NA_integer_)
+  expect_equal(default_missing("a"), NA_character_)
+  expect_equal(default_missing(list()), NULL)
+})
+
+test_that("default value errors for complicated structures", {
+  expect_error(default_missing(factor("a")), "generate default for object")  
+  expect_error(default_missing(mtcars), "generate default for object")
+})
