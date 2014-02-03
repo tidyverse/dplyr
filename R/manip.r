a16b9543560b7aa91b89c67e02c73185db4ac208
hadley <h.wickham@gmail.com>
2014-02-03 17:14:41 -0600

    Semantics for select

--- R/manip.r
+++ R/manip.r
@@ -79,11 +79,133 @@ arrange <- function(.data, ...) UseMethod("arrange")
 #' @export
 select <- function(.data, ...) UseMethod("select")
 
+
+#' Select variables.
+#'
+#'
+#' @section Selection functions:
+#' As well as using existing functions like \code{:} and \code{c}, there are
+#' a number of special functions that only work inside \code{select}
+#'
+#' \itemize{
+#'  \item \code{starts_with(x, ignore.case = FALSE)}:
+#'    names starts with \code{x}
+#'  \item \code{ends_with(x, ignore.case = FALSE)}:
+#'    names ends in \code{x}
+#'  \item \code{contains(x, ignore.case = FALSE)}:
+#'    selects all variables whose name contains \code{x}
+#'  \item \code{matches(x, ignore.case = FALSE)}:
+#'    selects all variables whose name matches the regular expression \code{x}
+#'  \item \code{num_range(x = 1:5)}:
+#'    selects all variables (numerically) from x1 to x5.
+#' }
+#'
+#' To exclude instead of including variables, use \code{-}.
+#'
+#' @param vars A character vector of existing column names.
+#' @param ... Expressions to compute
+#' @export
+#' @keywords internal
+#' @value A named character vector. Values are existing column names,
+#'   names are new names.
+#' @examples
+#' # Keep variables
+#' select_vars(names(iris), starts_with("Petal"))
+#' select_vars(names(iris), ends_with("Width"))
+#' select_vars(names(iris), contains("etal"))
+#' select_vars(names(iris), matches(".t."))
+#' select_vars(names(iris), Petal.Length, Petal.Width)
+#'
+#' df <- as.data.frame(matrix(runif(100), nrow = 10))
+#' df <- df[c(3, 4, 7, 1, 9, 8, 5, 2, 6, 10)]
+#' select_vars(names(df), num_range(V = 4:6))
+#'
+#' # Drop variables
+#' select_vars(names(iris), -starts_with("Petal"))
+#' select_vars(names(iris), -ends_with("Width"))
+#' select_vars(names(iris), -contains("etal"))
+#' select_vars(names(iris), -matches(".t."))
+#' select_vars(names(iris), -Petal.Length, -Petal.Width)
+#'
+#' # Rename variables
+#' select_vars(names(iris), petal_length = Petal.Length)
+#' select_vars(names(iris), petal = starts_with("Petal"))
+select_vars <- function(vars, ..., env = parent.frame()) {
+  args <- dots(...)
+
+  names_list <- setNames(as.list(seq_along(vars)), vars)
+  names_env <- list2env(names_list, parent = env)
+
+  # No non-standard evaluation - but all names mapped to their position.
+  # Keep integer semantics: include = +, exclude = -
+  # How to document starts_with, ends_with etc?
+
+  select_funs <- list(
+    starts_with = function(match, ignore.case = TRUE) {
+      stopifnot(is.string(match), !is.na(match))
+
+      if (ignore.case) match <- tolower(match)
+      n <- nchar(match)
+
+      if (ignore.case) vars <- tolower(vars)
+      which(substr(vars, 1, n) == match)
+    },
+    ends_with = function(match, ignore.case = TRUE) {
+      stopifnot(is.string(match), !is.na(match))
+
+      if (ignore.case) match <- tolower(match)
+      n <- nchar(match)
+
+      if (ignore.case) vars <- tolower(vars)
+      length <- nchar(vars)
+
+      which(substr(vars, pmax(1, length - n + 1), length) == match)
+    },
+    contains = function(match, ignore.case = TRUE) {
+      stopifnot(is.string(match))
+
+      grep(match, vars, ignore.case = ignore.case)
+    },
+    matches = function(match, ignore.case = TRUE) {
+      stopifnot(is.string(match))
+
+      grep(match, vars, ignore.case = ignore.case)
+    },
+    num_range = function(...) {
+      col <- list(...)
+      vals <- Map(function(pref, range) paste0(pref, range), names(col), col)
+      match(unlist(vals, use.names = FALSE), vars)
+    }
+  )
+  select_env <- list2env(select_funs, names_env)
+
+  ind_list <- lapply(args, eval, env = select_env)
+  names(ind_list) <- names2(args)
+
+  ind <- unlist(ind_list)
+  incl <- ind[ind > 0]
+  if (length(incl) == 0) {
+    incl <- seq_along(vars)
+  }
+  # Remove dupliates (unique loses names)
+  incl <- incl[!duplicated(incl)]
+
+  # Remove variables to be excluded (setdiff loses names)
+  excl <- abs(ind[ind < 0])
+  incl <- incl[match(incl, excl, 0L) == 0L]
+
+  # Ensure all output vars named
+  unnamed <- names2(incl) == ""
+  names(incl)[unnamed] <- vars[incl][unnamed]
+  setNames(vars[incl], names(incl))
+}
+
+
 #' The number of observations in the current group.
-#' 
+#'
 #' This function is implemented special for each data source and can only
 #' be used from within \code{\link{summarise}}.
-#' 
+#'
 #' @export
 #' @examples
 #' if (require("hflights")) {
