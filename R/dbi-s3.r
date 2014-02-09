847c65df3d6a334a9668123b8b5466deeae1db9e
TMOD <tommy.odell@gmail.com>
2014-02-09 14:57:21 +0800

    1. sql_as(): No longer has leading ")" i.e. this must now be in the call to build_sql()

--- R/dbi-s3.r
+++ R/dbi-s3.r
@@ -373,12 +373,12 @@ sql_as <- function(con, alias = NULL) {
 
 #' @export
 sql_as.DBIConnection <- function(con, alias = NULL) {
-  return(sql(paste0(") AS ", alias)))
+  return(sql(paste0(" AS ", alias)))
 }
  
 #' @export
 sql_as.OraConnection <- function(con, alias = NULL) {
-  return(sql(paste0(") ", alias)))
+  return(sql(alias))
 }
 
 sql_create_indexes <- function(con, table, indexes = NULL, ...) {
