a3393b34c8b6d61b454edaaf3873d5b3dca64def
Romain Francois <romain@r-enthusiasts.com>
2014-02-24 11:06:28 +0100

    less sfinae noise on solaris

--- inst/include/solaris/solaris.h
+++ inst/include/solaris/solaris.h
@@ -9,6 +9,9 @@ namespace traits{
   template <typename T> struct is_convertible< std::vector<T>, SEXP> : public false_type{} ;
   template <> struct is_convertible<Range,SEXP> : public false_type{} ;
   
+  template <int RTYPE, bool NA>
+  struct is_convertible< sugar::Minus_Vector_Primitive< RTYPE, NA, Vector<RTYPE> >, SEXP> : public false_type{} ;
+  
 }
 }
 
