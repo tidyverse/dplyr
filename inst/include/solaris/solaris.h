35e445cba1c5ccc980b8efd4c38d8762eac91219
Romain Francois <romain@r-enthusiasts.com>
2014-02-24 10:53:08 +0100

    make less noise solaris please

---
+++ inst/include/solaris/solaris.h
@@ -0,0 +1,14 @@
+#ifndef DPLYR_SOLARIS_H
+#define DPLYR_SOLARIS_H
+
+#ifdef __SUNPRO_CC
+
+namespace Rcpp{
+namespace traits{
+
+  template <> struct is_convertible< std::vector<int>, SEXP> : public true_type{} ;
+  
+}
+}
+
+#endif
