a7c433e8726e443c7efc172e1b4b0e1e901a11eb
hannes <hannes@cwi.nl>
2014-03-13 09:48:17 +0100

    MonetDB connector documentation update

--- R/src-monetdb.r
+++ R/src-monetdb.r
@@ -1,3 +1,86 @@
+#' Connect to MonetDB (http://www.monetdb.org), an Open Source analytics-focused database
+#'
+#' Use \code{src_monetdb} to connect to an existing MonetDB database,
+#' and \code{tbl} to connect to tables within that database.
+#' If you are running a local database with, you only need to define the name of the database you want to connect to.
+#'
+#' @template db-info
+#' @param dbname Database name
+#' @param host,port Host name and port number of database (defaults to localhost:50000)
+#' @param user,password User name and password (if needed)
+#' @param ... for the src, other arguments passed on to the underlying
+#'   database connector, \code{dbConnect}.
+#' @param src a MonetDB src created with \code{src_monetdb}.
+#' @param from Either a string giving the name of table in database, or
+#'   \code{\link{sql}} described a derived table or compound join.
+#' @export
+#' @examples
+#' \dontrun{
+#' # Connection basics ---------------------------------------------------------
+#' # To connect to a database first create a src:
+#' my_db <- src_monetdb(dbname="demo",host = "localhost", port=50000, user = "monetdb",
+#'   password = "monetdb")
+#' # Then reference a tbl within that src
+#' my_tbl <- tbl(my_db, "my_table")
+#' }
+#'
+#' # Here we'll use the Lahman database: to create your own local copy,
+#' # create a local database called "lahman" first.
+#'
+#' if (has_lahman("monetdb")) {
+#' # Methods -------------------------------------------------------------------
+#' batting <- tbl(lahman_monetdb(), "Batting")
+#' dim(batting)
+#' colnames(batting)
+#' head(batting)
+#'
+#' # Data manipulation verbs ---------------------------------------------------
+#' filter(batting, yearID > 2005, G > 130)
+#' select(batting, playerID:lgID)
+#' arrange(batting, playerID, desc(yearID))
+#' summarise(batting, G = mean(G), n = n())
+#' mutate(batting, rbi2 = if(is.null(AB)) 1.0 * R / AB else 0)
+#'
+#' # note that all operations are lazy: they don't do anything until you
+#' # request the data, either by `print()`ing it (which shows the first ten
+#' # rows), by looking at the `head()`, or `collect()` the results locally.
+#'
+#' system.time(recent <- filter(batting, yearID > 2010))
+#' system.time(collect(recent))
+#'
+#' # Group by operations -------------------------------------------------------
+#' # To perform operations by group, create a grouped object with group_by
+#' players <- group_by(batting, playerID)
+#' group_size(players)
+#' summarise(players, mean_g = mean(G), best_ab = max(AB))
+#'
+#' # When you group by multiple level, each summarise peels off one level
+#' per_year <- group_by(batting, playerID, yearID)
+#' stints <- summarise(per_year, stints = max(stint))
+#' filter(stints, stints > 3)
+#' summarise(stints, max(stints))
+#'
+#' # Joins ---------------------------------------------------------------------
+#' player_info <- select(tbl(lahman_monetdb(), "Master"), playerID, hofID,
+#'   birthYear)
+#' hof <- select(filter(tbl(lahman_monetdb(), "HallOfFame"), inducted == "Y"),
+#'  hofID, votedBy, category)
+#'
+#' # Match players and their hall of fame data
+#' inner_join(player_info, hof)
+#' # Keep all players, match hof data where available
+#' left_join(player_info, hof)
+#' # Find only players in hof
+#' semi_join(player_info, hof)
+#' # Find players not in hof
+#' anti_join(player_info, hof)
+#'
+#' # Arbitrary SQL -------------------------------------------------------------
+#' # You can also provide sql as is, using the sql function:
+#' batting2008 <- tbl(lahman_monetdb(),
+#'   sql('SELECT * FROM "Batting" WHERE "yearID" = 2008'))
+#' batting2008
+#' }
 src_monetdb <- function(dbname, host = "localhost", port = 50000L, user = "monetdb", 
   password = "monetdb", ...) {
   if (!require("MonetDB.R")) {
