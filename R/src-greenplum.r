5f67eeed84b83a233d1634aa07c41758223e386c
will0955 <will0955@gmail.com>
2014-03-19 12:29:09 -0500

    full list of functions

--- R/src-greenplum.r
+++ R/src-greenplum.r
@@ -128,10 +128,22 @@ over_greenplum <- function(expr, partition = NULL, order = NULL, frame = NULL) {
   build_sql(expr, " OVER ", over)
 }
 
+win_rank_greenplum <- function(f) {
+  force(f)
+  function(order = NULL) {
+    over_greenplum(build_sql(sql(f), list()), partition_group(), order %||% partition_order())
+  }
+}
 win_recycled_greenplum <- function(f) {
   force(f)
   function(x) {
     over_greenplum(build_sql(sql(f), list(x)), partition_group(), NULL, frame = NULL)
+  }
+}
+win_cumulative_greenplum <- function(f) {
+  force(f)
+  function(x) {
+    over_greenplum(build_sql(sql(f), list(x)), partition_group(), partition_order(), frame = c(-Inf, 0))
   }
 }
 
@@ -164,12 +176,66 @@ translate_env.src_greenplum <- function(x) {
       any = sql_prefix("bool_or"),
       paste = function(x, collapse) build_sql("string_agg(", x, collapse, ")")
     ),
-    sql_translator(.parent=base_win,
+    sql_translator(.parent = base_win,
+      # rank functions have a single order argument that overrides the default
+      row_number   = win_rank_greenplum("row_number"),
+      min_rank     = win_rank_greenplum("rank"),
+      rank         = win_rank_greenplum("rank"),
+      dense_rank   = win_rank_greenplum("dense_rank"),
+      percent_rank = win_rank_greenplum("percent_rank"),
+      cume_dist    = win_rank_greenplum("cume_dist"),
+      ntile        = function(order_by, n) {
+        over_greenplum(
+          build_sql("NTILE", list(n)),partition_group(),order_by %||% partition_order())
+        },
+                   
+      # Recycled aggregate fuctions take single argument, don't need order and
+      # include entire partition in frame.
       mean  = win_recycled_greenplum("avg"),
       sum   = win_recycled_greenplum("sum"),
       min   = win_recycled_greenplum("min"),
       max   = win_recycled_greenplum("max"),
-      n = function() over_greenplum(sql("COUNT(*)"), partition_group(), frame = NULL)     
+      n     = function() {
+        over_greenplum(sql("COUNT(*)"), partition_group(), frame = c(-Inf, Inf))
+        },
+                   
+      # Cumulative function are like recycled aggregates except that R names
+      # have cum prefix, order_by is inherited and frame goes from -Inf to 0.
+      cummean = win_cumulative_greenplum("mean"),
+      cumsum  = win_cumulative_greenplum("sum"),
+      cummin  = win_cumulative_greenplum("min"),
+      cummax  = win_cumulative_greenplum("max"),
+                   
+      # Finally there are a few miscellaenous functions that don't follow any
+      # particular pattern
+      nth = function(x, order = NULL) {
+        over_greenplum(build_sql("NTH_VALUE", list(x)), partition_group(), order %||% partition$order())
+        },
+      first = function(x, order = NULL) {
+        over_greenplum(sql("FIRST_VALUE()"), partition_group(), order %||% partition_order())
+        },
+      last = function(x, order = NULL) {
+        over_greenplum(sql("LAST_VALUE()"), partition_group(), order %||% partition_order())
+        },
+                   
+      lead = function(x, n = 1L, default = NA, order = NULL) {
+        over_greenplum(
+          build_sql("LEAD", list(x, n, default)),
+          partition_group(),
+          order %||% partition_order()
+          )
+        },
+      lag = function(x, n = 1L, default = NA, order = NULL) {
+        over_greenplum(
+          build_sql("LAG", list(x, n, default)),
+          partition_group(),
+          order %||% partition_order()
+          )
+        },
+                   
+      order_by = function(order_by, expr) {
+        over_greenplum(expr, partition_group(), order_by)
+      }                
     )
   )
 }
