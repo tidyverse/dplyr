1fac96c8740dfb32997c7a1ad70b373be232283d
hadley <h.wickham@gmail.com>
2014-03-25 13:32:05 -0500

    Flexible way to specify functions

--- R/colwise.R
+++ R/colwise.R
@@ -4,7 +4,7 @@
 # colwise(df, c("min", "mean", "max"), x)
 # colwise(df, c("min", "mean", "max"), x, y, z)
 # colwise(df, c("min", "mean", "max"), contains("date"))
-# colwise(df, c("min", "mean", "max"))
+# colwise(df, c("min", "mean", "max"))g
 # would be nice to supply additional ...
 
 # This function is the workhorse. Powers all the others.
@@ -59,3 +59,42 @@ make_function <- function(args, body, env = parent.frame()) {
 }
 
 empty_arg <- function() quote(expr = )
+
+# funs(mean, "mean", mean(., na.rm = TRUE))
+#
+# Character vector
+# Name of another function
+# Otherwise it's a dotted call
+funs <- function(..., env = parent.frame()) {
+  args <- dots(...)
+  names(args) <- names2(args)
+
+  funs <- lapply(args, make_fun, env = env)
+
+  missing_names <- names(funs) == ""
+  default_names <- vapply(args[missing_names], make_name, character(1))
+  names(funs)[missing_names] <- default_names
+
+  funs
+}
+
+make_fun <- function(x, env) {
+  if (is.name(x) || is.character(x)) {
+    get(as.character(x), envir = env, mode = "function")
+  } else if (is.call(x)) {
+    dot_fun(x, env)
+  } else {
+    stop("Unknown inputs")
+  }
+}
+make_name <- function(x, env) {
+  if (is.character(x)) {
+    x
+  } else if (is.name(x)) {
+    as.character(x)
+  } else if (is.call(x)) {
+    as.character(x[[1]])
+  } else {
+    stop("Unknown input:", class(x)[1])
+  }
+}
