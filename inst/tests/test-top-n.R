dfacc410f2e73b9c041021b5b2138aea446cf99d
leondutoit <leon@comoyo.com>
2014-04-02 18:31:20 +0200

    Simplify top_n test

--- inst/tests/test-top-n.R
+++ inst/tests/test-top-n.R
@@ -1,18 +1,7 @@
 context("top_n")
 
-test_data <- function() {
-  data.frame(
-    name = rep(
-      unlist(
-        strsplit("this,is,to,make,sure,that,top_n,works,the,rightway", ",")), 10),
-    values = 1:100)
-}
-
 test_that("top_n returns n rows", {
-  test_df <- test_data()
-  top_four <- test_df %.%
-    group_by(name) %.%
-    summarise(total = sum(values)) %.%
-    top_n(4)
+  test_df <- data.frame(x = 1:10, y = 11:20)
+  top_four <- test_df %.% top_n(4)
   expect_equal(dim(top_four), c(4, 2))
 })
