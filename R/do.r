691965a07ca18cc024d0edb32b20e6fb457e20df
hadley <h.wickham@gmail.com>
2014-04-09 16:31:38 -0400

    Move do methods into one file

--- R/do.r
+++ R/do.r
@@ -70,3 +70,100 @@ do <- function(.data, ...) UseMethod("do")
 do.NULL <- function(.data, ...) {
   NULL
 }
+
+# Data frames -----------------------------------------------------------------
+
+#' @export
+do.data.frame <- function(.data, ...) {
+  args <- dots(...)
+  named <- named_args(args)
+
+  env <- new.env(parent = parent.frame())
+  env$. <- .data
+
+  if (!named) {
+    out <- eval(args[[1]], envir = env)
+    if (!is.data.frame(out)) {
+      stop("Result must be a data frame", call. = FALSE)
+    }
+  } else {
+    out <- lapply(args, function(arg) list(eval(arg, envir = env)))
+    names(out) <- names(args)
+    attr(out, "row.names") <- .set_row_names(1L)
+    # Use tbl_df to ensure safe printing of list columns
+    class(out) <- c("tbl_df", "data.frame")
+  }
+
+  out
+}
+
+#' @export
+do.grouped_df <- function(.data, ..., env = parent.frame()) {
+  # Force computation of indices
+  if (is.null(attr(.data, "indices"))) {
+    .data <- grouped_df_impl(.data, attr(.data, "vars"),
+      attr(.data, "drop") %||% TRUE)
+  }
+
+  args <- dots(...)
+  named <- named_args(args)
+
+  labels <- attr(.data, "labels")
+  index <- attr(.data, "indices")
+
+  # Create new environment, inheriting from parent, with an active binding
+  # for . that resolves to the current subset. `_i` is found in environment
+  # of this function because of usual scoping rules.
+  env <- new.env(parent = parent.frame())
+  makeActiveBinding(".", function() {
+    .data[index[[`_j`]] + 1L, , drop = FALSE]
+  }, env)
+
+  n <- length(index)
+  m <- length(args)
+
+  out <- replicate(m, vector("list", n), simplify = FALSE)
+  p <- Progress(n * m, min_time = 5)
+
+  for (i in seq_len(m)) {
+    for (`_j` in seq_len(n)) {
+      out[[i]][`_j`] <- list(eval(args[[i]], env = env))
+      p$tick()$show()
+    }
+  }
+
+  if (!named) {
+    data_frame <- vapply(out[[1]], is.data.frame, logical(1))
+    if (any(!data_frame)) {
+      stop("Results are not data frames at positions: ",
+        paste(which(!data_frame), collapse = ", "), call. = FALSE)
+    }
+
+    rows <- vapply(out[[1]], nrow, numeric(1))
+    labels <- labels[rep(1:nrow(labels), rows), , drop = FALSE]
+    rownames(labels) <- NULL
+
+    out <- rbind_all(out[[1]])
+    grouped_df(cbind(labels, out), groups(.data))
+  } else {
+    # Each result should be stored in a list
+    labels[names(args)] <- out
+    grouped_df(labels, groups(.data))
+  }
+}
+
+# Utils ------------------------------------------------------------------------
+
+named_args <- function(args) {
+  # Arguments must either be all named or all unnamed.
+  named <- sum(names2(args) != "")
+  if (!(named == 0 || named == length(args))) {
+    stop("Arguments to do() must either be all named or all unnamed",
+      call. = FALSE)
+  }
+  if (named == 0 && length(args) > 1) {
+    stop("Can only supply single unnamed argument to do()", call. = FALSE)
+  }
+
+  named != 0
+}
--- R/tbl-data-frame.R
+++ R/tbl-data-frame.R
@@ -39,29 +39,6 @@ select.data.frame <- function(.data, ...) {
   vars <- select_vars(names(.data), ..., env = parent.frame())
   select_impl(.data, vars)
 }
-#' @export
-do.data.frame <- function(.data, ...) {
-  args <- dots(...)
-  named <- named_args(args)
-
-  env <- new.env(parent = parent.frame())
-  env$. <- .data
-
-  if (!named) {
-    out <- eval(args[[1]], envir = env)
-    if (!is.data.frame(out)) {
-      stop("Result must be a data frame", call. = FALSE)
-    }
-  } else {
-    out <- lapply(args, function(arg) list(eval(arg, envir = env)))
-    names(out) <- names(args)
-    attr(out, "row.names") <- .set_row_names(1L)
-    # Use tbl_df to ensure safe printing of list columns
-    class(out) <- c("tbl_df", "data.frame")
-  }
-
-  out
-}
 
 # Joins ------------------------------------------------------------------------
 
