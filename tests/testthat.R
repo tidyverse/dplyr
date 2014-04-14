14f4b090b27625c0d91c78611ee2c81512fb0d96
hadley <h.wickham@gmail.com>
2014-04-14 13:44:22 -0500

    Upgrade to modern testthat dir structure. Closes #379

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,10 +1,11 @@
 library("testthat")
 library("dplyr")
 
+# Ensure database creation done before tests
 library("Lahman")
 lahman_sqlite()
 if (identical(Sys.getenv("NOT_CRAN"), "true")) {
   lahman_postgres()  
 }
 
-test_package("dplyr")
+test_check("dplyr")
