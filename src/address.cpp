1b6cfb93fe2f52bdd84c307fd5da2733e993f3ee
Romain François <romain@r-enthusiasts.com>
2014-04-22 10:33:04 +0200

    cast to void* which pleases -Wformat

--- src/address.cpp
+++ src/address.cpp
@@ -3,7 +3,7 @@ using namespace Rcpp;
 
 const char* address(SEXP x){
   static char buffer[20] ;
-  snprintf( buffer, 20, "%p", x ) ;
+  snprintf( buffer, 20, "%p", reinterpret_cast<void*>(x) ) ;
   return (const char*)buffer ;  
 }
 
