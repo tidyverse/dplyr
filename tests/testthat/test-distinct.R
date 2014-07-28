0d6357f897264040f7731ece2135239129ea1eaa
hadley <h.wickham@gmail.com>
2014-07-28 12:19:50 -0500

    Implement distinct for ungrouped tbls

---
+++ tests/testthat/test-distinct.R
@@ -0,0 +1,20 @@
+context("Distinct")
+
+df <- data.frame(
+  x = c(1, 1, 1, 1),
+  y = c(1, 1, 2, 2),
+  z = c(1, 1, 2, 2)
+)
+tbls <- temp_load(c("df", "dt", "sqlite"), df)
+
+test_that("distinct equivalent to local unique", {
+  compare_tbls(tbls, function(x) x %>% distinct(), ref = unique(df))
+})
+
+test_that("distinct removes duplicates (data.table)", {
+  expect_equal(nrow(distinct(tbls$dt, x)), 1)
+})
+
+test_that("distinct removes duplicates (sql)", {
+  expect_error(nrow(distinct(tbls$sqlite, x)), "specified columns")
+})
