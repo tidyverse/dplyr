6f13dac9ebd9c86d565a5a5ecb34a936c5e7a350
hadley <h.wickham@gmail.com>
2014-09-04 14:52:18 -0500

    useDynLibs & don't need internals

--- R/utils.r
+++ R/utils.r
@@ -125,14 +125,17 @@ substitute_q <- function(x, env) {
   eval(call)
 }
 
+#' @useDynLib dplyr setattr_
 setattr <- function(object, attr, value) {
-  .Call("setattr", object, as.name(attr), value, PACKAGE = "dplyr")
+  .Call(setattr_, object, as.name(attr), value)
 }
 
+#' @useDynLib dplyr set_string_elt_
 set_string_elt <- function(vector, index, value) {
-  .Call("set_string_elt", vector, as.integer(index) - 1L, value, PACKAGE = "dplyr")
+  .Call(set_string_elt_, vector, as.integer(index) - 1L, value)
 }
 
+#' @useDynLib dplyr set_vector_elt_
 set_vector_elt <- function(vector, index, value) {
-  .Call("set_vector_elt", vector, as.integer(index) - 1L, value, PACKAGE = "dplyr")
+  .Call(set_vector_elt_, vector, as.integer(index) - 1L, value)
 }
--- src/set.c
+++ src/set.c
@@ -1,22 +1,17 @@
-#define USE_RINTERNALS
-
 #include <R.h>
-#include <Rinternals.h>
+#include <Rdefines.h>
 
-SEXP setattr(SEXP object, SEXP attr, SEXP value) {
+SEXP setattr_(SEXP object, SEXP attr, SEXP value) {
   Rf_setAttrib(object, attr, value);
   return R_NilValue;
 }
 
-SEXP set_string_elt(SEXP vector, SEXP index, SEXP value) {
+SEXP set_string_elt_(SEXP vector, SEXP index, SEXP value) {
   SET_STRING_ELT(vector, INTEGER(index)[0], STRING_ELT(value, 0));
   return R_NilValue;
 }
 
-
-SEXP set_vector_elt(SEXP vector, SEXP index, SEXP value) {
+SEXP set_vector_elt_(SEXP vector, SEXP index, SEXP value) {
   SET_VECTOR_ELT(vector, INTEGER(index)[0], value);
   return R_NilValue;
 }
-
-#undef USE_RINTERNALS
