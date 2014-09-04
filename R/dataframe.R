9455ba53a4c776b1f047350646fa4f3a8acff319
hadley <h.wickham@gmail.com>
2014-09-04 14:52:39 -0500

    Only one #

--- R/dataframe.R
+++ R/dataframe.R
@@ -1,62 +1,62 @@
-##' Make a Data Frame
-##'
-##' A trimmed down version of \code{\link{data.frame}} that:
-##'
-##' 1. Evaluates its arguments lazily and in order,
-##' 2. Foregoes the option of providing \code{row.names},
-##' 3. Foregoes column name checking (except that we confirm names are not empty),
-##' 4. Does not convert strings to factors.
-##'
-##' @param ... A set of named arguments.
-##' @examples
-##' a <- 1:5
-##' data_frame(x = a, y = x ^ 2, z = x + y, a = a + 1, b = a)
-##' @export
+#' Make a Data Frame
+#'
+#' A trimmed down version of \code{\link{data.frame}} that:
+#'
+#' 1. Evaluates its arguments lazily and in order,
+#' 2. Foregoes the option of providing \code{row.names},
+#' 3. Foregoes column name checking (except that we confirm names are not empty),
+#' 4. Does not convert strings to factors.
+#'
+#' @param ... A set of named arguments.
+#' @examples
+#' a <- 1:5
+#' data_frame(x = a, y = x ^ 2, z = x + y, a = a + 1, b = a)
+#' @export
 data_frame <- function(...) {
   data_frame_(lazy::lazy_dots(...))
 }
 
 data_frame_ <- function(dots) {
 
-  ## Get the dots used
+  # Get the dots used
   dots_nm <- names(dots)
   n <- length(dots)
 
-  ## Early escape for dots with no arguments
+  # Early escape for dots with no arguments
   if (n == 0) return(data.frame())
 
-  ## Construct the list output
+  # Construct the list output
   output <- vector("list", n)
   names(output) <- character(n)
 
-  ## Get reference to names
+  # Get reference to names
   output_nm <- names(output)
 
-  ## Check the names
+  # Check the names
   if (any(!nzchar(names(dots)))) {
     stop("All arguments to `data_frame` should be named")
   }
 
-  ## Fill the output
+  # Fill the output
   i <- 1L
   while (i <= n) {
 
-    ## Fill by reference
+    # Fill by reference
     set_vector_elt(output, i, lazy::lazy_eval(dots[[i]], output))
     set_string_elt(output_nm, i, dots_nm[[i]])
 
-    ## Update
+    # Update
     i <- i + 1L
   }
 
-  ## Validate column lengths
+  # Validate column lengths
   lengths <- vapply(output, NROW, integer(1))
   if (length(unique(lengths)) > 1) {
     stop("arguments imply differing number of rows: ",
          paste(lengths, collapse = ", "))
   }
 
-  ## Set attributes
+  # Set attributes
   setattr(output, "row.names", c(NA_integer_, -lengths[[1]]))
   setattr(output, "class", c("tbl_df", "tbl", "data.frame"))
 
