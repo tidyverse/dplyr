e64728800d5fc0ad914955b1febd543269b5a9eb
hadley <h.wickham@gmail.com>
2014-09-22 13:14:00 -0500

    Better subsetting for tbls. Closes #587. Closes #610

--- tests/testthat/test-tbl-df.r
+++ tests/testthat/test-tbl-df.r
@@ -4,3 +4,10 @@ test_that("grouped_df returns tbl_df if no groups", {
   df <- grouped_df(mtcars, list())
   expect_equal(class(df), c("tbl_df", "tbl", "data.frame"))
 })
+
+test_that("[ never drops", {
+  mtcars2 <- tbl_df(mtcars)
+  expect_is(mtcars2[, 1], "data.frame")
+  expect_is(mtcars2[, 1], "tbl_df")
+  expect_equal(mtcars2[, 1], mtcars2[1])
+})
