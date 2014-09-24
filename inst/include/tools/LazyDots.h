20feb1659adc6394a651c4bef754e854f4f120f6
hadley <h.wickham@gmail.com>
2014-09-24 15:42:21 -0500

    If no dots, names is NULL

--- inst/include/tools/LazyDots.h
+++ inst/include/tools/LazyDots.h
@@ -2,63 +2,65 @@
 #define dplyr__Lazy_h
 
 namespace Rcpp {
-    
+
     class Lazy {
     public:
-        Lazy( List data_, SEXP name_ ) : 
-            env(data_[1]), 
-            expr(data_[0]), 
+        Lazy( List data_, SEXP name_ ) :
+            env(data_[1]),
+            expr(data_[0]),
             name(name_)
         {}
-        
+
         Environment env ;
         SEXP expr ;
         SEXP name ;
     } ;
-    
+
     template <>
     inline bool is<Lazy>(SEXP x){
-        return TYPEOF(x) == VECSXP && 
-               Rf_length(x) == 2 && 
-               Rf_inherits(x, "lazy") &&   
-               TYPEOF(VECTOR_ELT(x,1)) == ENVSXP 
+        return TYPEOF(x) == VECSXP &&
+               Rf_length(x) == 2 &&
+               Rf_inherits(x, "lazy") &&
+               TYPEOF(VECTOR_ELT(x,1)) == ENVSXP
                ;
     }
-          
+
     class LazyDots {
     public:
         LazyDots( List data_ ) : data(){
-            CharacterVector names = data_.names() ;
             int n = data_.size() ;
+            if (n == 0) return;
+
+            CharacterVector names = data_.names() ;
             for(int i=0; i<n; i++){
                 SEXP x = data_[i] ;
                 if( !is<Lazy>(x) ){
-                    stop( "corrupt lazy object" );    
+                    stop( "corrupt lazy object" );
                 }
                 data.push_back( Lazy(data_[i], names[i]) ) ;
             }
         }
-        
+
         inline const Lazy& operator[](int i) const {
-            return data[i] ;    
+            return data[i] ;
         }
-        
+
         inline int size() const {
-            return data.size() ;    
+            return data.size() ;
         }
-        
+
         inline bool single_env() const {
             if( data.size() <= 1 ) return true ;
             SEXP env = data[0].env ;
             for( size_t i=1; i<data.size(); i++){
-                if( data[i].env != env ) return false ;    
+                if( data[i].env != env ) return false ;
             }
             return true ;
         }
-        
+
     private:
-        std::vector<Lazy> data ; 
+        std::vector<Lazy> data ;
     } ;
-    
-}    
+
+}
 #endif
