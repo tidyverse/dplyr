30f3a0e98681ca43473b4c2094c01f9f4c8c0fbc
hadley <h.wickham@gmail.com>
2014-09-25 08:10:05 -0500

    Make tests quieter

--- tests/testthat/helper-data.r
+++ tests/testthat/helper-data.r
@@ -1,12 +1,14 @@
-library(data.table)
-library(RSQLite)
+library(data.table, warn.conflicts = FALSE)
+library(RSQLite, quietly = TRUE)
+library(RSQLite.extfuns, quietly = TRUE)
+library(RPostgreSQL, quietly = TRUE)
 
 int_to_num <- function(x, y) {
   is_integer_x <- vapply(x, is.integer, logical(1))
   is_integer_y <- vapply(y, is.integer, logical(1))
-  
+
   x[is_integer_x] <- lapply(x[is_integer_x], as.numeric)
   y[is_integer_y] <- lapply(y[is_integer_y], as.numeric)
-  
+
   equal_data_frame(x, y)
 }
