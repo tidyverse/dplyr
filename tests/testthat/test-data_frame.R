655bdf8d4f1f958ab43e49480568fdf136d48a5c
Jim Hester <james.f.hester@gmail.com>
2014-10-10 14:21:55 -0400

    Set the row.names by the first non-short length
    Before this was always using the first length, which resulted in the
    data.frame being truncated if the first argument was a length 1 vector
    to be recycled.
    
    Fixes #680

---
+++ tests/testthat/test-data_frame.R
@@ -0,0 +1,8 @@
+Context("data_frame")
+test_that("data_frame returns correct number of rows", {
+  expect_equal(nrow(data_frame(value = 1:10)), 10L)
+
+  expect_equal(nrow(data_frame(value = 1:10, name = "recycle_me")), 10L)
+
+  expect_equal(nrow(data_frame(name = "recycle_me", value = 1:10)), 10L)
+})
