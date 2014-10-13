1fbe3f1618f32dd8806593cd53a022dea4095f50
Jim Hester <james.f.hester@gmail.com>
2014-10-12 20:47:28 -0400

    Extra whitespace
    hopefully travis will run correctly this time :-/

--- tests/testthat/test-data_frame.R
+++ tests/testthat/test-data_frame.R
@@ -1,5 +1,6 @@
 context("data_frame")
 test_that("data_frame returns correct number of rows with all combinatinos", {
+
   expect_equal(nrow(data_frame(value = 1:10)), 10L)
 
   expect_equal(nrow(data_frame(value = 1:10, name = "recycle_me")), 10L)
@@ -9,4 +10,5 @@ test_that("data_frame returns correct number of rows with all combinatinos", {
   expect_equal(nrow(data_frame(name = "recycle_me", value = 1:10, value2 = 11:20)), 10L)
 
   expect_equal(nrow(data_frame(value = 1:10, name = "recycle_me", value2 = 11:20)), 10L)
+
 })
