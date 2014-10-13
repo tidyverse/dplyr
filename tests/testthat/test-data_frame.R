8bdbc3d7647ea23ae78f2e75c0b22679b2d9556f
Jim Hester <james.f.hester@gmail.com>
2014-10-12 20:08:07 -0400

    Fix extraneous parenthesis.

--- tests/testthat/test-data_frame.R
+++ tests/testthat/test-data_frame.R
@@ -8,5 +8,5 @@ test_that("data_frame returns correct number of rows with all combinatinos", {
 
   expect_equal(nrow(data_frame(name = "recycle_me", value = 1:10, value2 = 11:20)), 10L)
 
-  expect_equal(nrow(data_frame(value = 1:10, name = "recycle_me"), value2 = 11:20)), 10L)
+  expect_equal(nrow(data_frame(value = 1:10, name = "recycle_me", value2 = 11:20)), 10L)
 })
