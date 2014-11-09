1af1c0d9ee1ecf8015cd880f729cba3ab0d81110
Romain François <romain@r-enthusiasts.com>
2014-11-09 10:53:01 +0100

    added small strings_addresses utility used for testing that we did not invade strings (internal use only)

---
+++ src/strings_addresses.cpp
@@ -0,0 +1,18 @@
+#include <Rcpp.h>
+using namespace Rcpp ;
+
+// [[Rcpp::export]]
+CharacterVector strings_addresses(CharacterVector s){
+    static char buffer[20] ;
+    int n = s.size() ;
+    
+    CharacterVector res(n) ;
+    for( int i=0; i<n; i++){
+        SEXP x = s[i] ;
+        snprintf( buffer, 20, "%p", reinterpret_cast<void*>(x) ) ;
+        res[i] = buffer ;
+    }
+    
+    return res ;
+}
+
