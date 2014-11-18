cf7d9ebd94df6cebedabd6bc7346d7361e307d36
hadley <h.wickham@gmail.com>
2014-11-18 16:59:08 -0600

    Better handle 0-colum tbl_df when subsetting
    This is a little tricky to support commands like mtcars %>% tbl_df() %>% `[`(0) %>% `[`(1:5, ), which should yield a 5 x 0 tbl_df.
    
    Closes #656

--- tests/testthat/test-tbl-df.r
+++ tests/testthat/test-tbl-df.r
@@ -11,3 +11,19 @@ test_that("[ never drops", {
   expect_is(mtcars2[, 1], "tbl_df")
   expect_equal(mtcars2[, 1], mtcars2[1])
 })
+
+test_that("[ with 0 cols creates correct row names (#656)", {
+  zero_row <- iris %>% tbl_df() %>% `[`(, 0)
+  expect_is(zero_row, "tbl_df")
+  expect_equal(nrow(zero_row), 150)
+  expect_equal(ncol(zero_row), 0)
+  expect_output(print(zero_row), "[150 x 0]", fixed = TRUE)
+})
+
+test_that("[ with 0 cols creates correct row names (#656)", {
+  zero_row <- iris %>% tbl_df() %>% `[`(0)
+  expect_is(zero_row, "tbl_df")
+  expect_equal(nrow(zero_row), 150)
+  expect_equal(ncol(zero_row), 0)
+  expect_output(print(zero_row), "[150 x 0]", fixed = TRUE)
+})
