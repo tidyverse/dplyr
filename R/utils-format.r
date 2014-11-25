e000c0cece947b3b0cab6501b74750dc34b2cf09
hadley <h.wickham@gmail.com>
2014-11-25 10:51:58 -0600

    Make extra variables bold in md.

--- R/utils-format.r
+++ R/utils-format.r
@@ -113,7 +113,7 @@ knit_print.trunc_mat <- function(x, options) {
 
   if (length(x$extra) > 0) {
     var_types <- paste0(names(x$extra), " (", x$extra, ")", collapse = ", ")
-    extra <- wrap("\nVariables not shown: ", var_types)
+    extra <- wrap("\n__Variables not shown__: ", var_types)
   } else {
     extra <- "\n"
   }
