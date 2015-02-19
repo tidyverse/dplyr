8dd434a6884b6576fdd6b743c9eb372983cc761b
Kasia Janocha <kasiajanocha@gmail.com>
2015-02-19 15:37:02 +0100

    correct test

--- tests/testthat/test-sweep_simultaneously.R
+++ tests/testthat/test-sweep_simultaneously.R
@@ -3,5 +3,5 @@ context("sweep_simultaneously")
 test_that("simple test with min", {
   df <- data.frame(a=c(1, 2), b=c(3, 4))
   swept.df <- sweep_simultaneously(df, 2, df[1,], min)
-  expect_equal(swept.df, data.frame(a=c(1, 2), b=c(1, 2))
+  expect_equal(swept.df, data.frame(a=c(1, 1), b=c(3, 3))
 })
