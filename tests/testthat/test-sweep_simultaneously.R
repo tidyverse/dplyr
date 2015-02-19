e26b445a4ec99379e81a5bbd3cf458fe0180234f
Kasia Janocha <kasiajanocha@gmail.com>
2015-02-19 15:06:26 +0100

    test

---
+++ tests/testthat/test-sweep_simultaneously.R
@@ -0,0 +1,7 @@
+context("sweep_simultaneously")
+
+test_that("simple test with min", {
+  df <- data.frame(a=c(1, 2), b=c(3, 4))
+  swept.df <- sweep_simultaneously(df, 2, df[1,], min)
+  expect_equal(swept.df, data.frame(a=c(1, 2), b=c(1, 2))
+})
