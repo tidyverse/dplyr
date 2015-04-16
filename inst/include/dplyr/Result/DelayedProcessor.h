c0b28a93dfbebee0a7e5f8aa17d2474ab773e132
hadley <h.wickham@gmail.com>
2015-04-16 13:43:10 -0500

    Fix a typo
    @romainfrancois can you please double check

--- inst/include/dplyr/Result/DelayedProcessor.h
+++ inst/include/dplyr/Result/DelayedProcessor.h
@@ -137,7 +137,7 @@ namespace dplyr{
         } else if( Rcpp::is<Rcpp::List>( first_result ) ){
             if( Rf_length(first_result) != 1 ) return 0 ;
             return new DelayedProcessor<VECSXP, CLASS, Data>(i) ;
-        } else if( Rf_length(first_result) == 1 && TYPEOF(x) == CPLXSXP ){
+        } else if( Rf_length(first_result) == 1 && TYPEOF(first_result) == CPLXSXP ){
             return new DelayedProcessor<CPLXSXP, CLASS, Data>(i) ;
         }
         return 0 ;
