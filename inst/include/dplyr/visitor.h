52d885d5f5e08f65a6849aa4aae626396699d1ee
Romain François <romain@r-enthusiasts.com>
2015-04-18 15:41:52 +0200

    revert previous commit until I can figure out what makes it fail

--- inst/include/dplyr/visitor.h
+++ inst/include/dplyr/visitor.h
@@ -44,9 +44,6 @@ namespace dplyr {
                     if( Rf_inherits( vec, "data.frame" ) ){
                         return new DataFrameColumnVisitor(vec) ;    
                     }
-                    if( Rf_inherits( vec, "POSIXlt" ) ){
-                        stop( "POSIXlt not supported" ) ;    
-                    }
                     return new VectorVisitorImpl<VECSXP>( vec ) ;
             }
             default: break ;
