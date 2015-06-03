d34d4ed4133ee9ee17e8cdef0d387204e8745193
Romain François <romain@r-enthusiasts.com>
2015-06-03 11:14:25 +0200

    need shallow_copy for the Rcppdevel branch

--- inst/include/dplyr/workarounds.h
+++ inst/include/dplyr/workarounds.h
@@ -4,7 +4,7 @@
 // Rcpp workaround - get rid of this once we have a released version 
 //                   greater than 0.11.6
 #if RCPP_DEV_VERSION > RcppDevVersion(0,11,6,0)
-    #define SHALLOW_COPY(__NAME__,__SOURCE__) DataFrame __NAME__(__SOURCE__)
+    #define SHALLOW_COPY(__NAME__,__SOURCE__) DataFrame __NAME__(shallow_copy(__SOURCE__))
 #else
     #define SHALLOW_COPY(__NAME__,__SOURCE__)                       \
         SEXP shallow_copied = PROTECT( shallow_copy(__SOURCE__) ) ; \
