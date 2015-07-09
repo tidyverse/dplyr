2795b1529ad52a91150bc4527f127f64a810619d
Romain François <romain@r-enthusiasts.com>
2015-07-09 11:15:05 +0200

    NAmedListAccumulator no longer uses Name, a step for the encoding stuff. #951

--- inst/include/dplyr/NamedListAccumulator.h
+++ inst/include/dplyr/NamedListAccumulator.h
@@ -3,45 +3,73 @@
 
 namespace dplyr {
 
+    struct Index {
+        enum Origin { HASH, RMATCH, NEW } origin ;
+        int index ;
+        
+        Index( int index_, Origin origin_ ) : index(index_), origin(origin_){}
+    } ;
+    
     template <typename Data>
     class NamedListAccumulator {
     public:
         
-        inline void set(Name name, SEXP x){
+        NamedListAccumulator() : 
+            data(), names(), r_match( "match" ), hash()
+        {}
+        
+        inline void set(SEXP name, SEXP x){
             if( ! Rcpp::traits::same_type<Data, RowwiseDataFrame>::value )
                 check_supported_type(x, name);
-                    
-            size_t n = size() ;
-            for( size_t i = 0; i<n; i++){
-                const Name& s = names[i] ;
-                if( s == name ){
-                    data[i] = x ;
-                    return ;
-                }
-            }
             
-            names.push_back(name) ;
-            data.push_back(x) ;
-            
+            Index index = get_index(name) ; 
+            int idx = index.index ;
+            switch( index.origin ){
+                case Index::HASH :
+                    data[idx] = x ;
+                    break ;
+                case Index::RMATCH :
+                    data[idx] = x ;
+                    hash.insert( std::make_pair( name, idx ) ) ;
+                    break ;
+                case Index::NEW :
+                    data.push_back(x) ;
+                    names.push_back(name) ;
+                    hash.insert( std::make_pair( name, idx ) ) ;
+                    break ; 
+            } ;
         }
         
-        inline void rm(Name name){
-            std::vector<Name>::iterator it = std::find( names.begin(), names.end(), name ) ;
-            if( it != names.end() ){
-                names.erase(it) ;
-                data.erase( data.begin() + std::distance( names.begin(), it ) );
+        inline void rm(SEXP name){   
+            Index index = get_index(name) ;
+            if( index.origin == Index::NEW ) return ;
+            
+            int idx = index.index ;
+            // update data and names
+            names.erase(names.begin() + idx) ;
+            data.erase(data.begin() + idx ) ;
+            
+            // update the hash table as well
+            for( dplyr_hash_map<SEXP,int>::iterator it=hash.begin(); it != hash.end(); ++it){
+                int k = it->second ;
+                // nothing to do in that case
+                if( k < idx ) continue ;
+                
+                if( k == idx ){
+                    // need to remove the data from the hash table
+                    it = hash.erase(it) ;
+                    continue ;
+                } else {
+                    // decrement the index
+                    it->second-- ;    
+                }
+                
             }
         }
         
         inline operator List() const {
-            int n = data.size() ;
-            List out(n) ;
-            CharacterVector out_names(n) ;
-            for( int i=0; i<n; i++){
-                out[i] = data[i] ;
-                out_names[i] = names[i].get() ;
-            }
-            out.attr( "names" ) = out_names ;
+            List out = wrap(data) ;
+            out.names() = names ;
             return out ;
         }
         
@@ -50,8 +78,39 @@ namespace dplyr {
         }
         
     private:
+        
         std::vector<SEXP> data ;
-        std::vector<Name> names ;
+        CharacterVector names ;
+        Function r_match ;
+        
+        dplyr_hash_map<SEXP,int> hash ;
+        
+        Index get_index( SEXP name ){
+            
+            // first, look in the hash table for exact match
+            dplyr_hash_map<SEXP,int>::iterator it = hash.find(name) ;
+            if( it != hash.end() ){
+                return Index( it->second, Index::HASH );                 
+            }
+            
+            // otherwise, don't give up just yet, name might still be 
+            // in there but with a different encoding
+            // 
+            // this is very expensive, but I don't know how else to do this stuff
+            // since C/RAPI does not give us the proper tools
+            CharacterVector y = CharacterVector::create(name) ;
+            int idx = as<int>( r_match(y, names ) ) ;
+            if( idx != NA_INTEGER ){
+                // great, we have a match. Let's update the data
+                return Index( idx-1, Index::RMATCH) ; 
+            }
+            
+            // no match 
+            return Index( data.size(), Index::NEW ) ;
+            
+        }
+        
+        
     } ;
 
 }
