c1dc8b144f7cea947d7f550e3de17774523364bc
Romain François <romain@r-enthusiasts.com>
2015-07-14 15:47:31 +0200

    be more careful about encoding of column names in bind_rows. closes #1265

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,7 @@
 # dplyr 0.4.2.9000
 
+* `bind_rows` is more careful about column names encodings (#1265). 
+
 * `mutate` better protects intermediary results (#1231). 
 
 * `mutate` and `arrange` works on empty data frames (#1142). 
--- tests/testthat/test-rbind.r
+++ tests/testthat/test-rbind.r
@@ -178,3 +178,12 @@ test_that( "bind_rows handles complex. #933", {
   expect_equal( nrow(df3), 4L)
   expect_equal( df3$r, c(df1$r, df2$r) )
 })
+
+test_that("bind_rows is careful about column names encoding #1265", {
+  one <- data.frame(fü=1:3, bar=1:3)
+  two <- data.frame(fü=1:3, bar=1:3)
+  Encoding(names(one)[1]) <- "UTF-8"
+  expect_equal( names(one), names(two))
+  res <- bind_rows(one,two)
+  expect_equal(ncol(res), 2L)  
+})
