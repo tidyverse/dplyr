2368c1a096e3c528af10092e2015578eea560348
Romain Francois <romain@r-enthusiasts.com>
2015-11-01 12:21:08 +0100

    equality test fails when convert is FALSE and types don't match. closes #1484

--- NEWS.md
+++ NEWS.md
@@ -1,6 +1,8 @@
 # dplyr 0.4.3.9000
 
-* `bind_rows` warns on binding factor and character (#1485). 
+* equality test fails when convert is FALSE and types don't match (#1484). 
+
+* `bind_rows` warns on binding factor and character (#1485).
 
 * `arrange()` fails gracefully on list columns (#1489).
 
--- tests/testthat/test-sets.R
+++ tests/testthat/test-sets.R
@@ -45,7 +45,6 @@ test_that("set operations use coercion rules (#799)", {
   expect_warning( { res <- intersect(df2, df1) })
   expect_equal( res, data_frame(x = letters[6:10]) )
 
-
   expect_warning( { res <- union(df1, df2) })
   expect_equal( res, data_frame(x = letters[1:15]) )
   expect_warning( { res <- union(df2, df1) })
