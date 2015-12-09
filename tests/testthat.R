93bef735ae5974143864f2f04303989aedc3c618
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-12-09 03:00:49 +0100

    fix testthat

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,11 +1,3 @@
 library("testthat")
-library("dplyr")
 
-# Ensure database creation done before tests
-library("Lahman")
-has_lahman("sqlite")
-if (identical(Sys.getenv("NOT_CRAN"), "true")) {
-  has_lahman("postgresql")
-}
-
-test_check("dplyr")
+test_check("tibble")
