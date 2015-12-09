d48653eaa46157b49c991087f208c34edf9d995b
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-12-09 10:49:18 +0100

    remove dplyr examples

--- R/tbl-df.r
+++ R/tbl-df.r
@@ -30,46 +30,6 @@
 #' dim(batting)
 #' colnames(batting)
 #' head(batting)
-#'
-#' # Data manipulation verbs ---------------------------------------------------
-#' filter(batting, yearID > 2005, G > 130)
-#' select(batting, playerID:lgID)
-#' arrange(batting, playerID, desc(yearID))
-#' summarise(batting, G = mean(G), n = n())
-#' mutate(batting, rbi2 = if(is.null(AB)) 1.0 * R / AB else 0)
-#'
-#' # Group by operations -------------------------------------------------------
-#' # To perform operations by group, create a grouped object with group_by
-#' players <- group_by(batting, playerID)
-#' head(group_size(players), 100)
-#'
-#' summarise(players, mean_g = mean(G), best_ab = max(AB))
-#' best_year <- filter(players, AB == max(AB) | G == max(G))
-#' progress <- mutate(players, cyear = yearID - min(yearID) + 1,
-#'  rank(desc(AB)), cumsum(AB))
-#'
-#' # When you group by multiple level, each summarise peels off one level
-#' \donttest{
-#' per_year <- group_by(batting, playerID, yearID)
-#' stints <- summarise(per_year, stints = max(stint))
-#' filter(stints, stints > 3)
-#' summarise(stints, max(stints))
-#' mutate(stints, cumsum(stints))
-#' }
-#'
-#' # Joins ---------------------------------------------------------------------
-#' player_info <- select(tbl_df(Master), playerID, birthYear)
-#' hof <- select(filter(tbl_df(HallOfFame), inducted == "Y"),
-#'  playerID, votedBy, category)
-#'
-#' # Match players and their hall of fame data
-#' inner_join(player_info, hof)
-#' # Keep all players, match hof data where available
-#' left_join(player_info, hof)
-#' # Find only players in hof
-#' semi_join(player_info, hof)
-#' # Find players not in hof
-#' anti_join(player_info, hof)
 #' }
 tbl_df <- function(data) {
   as_data_frame(data)
