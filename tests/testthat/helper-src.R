c1ff044ecd7c26740060633bbc7ddbccfd478acc
hadley <h.wickham@gmail.com>
2016-04-19 13:37:20 -0500

    New postgres seems to require explicit host

--- tests/testthat/helper-src.R
+++ tests/testthat/helper-src.R
@@ -2,7 +2,7 @@ test_register_src("df", src_df(env = new.env(parent = emptyenv())))
 test_register_src("sqlite", src_sqlite(":memory:", create = TRUE))
 
 if (identical(Sys.info()[["user"]], "hadley")) {
-  test_register_src("postgres", src_postgres("test"))
+  test_register_src("postgres", src_postgres("test", host = "localhost"))
 } else if (identical(Sys.getenv("TRAVIS"), "true")) {
   test_register_src("postgres", src_postgres("test", user = "travis", password = ""))
 }
