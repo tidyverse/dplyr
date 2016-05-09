tidyverse/dplyr

commit 31b1e2af82ea38f504b9f8bbefd94163177e4bd9

less detail of the error (for testing on travis)

src/api.cpp
@@ -69,12 +69,12 @@ namespace dplyr{
             name_right = names_right[i] ;
 
             if( indices_left[i] == NA_INTEGER ){
-              stop( "'%s' column not found in lhs, cannot join. (names: %s)", name_left, comma_collapse(RCPP_GET_NAMES(left)).get_cstring() ) ;
+              stop( "'%s' column not found in lhs, cannot join" ) ;
             }
             if( indices_right[i] == NA_INTEGER ){
-              stop( "'%s' column not found in rhs, cannot join. (names: %s)", name_right, comma_collapse(RCPP_GET_NAMES(right)).get_cstring() ) ;
+              stop( "'%s' column not found in rhs, cannot join" ) ;
             }
-            
+
             visitors[i] = join_visitor( left[indices_left[i]-1], right[indices_right[i]-1], name_left, name_right, warn ) ;
 
         }

commit 41a1746eefaa3549bb606b8a30cd6985abf12694

fix join error message
introduced in 31b1e2af82ea38f504b9f8bbefd94163177e4bd9

src/api.cpp
@@ -68,11 +68,12 @@ namespace dplyr{
             name_right = names_right[i] ;
 
             if( indices_left[i] == NA_INTEGER ){
-              stop( "'%s' column not found in lhs, cannot join" ) ;
+              stop( "'%s' column not found in lhs, cannot join", name_left ) ;
             }
             if( indices_right[i] == NA_INTEGER ){
-              stop( "'%s' column not found in rhs, cannot join" ) ;
+              stop( "'%s' column not found in rhs, cannot join", name_right ) ;
             }
+
             visitors[i] = join_visitor( left[indices_left[i]-1], right[indices_right[i]-1], name_left, name_right, warn ) ;
         }
     }