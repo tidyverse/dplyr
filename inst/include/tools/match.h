ed68b18088210ab2dd21c415e32f1f87af33f692
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-05-10 09:38:39 +0200

    lookup match() only once

--- inst/include/tools/match.h
+++ inst/include/tools/match.h
@@ -4,8 +4,20 @@
 
 namespace dplyr {
 
+class RMatch {
+public:
+  RMatch() : match_fun("match", R_BaseEnv) {}
+  IntegerVector operator()(SEXP x, SEXP y) {
+    return match_fun(x, y, NA_INTEGER, CharacterVector());
+  }
+
+private:
+  Function match_fun;
+};
+
 inline IntegerVector r_match( SEXP x, SEXP y ) {
-  return Language( "match", x, y, NA_INTEGER, CharacterVector() ).fast_eval(R_BaseEnv) ;
+  static RMatch m;
+  return m(x, y);
 }
 
 };
