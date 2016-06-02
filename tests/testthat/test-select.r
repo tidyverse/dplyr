6d70cc7bec0ff7a2c4eff1c1ff876ae19eda4818
Lionel Henry <lionel.hry@gmail.com>
2016-06-02 14:19:11 +0100

    Add select_if() to select based on predicate (#1880)
    * Add select_if() to select based on predicate
    
    This function is analogous to summarise_if() and mutate_if()
    
    Closes #497, closes #1569
    
    * Silently add grouping variables in select_if()

--- tests/testthat/test-select.r
+++ tests/testthat/test-select.r
@@ -165,3 +165,16 @@ test_that("select succeeds in presence of raw columns (#1803)", {
   expect_identical(select(df, b), df["b"])
   expect_identical(select(df, -b), df["a"])
 })
+
+test_that("select_if can use predicate", {
+  expect_identical(iris %>% select_if(is.factor), iris["Species"])
+})
+
+test_that("select_if fails with databases", {
+  expect_error(memdb_frame(x = 1) %>% select_if(is.numeric) %>% collect())
+})
+
+test_that("select_if keeps grouping cols", {
+  expect_silent(df <- iris %>% group_by(Species) %>% select_if(is.numeric))
+  expect_equal(df, tbl_df(iris[c(5, 1:4)]))
+})
