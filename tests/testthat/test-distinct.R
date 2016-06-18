afcf46941475a4cc50fca980a7be4adb96954199
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-06-18 21:56:20 +0200

    add failing test

--- tests/testthat/test-distinct.R
+++ tests/testthat/test-distinct.R
@@ -11,6 +11,11 @@ test_that("distinct equivalent to local unique when keep_all is TRUE", {
   compare_tbls(tbls, function(x) x %>% distinct(), ref = unique(df))
 })
 
+test_that("distinct for single column works as expected (#1937)", {
+  compare_tbls(tbls, function(x) x %>% distinct(x, .keep_all = FALSE), ref = df[1, "x", drop= FALSE])
+  compare_tbls(tbls, function(x) x %>% distinct(y, .keep_all = FALSE), ref = df[c(1, 3), "y", drop= FALSE])
+})
+
 test_that("distinct throws error if column is specified and .keep_all is TRUE", {
   skip_if_no_sqlite()
   expect_error(collect(distinct(tbls$sqlite, x, .keep_all = TRUE)),
