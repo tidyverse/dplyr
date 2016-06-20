3ed3449875649d563ea7066e8896e1f0d2e99757
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-06-20 18:41:39 +0200

    tests and current output

---
+++ tests/testthat/test-output.R
@@ -0,0 +1,26 @@
+context("output")
+
+test_that("ungrouped output", {
+  mtcars_mem <- src_memdb() %>% copy_to(mtcars, name = random_table_name())
+  iris_mem <- src_memdb() %>% copy_to(iris, name = random_table_name())
+
+  expect_output_file_rel(
+    print(mtcars_mem, n = 8L, width = 30L),
+    "mtcars-8-30.txt")
+
+  expect_output_file_rel(
+    print(iris_mem, n = 5L, width = 30L),
+    "iris-5-30.txt")
+
+  expect_output_file_rel(
+    print(iris_mem, n = 3L, width = 5L),
+    "iris-3-5.txt")
+
+  expect_output_file_rel(
+    print(iris_mem, n = NULL, width = 70L),
+    "iris--70.txt")
+
+  expect_output_file_rel(
+    print(iris_mem %>% head(), n = 30L, width = 80L),
+    "iris-head-30-80.txt")
+})
