c5a9af2995f60e753f066c2cd079017b728bdf7b
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-06-21 10:12:01 +0200

    restrict test to tibble version

--- tests/testthat/test-output.R
+++ tests/testthat/test-output.R
@@ -1,6 +1,9 @@
 context("output")
 
 test_that("ungrouped output", {
+  if (packageVersion("tibble") < "1.0-10")
+    skip("need tibble 1.0-10 or later for this test")
+
   mtcars_mem <- src_memdb() %>% copy_to(mtcars, name = random_table_name())
   iris_mem <- src_memdb() %>% copy_to(iris, name = random_table_name())
 
