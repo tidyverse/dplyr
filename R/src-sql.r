34da3a76dd3d65cb5163270ddc1e6cdd927feac2
Joe Cheng <joe@rstudio.com>
2016-07-11 16:53:55 -0700

    Add indirection for src$con
    This commit adds con_acquire and con_release interfaces
    which dplyr can use when it needs to get a connection from
    a src (instead of just src$con). This makes it possible to
    support connection pools properly; without this change,
    there is no way for dplyr to signal to a connection pool
    that it is done with a connection.

--- R/src-sql.r
+++ R/src-sql.r
@@ -11,18 +11,55 @@
 #' @param ... fields used by object
 src_sql <- function(subclass, con, ...) {
   subclass <- paste0("src_", subclass)
-  structure(list(con = con, ...), class = c(subclass, "src_sql", "src"))
+  structure(list(obj = con, ...), class = c(subclass, "src_sql", "src"))
 }
+
+#' Acquire/release connections from a src object
+#'
+#' \code{con_acquire} gets a connection from a src object; \code{con_release}
+#' returns a previously acquired connection back to its src object. Intended for
+#' internal use.
+#'
+#' These methods have default implementations for \code{src_sql} and can be
+#' overridden for src objects that are not themselves DB connections, but know
+#' how to get them (e.g. a connection pool).
+#'
+#' @keywords internal
+#' @export
+#' @param src A src object (most commonly, from \code{src_sql})
+#' @param con A connection
+#' @return For \code{con_acquire}, a connection object; for \code{con_release},
+#'   nothing.
+con_acquire <- function(src) {
+  UseMethod("con_acquire", src)
+}
+
+#' @rdname con_acquire
+#' @export
+con_release <- function(src, con) {
+  UseMethod("con_release", src)
+}
+
+con_acquire.src_sql <- function(src) {
+  src$obj
+}
+
+con_release.src_sql <- function(src, con) {
+}
+
 
 #' @export
 same_src.src_sql <- function(x, y) {
   if (!inherits(y, "src_sql")) return(FALSE)
-  identical(x$con, y$con)
+  identical(x$obj, y$obj)
 }
 
 #' @export
 src_tbls.src_sql <- function(x, ...) {
-  db_list_tables(x$con)
+  con <- con_acquire(x)
+  on.exit(con_release(x, con), add = TRUE)
+
+  db_list_tables(con)
 }
 
 #' @export
