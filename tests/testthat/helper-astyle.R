6c4b09b758be7354f418382ff8dba47a971f4b92
Kirill Müller <krlmlr+r@mailbox.org>
2016-08-31 22:58:17 +0200

    also prettify header files in inst

--- tests/testthat/helper-astyle.R
+++ tests/testthat/helper-astyle.R
@@ -16,8 +16,9 @@ astyle <- function(extra_args = character()) {
     "--align-reference=type"
   )
 
-  src_path <- normalizePath(testthat::test_path("../../src"))
-  astyle_files <- file.path(src_path, setdiff(dir(src_path, "[.]cpp$"), "RcppExports.cpp"))
+  src_path <- normalizePath(vcapply(c("../../src", "../../inst/include"), testthat::test_path))
+  src_files <- dir(src_path, "[.](?:cpp|h)$", recursive = TRUE, full.names = TRUE)
+  astyle_files <- grep("(?:RcppExports[.]cpp|static_assert[.]h)", src_files, value = TRUE, invert = TRUE)
   output <- system2(astyle_cmd, c(astyle_args, astyle_files, extra_args), stdout = TRUE, stderr = TRUE)
   unchanged <- grepl("^Unchanged", output)
   if (any(!unchanged)) {
