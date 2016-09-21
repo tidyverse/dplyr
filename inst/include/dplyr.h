4dc5d28b5cef760392f8d81735a3a372b9d57934
Kirill Müller <krlmlr+r@mailbox.org>
2016-09-22 00:30:21 +0200

    move to dplyr/dplyr.h

--- inst/include/dplyr.h
+++ inst/include/dplyr.h
@@ -15,52 +15,6 @@ using namespace Rcpp;
 
 #include <tools/tools.h>
 
-#include <dplyr/ForwardDeclarations.h>
-#include <dplyr/registration.h>
-#include <dplyr/DataFrameAble.h>
-#include <dplyr/CharacterVectorOrderer.h>
-#include <dplyr/white_list.h>
-#include <dplyr/check_supported_type.h>
-#include <dplyr/visitor_set/visitor_set.h>
-#include <dplyr/DataFrameVisitorsIndexSet.h>
-#include <dplyr/DataFrameVisitorsIndexMap.h>
-#include <dplyr/BoolResult.h>
-#include <dplyr/EmptySubset.h>
-#include <dplyr/FullDataFrame.h>
-#include <dplyr/GroupedDataFrame.h>
-#include <dplyr/RowwiseDataFrame.h>
-#include <dplyr/tbl_cpp.h>
-#include <dplyr/comparisons.h>
-#include <dplyr/comparisons_different.h>
-#include <dplyr/VectorVisitor.h>
-#include <dplyr/SubsetVectorVisitor.h>
-#include <dplyr/OrderVisitor.h>
-#include <dplyr/VectorVisitorImpl.h>
-#include <dplyr/SubsetVectorVisitorImpl.h>
-#include <dplyr/DataFrameVisitors.h>
-#include <dplyr/MultipleVectorVisitors.h>
-#include <dplyr/DataFrameSubsetVisitors.h>
-#include <dplyr/DataFrameColumnSubsetVisitor.h>
-#include <dplyr/MatrixColumnSubsetVectorVisitor.h>
-#include <dplyr/MatrixColumnVisitor.h>
-#include <dplyr/DataFrameColumnVisitor.h>
-#include <dplyr/subset_visitor.h>
-#include <dplyr/visitor.h>
-#include <dplyr/OrderVisitorImpl.h>
-#include <dplyr/JoinVisitor.h>
-#include <dplyr/JoinVisitorImpl.h>
-#include <dplyr/DataFrameJoinVisitors.h>
-#include <dplyr/Order.h>
-#include <dplyr/SummarisedVariable.h>
-#include <dplyr/Hybrid.h>
-#include <dplyr/Result/all.h>
-#include <dplyr/vector_class.h>
-#include <dplyr/Gatherer.h>
-#include <dplyr/Replicator.h>
-#include <dplyr/Collecter.h>
-#include <dplyr/NamedListAccumulator.h>
-#include <dplyr/train.h>
-#include <dplyr/Groups.h>
-
+#include <dplyr/dplyr.h>
 
 #endif
