553e278c910c2b9f1f50a134400d61d6737f6dea
Kirill Müller <krlmlr+r@mailbox.org>
2016-09-21 23:18:31 +0200

    move functions to modules

--- src/utils.cpp
+++ src/utils.cpp
@@ -37,3 +37,33 @@ SEXP shallow_copy(const List& data) {
   copy_attributes(out, data);
   return out;
 }
+
+SEXP pairlist_shallow_copy(SEXP p) {
+  Shield<SEXP> attr(Rf_cons(CAR(p), R_NilValue));
+  SEXP q = attr;
+  SET_TAG(q, TAG(p));
+  p = CDR(p);
+  while (!Rf_isNull(p)) {
+    Shield<SEXP> s(Rf_cons(CAR(p), R_NilValue));
+    SETCDR(q, s);
+    q = CDR(q);
+    SET_TAG(q, TAG(p));
+    p = CDR(p);
+  }
+  return attr;
+}
+
+void copy_attributes(SEXP out, SEXP data) {
+  SEXP att = ATTRIB(data);
+  if (!Rf_isNull(att)) {
+    SET_ATTRIB(out, pairlist_shallow_copy(ATTRIB(data)));
+  }
+  SET_OBJECT(out, OBJECT(data));
+  if (IS_S4_OBJECT(data)) SET_S4_OBJECT(out);
+}
+
+// same as copy_attributes but without names
+void copy_most_attributes(SEXP out, SEXP data) {
+  copy_attributes(out,data);
+  Rf_setAttrib(out, R_NamesSymbol, R_NilValue);
+}
