60c344ff3942b69c06f5e8c010f8744f430831db
Kirill Müller <krlmlr+r@mailbox.org>
2016-09-22 00:08:46 +0200

    move to hash.h

--- inst/include/dplyr.h
+++ inst/include/dplyr.h
@@ -24,24 +24,6 @@ using namespace Rcpp;
 #include <boost/shared_ptr.hpp>
 #include <boost/functional/hash.hpp>
 
-#ifndef dplyr_hash_map
-  #if defined(_WIN32)
-    #define dplyr_hash_map RCPP_UNORDERED_MAP
-  #else
-    #include <boost/unordered_map.hpp>
-    #define dplyr_hash_map boost::unordered_map
-  #endif
-#endif
-
-#ifndef dplyr_hash_set
-  #if defined(_WIN32)
-    #define dplyr_hash_set RCPP_UNORDERED_SET
-  #else
-    #include <boost/unordered_set.hpp>
-    #define dplyr_hash_set boost::unordered_set
-  #endif
-#endif
-
 #include <tools/tools.h>
 
 #include <dplyr/ForwardDeclarations.h>
