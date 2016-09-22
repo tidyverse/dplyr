72484574e7727b6b9c18bf4ca57cc3d18a5c39c0
Kirill Müller <krlmlr+r@mailbox.org>
2016-09-22 13:00:08 +0200

    get rid of forward declarations

--- inst/include/tools/utils.h
+++ inst/include/tools/utils.h
@@ -7,5 +7,7 @@ SEXP shallow_copy(const List& data);
 SEXP pairlist_shallow_copy(SEXP p);
 void copy_attributes(SEXP out, SEXP data);
 void copy_most_attributes(SEXP out, SEXP data);
+void strip_index(DataFrame x);
+bool same_levels(SEXP left, SEXP right);
 
 #endif // #ifndef dplyr_tools_utils_H
