088e05f0b31e64eba0261c4ecdabb7938ea81a98
Kirill Müller <krlmlr+r@mailbox.org>
2016-09-22 13:59:03 +0200

    remove redundant includes

--- inst/include/tools/tools.h
+++ inst/include/tools/tools.h
@@ -1,20 +1,12 @@
 #ifndef dplyr_tools_tools_H
 #define dplyr_tools_tools_H
 
-#include <tools/all_na.h>
 #include <tools/debug.h>
 #include <tools/hash.h>
-#include <tools/Encoding.h>
 #include <tools/match.h>
-#include <tools/SymbolMap.h>
 #include <tools/DotsOf.h>
-#include <tools/Call.h>
-#include <tools/delete_all.h>
 #include <tools/pointer_vector.h>
 #include <tools/collapse.h>
-#include <tools/SlicingIndex.h>
-#include <tools/ShrinkableVector.h>
-#include <tools/wrap_subset.h>
 #include <tools/LazyDots.h>
 #include <tools/utils.h>
 
