b51495536c46d9b83561d8670b4188da42730290
Kirill Müller <krlmlr+r@mailbox.org>
2016-09-22 13:02:27 +0200

    avoid inclusion of package header file in RcppExports.cpp

--- src/dplyr_types.h
+++ src/dplyr_types.h
@@ -1,6 +1,11 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
+#include <tools/DotsOf.h>
+#include <tools/LazyDots.h>
 #include <dplyr/registration.h>
 #include <dplyr/BoolResult.h>
 #include <dplyr/GroupedDataFrame.h>
+
+// avoid inclusion of package header file
+#define dplyr_dplyr_H
