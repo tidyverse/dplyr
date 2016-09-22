713991a68b67b7c258ed533ff2b112824985b9fb
Kirill Müller <krlmlr+r@mailbox.org>
2016-09-22 11:33:43 +0200

    remove redundant dplyr/visitor_set/visitor_set.h includes

--- inst/include/dplyr/visitor_set/visitor_set.h
+++ inst/include/dplyr/visitor_set/visitor_set.h
@@ -1,15 +1,6 @@
 #ifndef dplyr_visitor_set_H
 #define dplyr_visitor_set_H
 
-#include <dplyr/visitor_set/VisitorEqualPredicate.h>
-#include <dplyr/visitor_set/VisitorHash.h>
-
-#include <dplyr/visitor_set/VisitorSetMixin.h>
-
-#include <dplyr/visitor_set/VisitorSetHasher.h>
-
-#include <dplyr/visitor_set/VisitorSetEqualPredicate.h>
-
 #include <dplyr/visitor_set/VisitorSetIndexSet.h>
 #include <dplyr/visitor_set/VisitorSetIndexMap.h>
 
