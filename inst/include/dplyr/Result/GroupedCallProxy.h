e1f18027e25b5409329f2d6e73edc769abd5c3a8
Kirill Müller <krlmlr+r@mailbox.org>
2016-09-26 10:22:11 +0200

    CallProxy is now a specialization of GroupedCallProxy
    to get rid of the redundant implementation of traverse_call()

--- inst/include/dplyr/Result/GroupedCallProxy.h
+++ inst/include/dplyr/Result/GroupedCallProxy.h
@@ -38,6 +38,29 @@ namespace dplyr {
 
     ~GroupedCallProxy() {}
 
+    SEXP eval() {
+      if (TYPEOF(call) == LANGSXP) {
+
+        if (can_simplify(call)) {
+          SlicingIndex indices(0,subsets.nrows());
+          while (simplified(indices))
+            ;
+          set_call(call);
+        }
+
+        int n = proxies.size();
+        for (int i=0; i<n; i++) {
+          proxies[i].set(subsets[proxies[i].symbol]);
+        }
+        return call.eval(env);
+      } else if (TYPEOF(call) == SYMSXP) {
+        // SYMSXP
+        if (subsets.count(call)) return subsets.get_variable(call);
+        return call.eval(env);
+      }
+      return call;
+    }
+
     template <typename Container>
     SEXP get(const Container& indices) {
       subsets.clear();
@@ -103,6 +126,44 @@ namespace dplyr {
     }
 
   private:
+    bool simplified(const SlicingIndex& indices) {
+      // initial
+      if (TYPEOF(call) == LANGSXP) {
+        boost::scoped_ptr<Result> res(get_handler(call, subsets, env));
+
+        if (res) {
+          // replace the call by the result of process
+          call = res->process(indices);
+
+          // no need to go any further, we simplified the top level
+          return true;
+        }
+
+        return replace(CDR(call), indices);
+
+      }
+      return false;
+    }
+
+    bool replace(SEXP p, const SlicingIndex& indices) {
+      SEXP obj = CAR(p);
+
+      if (TYPEOF(obj) == LANGSXP) {
+        boost::scoped_ptr<Result> res(get_handler(obj, subsets, env));
+        if (res) {
+          SETCAR(p, res->process(indices));
+          return true;
+        }
+
+        if (replace(CDR(obj), indices)) return true;
+      }
+
+      if (TYPEOF(p) == LISTSXP) {
+        return replace(CDR(p), indices);
+      }
+
+      return false;
+    }
 
     void traverse_call(SEXP obj) {
       if (TYPEOF(obj) == LANGSXP && CAR(obj) == Rf_install("local")) return;
