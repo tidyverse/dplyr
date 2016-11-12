ff38827e062ace3361912fbaa2b385c31efb1236
Russell Pierce <russell.pierce@zapier.com>
2016-11-11 18:19:07 -0600

    Code Area Covered by expect_warning() Reduced

--- tests/testthat/test-group-by.r
+++ tests/testthat/test-group-by.r
@@ -271,14 +271,18 @@ test_that("grouped mutate coerces integer + double -> double (#1892)", {
 
 test_that("grouped mutate coerces factor + character -> character (WARN)", {
   skip("Currently failing")
-  expect_warning(
-    df <- data_frame(
-      id = c(1, 4),
-      value = factor(c("blue", NA)),
-      group = c("A", "B")
-    ) %>%
-    group_by(group) %>%
-    mutate(value = ifelse(id > 3, as.character("foo"), value))
+
+  df <- data_frame(
+    id = c(1, 4),
+    value = factor(c("blue", NA)),
+    group = c("A", "B")
   )
+
+  expect_warning(
+    df <- df %>%
+      group_by(group) %>%
+      mutate(value = ifelse(id > 3, as.character("foo"), value))
+  )
+
   expect_type(df$value, "character")
 })
