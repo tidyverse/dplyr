164dd28cda458a0a1668573e83611111e6a9370b
Tomasz Kalinowski <tkalinow@asu.edu>
2016-11-12 09:45:11 -0500

    oops

--- tests/testthat/test-nth-value.R
+++ tests/testthat/test-nth-value.R
@@ -34,27 +34,6 @@ test_that("first and last as part of compound expressions work within mutate (#2
   skip("Currently failing")
 
   df <-  data_frame(x = c(NA, 1L, 2L, NA, 3L, 4L, NA))
-
-  expected <- rep(1L, nrow(df))
-  expect_equal(mutate(df, y = first(na.omit(x)))$y,           expected)
-  expect_equal(mutate(df, y = first(x[!is.na(x)]))$y,         expected)
-  expect_equal(mutate(df, y = x %>% na.omit() %>% first())$y, expected)
-  expect_equal(mutate(df, y = x %>% na.omit %>% first)$y,     expected)
-
-  expected <- rep(4L, nrow(df))
-  expect_equal(mutate(df, y = last(na.omit(x)))$y,            expected)
-  expect_equal(mutate(df, y = last(x[!is.na(x)]))$y,          expected)
-  expect_equal(mutate(df, y = x %>% na.omit() %>% last())$y,  expected)
-  expect_equal(mutate(df, y = x %>% na.omit %>% last)$y,      expected)
-
-})
-
-
-test_that("last as part of compound expressions work within mutate (#2090)", {
-
-  skip("Currently failing")
-
-  df <-  data_frame(x = c(NA, 1L, 2L, NA, 3L, 4L, NA))
   expected_first <- rep(1L, nrow(df))
   expected_last  <- rep(4L, nrow(df))
 
@@ -69,6 +48,7 @@ test_that("last as part of compound expressions work within mutate (#2090)", {
   expect_equal(mutate(df, y = x %>% na.omit %>% last)$y,      expected_last)
 
 })
+
 
 
 
