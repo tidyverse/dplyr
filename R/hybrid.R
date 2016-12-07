26d452aae9f4e42c0108debcb11c22b2584b347d
Kirill Müller <krlmlr+r@mailbox.org>
2016-12-07 11:36:40 +0100

    special-case enclosing list() call

--- R/hybrid.R
+++ R/hybrid.R
@@ -12,7 +12,7 @@ with_hybrid <- function(expr, ...) {
 
 with_hybrid_ <- function(expr, ...) {
   stopifnot(any(class(expr) == "formula"))
-  expr[[2]] <- call("verify_hybrid", expr[[2]])
+  expr[[2]] <- prepend_call(expr[[2]], "verify_hybrid")
   data <- data_frame(...)
   summarise_(data, out = expr)["out"][[1]]
 }
@@ -24,7 +24,7 @@ without_hybrid <- function(expr, ...) {
 
 without_hybrid_ <- function(expr, ...) {
   stopifnot(any(class(expr) == "formula"))
-  expr[[2]] <- call("verify_not_hybrid", expr[[2]])
+  expr[[2]] <- prepend_call(expr[[2]], "verify_not_hybrid")
   data <- data_frame(...)
   summarise_(data, out = expr)["out"][[1]]
 }
@@ -36,4 +36,15 @@ eval_dots <- function(expr, ...) {
 eval_dots_ <- function(expr, ...) {
   data <- data_frame(...)
   eval(expr[[2]], data, enclos = environment(expr))
+}
+
+# some(func()) -> name(some(func()))
+# list(some(func())) -> list(name(some(func())))
+prepend_call <- function(expr, name) {
+  if (is.call(expr) && expr[[1]] == quote(list)) {
+    stopifnot(length(expr) == 2L)
+    call("list", call(name, expr[[2]]))
+  } else {
+    call(name, expr)
+  }
 }
