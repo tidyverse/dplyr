df3987eceda05b47c02cb04d3ef6d715944d88c6
Kirill Müller <krlmlr+r@mailbox.org>
2016-12-07 11:18:38 +0100

    test n() and n_distinct()

--- tests/testthat/test-hybrid.R
+++ tests/testthat/test-hybrid.R
@@ -8,6 +8,27 @@ test_df <- data_frame(
   d = c(TRUE, FALSE, NA)
 )
 
+test_that("n() and n_distinct() work", {
+  expect_hybrid(n(), a = 1:5, expected = 5L, test_eval = FALSE)
+
+  expect_hybrid(n_distinct(a), a = 1:5,
+                expected = 5L)
+  expect_hybrid(n_distinct(a), a = rep(1L, 3),
+                expected = 1L)
+  expect_hybrid(n_distinct(a, b), a = rep(1L, 3), b = 1:3,
+                expected = 3L)
+  expect_hybrid(n_distinct(a, b), a = rep(1L, 3), b = c(1, 1, 2),
+                expected = 2L)
+  expect_hybrid(n_distinct(a, b), a = rep(1L, 3), b = c(1, 1, NA),
+                expected = 2L)
+  expect_hybrid(n_distinct(a, b, na.rm = TRUE), a = rep(1L, 3), b = c(1, 1, NA),
+                expected = 1L)
+
+  c <- 1:3
+  expect_not_hybrid(n_distinct(c), a = 1:5,
+                    expected = 3L, test_eval = FALSE)
+})
+
 test_that("%in% works (#192)", {
   # Hybrid evaluation currently requires constants (#2143)
   test_df_mutated <-
