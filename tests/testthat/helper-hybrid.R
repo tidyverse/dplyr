e0bdfd336a5ae27eee79f24bdb267b1ac06d143f
Kirill Müller <krlmlr+r@mailbox.org>
2016-12-09 10:37:03 +0100

    rename

--- tests/testthat/helper-hybrid.R
+++ tests/testthat/helper-hybrid.R
@@ -1,19 +1,19 @@
-expect_hybrid <- function(expr, ..., expected, test_eval = TRUE) {
-  expect_hybrid_(lazyeval::f_capture(expr), ..., expected = expected, test_eval = test_eval)
+check_hybrid_result <- function(expr, ..., expected, test_eval = TRUE) {
+  check_hybrid_result_(lazyeval::f_capture(expr), ..., expected = expected, test_eval = test_eval)
 }
 
-expect_hybrid_ <- function(expr, ..., expected, test_eval) {
+check_hybrid_result_ <- function(expr, ..., expected, test_eval) {
   expect_identical(with_hybrid_(expr, ...), expected)
   if (test_eval) {
     expect_identical(eval_dots_(expr, ...), expected)
   }
 }
 
-expect_not_hybrid <- function(expr, ..., expected, test_eval = TRUE) {
-  expect_not_hybrid_(lazyeval::f_capture(expr), ..., expected = expected, test_eval = test_eval)
+check_not_hybrid_result <- function(expr, ..., expected, test_eval = TRUE) {
+  check_not_hybrid_result_(lazyeval::f_capture(expr), ..., expected = expected, test_eval = test_eval)
 }
 
-expect_not_hybrid_ <- function(expr, ..., expected, test_eval) {
+check_not_hybrid_result_ <- function(expr, ..., expected, test_eval) {
   expect_identical(without_hybrid_(expr, ...), expected)
   if (test_eval) {
     expect_identical(eval_dots_(expr, ...), expected)
