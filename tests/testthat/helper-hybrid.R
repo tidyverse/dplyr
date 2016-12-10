5ca650c4dae849033744f62e64b1c15c72ccd9fb
Kirill Müller <krlmlr+r@mailbox.org>
2016-12-10 19:35:59 +0100

    add expect_error()

--- tests/testthat/helper-hybrid.R
+++ tests/testthat/helper-hybrid.R
@@ -3,7 +3,7 @@ check_hybrid_result <- function(expr, ..., expected, test_eval = TRUE) {
 }
 
 check_hybrid_result_ <- function(expr, ..., expected, test_eval) {
-  expect_identical(with_hybrid_(expr, ...), expected)
+  expect_error(expect_identical(with_hybrid_(expr, ...), expected), NA)
   if (test_eval) {
     expect_identical(eval_dots_(expr, ...), expected)
   }
@@ -14,7 +14,7 @@ check_not_hybrid_result <- function(expr, ..., expected, test_eval = TRUE) {
 }
 
 check_not_hybrid_result_ <- function(expr, ..., expected, test_eval) {
-  expect_identical(without_hybrid_(expr, ...), expected)
+  expect_error(expect_identical(without_hybrid_(expr, ...), expected), NA)
   if (test_eval) {
     expect_identical(eval_dots_(expr, ...), expected)
   }
