dab1bf13616fb2b340d179fb94adbd74dee033a7
John Mount <jmount@win-vector.com>
2016-12-10 13:37:11 -0800

    fix spelling and formatting

--- tests/testthat/test-is-na.R
+++ tests/testthat/test-is-na.R
@@ -1,24 +1,26 @@
 context("is_na")
 
 
-test_that("is_na on databses as non-confusing IS NULL clause", {
+test_that("is_na on databases as non-confusing IS NULL clause", {
   skip_if_no_sqlite()
   db <- src_sqlite(":memory:", TRUE)
 
-  d <- data.frame(x=c(1,2,2),
-                  y=c(3,5,NA),
-                  z=c(NA,'a','b'),
-                  rowNum=1:3,
-                  stringsAsFactors = FALSE)
+  d <- data.frame(
+    x = c(1, 2, 2),
+    y = c(3, 5, NA),
+    z = c(NA, 'a', 'b'),
+    rowNum = 1:3,
+    stringsAsFactors = FALSE
+  )
 
   dfn1 <- copy_to(db, d , "dfn1")
 
-  dfn1 %>% mutate(nna=0) %>%
-    mutate(nna=nna+is.na(x)) %>%
-    mutate(nna=nna+is.na(y)) %>%
-    mutate(nna=nna+is.na(z)) %>%
+  dfn1 %>% mutate(nna = 0) %>%
+    mutate(nna = nna + is.na(x)) %>%
+    mutate(nna = nna + is.na(y)) %>%
+    mutate(nna = nna + is.na(z)) %>%
     arrange(rowNum) %>%
     collect() -> dres
 
-  expect_equal(dres$nna, c(1,0,1))
+  expect_equal(dres$nna, c(1, 0, 1))
 })
