3d8d94c06774fa9c65d56864aaee728220240b4d
Javier Luraschi <javierluraschi@hotmail.com>
2016-12-16 14:04:49 -0800

    add comment to sql_join return

--- R/sql-generic.R
+++ R/sql-generic.R
@@ -106,6 +106,7 @@ sql_join.default <- function(con, x, y, type = "inner", by = NULL, ...) {
     collapse = " AND ", parens = TRUE)
   cond <- build_sql("ON ", on, con = con)
 
+  # Wrap with SELECT since callers assume a valid query is returned
   build_sql(
     'SELECT * FROM ',x, "\n\n",
     join, " JOIN\n\n" ,
