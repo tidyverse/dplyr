c133557cfe5181c6dc875e6805faa526e175afc2
Jay Hesselberth <jay.hesselberth@gmail.com>
2016-12-30 14:51:20 -0700

    move copy_most_attributes to header (#2344)

--- inst/include/tools/utils.h
+++ inst/include/tools/utils.h
@@ -6,9 +6,13 @@ SEXP shared_SEXP(SEXP x);
 SEXP shallow_copy(const List& data);
 SEXP pairlist_shallow_copy(SEXP p);
 void copy_attributes(SEXP out, SEXP data);
-void copy_most_attributes(SEXP out, SEXP data);
 void strip_index(DataFrame x);
 bool same_levels(SEXP left, SEXP right);
 std::string get_single_class(SEXP x);
+
+// effectively the same as copy_attributes but without names and dims
+inline void copy_most_attributes(SEXP out, SEXP data) {
+  Rf_copyMostAttrib(data, out);
+}
 
 #endif // #ifndef dplyr_tools_utils_H
