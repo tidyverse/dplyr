60df32fa865ec6d7451e89ec306d3ea893426480
Dr. Connie Brett <cb4ds@users.noreply.github.com>
2017-01-27 11:11:48 -0500

    documentation fixes - #2152, #2205, #2218 (#2369)
    * fix #2218 documentation issue
    
    * fix #2218 documentation issue
    (changed from note to example)
    
    * fixed #2205 documentation issue
    
    * fixed #2152 documentation issue
    Added a test to show that #1175 was fixed so the section on Locales could be removed
    
    * Updates as per discussion with @krlmlr
    
    * changes per discussion - removed the test for #1175, added Locales section to arrange documentation

--- R/rank.R
+++ R/rank.R
@@ -39,6 +39,12 @@
 #'
 #' ntile(x, 2)
 #' ntile(runif(100), 10)
+#'
+#' # row_number can be used with single table verbs without specifying x
+#' # (for data frames and databases that support windowing)
+#' mutate(mtcars, row_number() == 1L)
+#' mtcars %>% filter(between(row_number(), 1, 10))
+#'
 NULL
 
 #' @export
