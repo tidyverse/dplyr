deb97a5fe5f003d16ee3255a104ef58500340db3
Garrett Grolemund <grolemund@gmail.com>
2017-02-16 13:26:19 -0500

    Reparses documentation to use @rdname, adds description of difference between band_instruments and band_instruments2

--- R/data-bands.R
+++ R/data-bands.R
@@ -4,10 +4,20 @@
 #' are toy data sets that can be displayed in their entirety on a slide (e.g. to
 #' demonstrate a join).
 #'
+#' `band_instruments` and `band_instruments2` contain the same data but use
+#' different column names for the first column of the data set.
+#' `band_instruments` uses `name`, which matches the name of the key column of
+#' `band_members`; `band_instruments2` uses `artist`, which does not.
+#'
 #' @format Each is a tibble with two variables and three observations
-#' @aliases band_members band_instruments band_instruments2
 #' @examples
 #' band_members
 #' band_instruments
 #' band_instruments2
 "band_members"
+
+#' @rdname band_members
+"band_instruments"
+
+#' @rdname band_members
+"band_instruments2"
