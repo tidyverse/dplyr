34a63b6326a2518a2f7ac1216a91c16279b4c607
Kirill Müller <krlmlr+r@mailbox.org>
2017-02-19 17:48:58 +0100

    check_length_one() forwards to check_length()

--- inst/include/dplyr/check_length_one.h
+++ inst/include/dplyr/check_length_one.h
@@ -1,12 +1,12 @@
 #ifndef dplyr_check_length_one_H
 #define dplyr_check_length_one_H
 
+#include <dplyr/check_length.h>
+
 namespace dplyr {
 
   void check_length_one(SEXP x) {
-    if (Rf_length(x) != 1) {
-      stop("expecting result of length one, got length %d", Rf_length(x));
-    }
+    check_length(Rf_length(x), 1, "a summary value");
   }
 
 }
