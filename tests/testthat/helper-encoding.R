80e09fe16dbe99fcd74fbf1e6b8fe287a68a68f1
Kirill Müller <krlmlr+r@mailbox.org>
2017-02-20 09:12:46 +0100

    refactor test

--- tests/testthat/helper-encoding.R
+++ tests/testthat/helper-encoding.R
@@ -1,6 +1,29 @@
-lang_strings <- c(
-  de = "Gl\u00fcck",
-  cn = "\u5e78\u798f",
-  ru = "\u0441\u0447\u0430\u0441\u0442\u044c\u0435",
-  ko = "\ud589\ubcf5"
-)
+get_lang_strings <- function() {
+  lang_strings <- c(
+    de = "Gl\u00fcck",
+    cn = "\u5e78\u798f",
+    ru = "\u0441\u0447\u0430\u0441\u0442\u044c\u0435",
+    ko = "\ud589\ubcf5"
+  )
+
+  native_lang_strings <- enc2native(lang_strings)
+
+  same <- (lang_strings == native_lang_strings)
+
+  list(
+    same = lang_strings[same],
+    different = lang_strings[!same]
+  )
+}
+
+get_native_lang_string <- function() {
+  lang_strings <- get_lang_strings()
+  if (length(lang_strings$same) == 0) testthat::skip("No native language string available")
+  lang_strings$same[[1L]]
+}
+
+get_alien_lang_string <- function() {
+  lang_strings <- get_lang_strings()
+  if (length(lang_strings$different) == 0) testthat::skip("No alien language string available")
+  lang_strings$different[[1L]]
+}
