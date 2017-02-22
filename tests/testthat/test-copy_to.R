1693ef4c1d2d3dcb111c168401c9efaf88423f45
hadley <h.wickham@gmail.com>
2017-02-22 06:50:47 -0600

    Weaken error test

--- tests/testthat/test-copy_to.R
+++ tests/testthat/test-copy_to.R
@@ -20,10 +20,8 @@ test_that("src_sql allows you to overwrite", {
   name <- random_table_name()
   copy_to(src_memdb(), tibble(x = 1), name = name)
 
-  expect_error(
-    copy_to(src_memdb(), tibble(x = 1), name = name),
-    "already exists"
-  )
+  # Can't check for specific error messages because will vary
+  expect_error(copy_to(src_memdb(), tibble(x = 1), name = name))
 
   df2 <- tibble(x = 2)
   copy_to(src_memdb(), df2, name = name, overwrite = TRUE)
