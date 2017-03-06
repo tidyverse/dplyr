91cc4cf1dc74d1d102412548def6b2d240b5870e
Kirill Müller <krlmlr+r@mailbox.org>
2017-03-06 21:13:36 +0100

    clean up hybrid handler for lead() and lag()

--- src/hybrid_offset.cpp
+++ src/hybrid_offset.cpp
@@ -12,39 +12,42 @@ using namespace dplyr;
 
 struct LeadLag {
 
-  LeadLag(SEXP call) : data(R_NilValue), n(1), def(R_NilValue), ok(true) {
+  LeadLag(SEXP call) : data(R_NilValue), n(1), def(R_NilValue), ok(false) {
 
     SEXP p = CDR(call);
     SEXP tag = TAG(p);
-    if (tag != R_NilValue && tag != Rf_install("x")) {
-      ok = false;
+    if (tag != R_NilValue && tag != Rf_install("x"))
       return;
-    }
     data = CAR(p);
-
     p = CDR(p);
-    while (p != R_NilValue) {
+
+    SEXP tag_default = Rf_install("default");
+    SEXP tag_n = Rf_install("n");
+    bool got_n = false;
+    bool got_default = false;
+
+    while (!Rf_isNull(p)) {
       tag = TAG(p);
-      if (tag != R_NilValue && tag != Rf_install("n") && tag != Rf_install("default")) {
-        ok = false;
+      if (!Rf_isNull(tag) && tag != tag_n && tag != tag_default)
         return;
+      if (!got_n && (Rf_isNull(tag) || tag == tag_n)) {
+        SEXP n_ = CAR(p);
+        if (TYPEOF(n_) != INTSXP && TYPEOF(n_) != REALSXP)
+          return;
+        n = as<int>(n_);
+        got_n = true;
       }
-      if (tag == Rf_install("n") || tag == R_NilValue) {
-        try {
-          n = as<int>(CAR(p));
-        } catch (...) {
-          SEXP n_ = CADDR(call);
-          std::stringstream s;
-          stop("could not convert second argument to an integer. type=%s, length = %d",
-               type2name(n_), Rf_length(n_));
-        }
-      }
-      if (tag == Rf_install("default")) {
+      else if (!got_default && (Rf_isNull(tag) || tag == tag_default)) {
+        if (TYPEOF(def) == LANGSXP) return;
         def = CAR(p);
-        if (TYPEOF(def) == LANGSXP) ok = false;
+        got_default = true;
       }
+      else
+        return;
       p = CDR(p);
     }
+
+    ok = true;
   }
 
   RObject data;
@@ -65,25 +68,27 @@ Result* leadlag_prototype(SEXP call, const ILazySubsets& subsets, int nargs) {
     return 0;
 
   SymbolString name = SymbolString(Symbol(data));
-  if (subsets.count(name)) {
-    bool is_summary = subsets.is_summary(name);
-    int n = args.n;
-    data = subsets.get_variable(name);
+  if (subsets.count(name) == 0)
+    return 0;
 
-    switch (TYPEOF(data)) {
-    case INTSXP:
-      return new Templ<INTSXP>(data, n, args.def, is_summary);
-    case REALSXP:
-      return new Templ<REALSXP>(data, n, args.def, is_summary);
-    case STRSXP:
-      return new Templ<STRSXP>(data, n, args.def, is_summary);
-    case LGLSXP:
-      return new Templ<LGLSXP>(data, n, args.def, is_summary);
-    default:
-      break;
-    }
+  bool is_summary = subsets.is_summary(name);
+  int n = args.n;
+  data = subsets.get_variable(name);
+
+  switch (TYPEOF(data)) {
+  case INTSXP:
+    return new Templ<INTSXP>(data, n, args.def, is_summary);
+  case REALSXP:
+    return new Templ<REALSXP>(data, n, args.def, is_summary);
+  case CPLXSXP:
+    return new Templ<CPLXSXP>(data, n, args.def, is_summary);
+  case STRSXP:
+    return new Templ<STRSXP>(data, n, args.def, is_summary);
+  case LGLSXP:
+    return new Templ<LGLSXP>(data, n, args.def, is_summary);
+  default:
+    return 0;
   }
-  return 0;
 }
 
 void install_offset_handlers(HybridHandlerMap& handlers) {
