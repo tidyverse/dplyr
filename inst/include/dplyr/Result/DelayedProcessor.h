b629260d3e131741f499c0370ca5c2188d16510c
Kirill Müller <krlmlr@users.noreply.github.com>
2017-03-09 08:19:20 +0100

    Duplicate summary values of type list (#2513)
    * add failing test
    
    * always duplicate returned list
    
    to retain all attributes such as names
    
    * don't need old Rcpp anymore

--- inst/include/dplyr/Result/DelayedProcessor.h
+++ inst/include/dplyr/Result/DelayedProcessor.h
@@ -233,7 +233,7 @@ namespace dplyr {
 
     virtual bool try_handle(const RObject& chunk) {
       if (is<List>(chunk) && Rf_length(chunk) == 1) {
-        res[pos++] = maybe_copy(VECTOR_ELT(chunk, 0));
+        res[pos++] = Rf_duplicate(VECTOR_ELT(chunk, 0));
         return true;
       }
       return false;
@@ -254,10 +254,6 @@ namespace dplyr {
   private:
     List res;
     int pos;
-
-    inline SEXP maybe_copy(SEXP x) const {
-      return is_ShrinkableVector(x) ? Rf_duplicate(x) : x;
-    }
   };
 
   template <typename CLASS>
