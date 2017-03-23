da6e170ae3d35b15b456cfae8cb014ea767aa859
Lionel Henry <lionel.hry@gmail.com>
2017-03-23 15:14:08 +0100

    Fix if-else style

--- R/funs-predicates.R
+++ R/funs-predicates.R
@@ -8,7 +8,7 @@ any_of <- function(..., .vectorised = TRUE) {
   quo_reduce(..., .op = op)
 }
 
-## @rdname .op Can be a function or a quoted name of a function. If a
+## @param .op Can be a function or a quoted name of a function. If a
 ##   quoted name, the default environment is the [base
 ##   environment][rlang::base_env] unless you supply a
 ##   [quosure][rlang::quosure].
@@ -16,10 +16,10 @@ quo_reduce <- function(..., .op) {
   stopifnot(is_symbol(.op) || is_function(.op))
 
   dots <- dots_quosures(...)
-  if (length(dots) == 1) {
+  if (length(dots) == 0) {
+    abort("There should be at least one expression")
+  } else if (length(dots) == 1) {
     return(dots[[1]])
-  } else if (!length(dots)) {
-    abort("There should be at least one predicate expression")
   }
 
   op_quo <- as_quosure(.op, base_env())
