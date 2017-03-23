5751330cf217cea257694edbd3a5e93f45ec719a
Lionel Henry <lionel.hry@gmail.com>
2017-03-23 19:53:52 +0100

    Fix tests to account for quosure class

--- tests/testthat/test-funs-predicates.R
+++ tests/testthat/test-funs-predicates.R
@@ -1,18 +1,16 @@
 context("funs-predicates")
 
 test_that("all_of() creates intersection", {
-  expect_identical(all_of(am == 1), ~am == 1)
+  expect_identical(all_of(am == 1), quo(am == 1))
 
-  quo <- quo((!! ~cyl == 2) & (!! ~am == 1))
-  f_env(quo) <- base_env()
+  quo <- set_env(quo((!! ~cyl == 2) & (!! ~am == 1)), base_env())
   expect_identical(all_of(cyl == 2, am == 1), quo)
 })
 
 test_that("any_of() creates union", {
-  expect_identical(any_of(am == 1), ~am == 1)
+  expect_identical(any_of(am == 1), quo(am == 1))
 
-  quo <- quo((!! ~cyl == 2) | (!! ~am == 1))
-  f_env(quo) <- base_env()
+  quo <- set_env(quo((!! ~cyl == 2) | (!! ~am == 1)), base_env())
   expect_identical(any_of(cyl == 2, am == 1), quo)
 })
 
