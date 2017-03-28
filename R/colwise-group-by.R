9c84cc29da17b610f1b43bb39ac295f21051ce4e
Lionel Henry <lionel.hry@gmail.com>
2017-03-29 01:32:14 +0200

    Allow optional mutate operation in scoped group_by() variants

--- R/colwise-group-by.R
+++ R/colwise-group-by.R
@@ -13,19 +13,33 @@
 #'
 #' # Group by variables selected by name:
 #' group_by_at(mtcars, vars(vs, am))
-group_by_all <- function(.tbl) {
-  syms <- syms(tbl_vars(.tbl))
-  group_by(.tbl, !!! syms(syms))
+#'
+#' # Like group_by(), the scoped variants have optional mutate
+#' # semantics. This provide a shortcut for group_by() + mutate():
+#' group_by_all(mtcars, as.factor)
+#' group_by_if(iris, is.factor, as.character)
+group_by_all <- function(.tbl, .funs = list(), ...) {
+  funs <- manip_all(.tbl, .funs, enquo(.funs), caller_env(), ...)
+  if (!length(funs)) {
+    funs <- syms(tbl_vars(.tbl))
+  }
+  group_by(.tbl, !!! funs)
 }
 #' @rdname group_by_all
 #' @export
-group_by_at <- function(.tbl, .vars, .add = FALSE) {
-  syms <- tbl_at_syms(.tbl, .vars)
-  group_by(.tbl, !!! syms, add = .add)
+group_by_at <- function(.tbl, .vars, .funs = list(), ..., .add = FALSE) {
+  funs <- manip_at(.tbl, .vars, .funs, enquo(.funs), caller_env(), ...)
+  if (!length(funs)) {
+    funs <- tbl_at_syms(.tbl, .vars)
+  }
+  group_by(.tbl, !!! funs, add = .add)
 }
 #' @rdname group_by_all
 #' @export
-group_by_if <- function(.tbl, .predicate, .add = FALSE) {
-  syms <- tbl_if_syms(.tbl, .predicate)
-  group_by(.tbl, !!! syms, add = .add)
+group_by_if <- function(.tbl, .predicate, .funs = list(), ..., .add = FALSE) {
+  funs <- manip_if(.tbl, .predicate, .funs, enquo(.funs), caller_env(), ...)
+  if (!length(funs)) {
+    funs <- tbl_if_syms(.tbl, .predicate)
+  }
+  group_by(.tbl, !!! funs, add = .add)
 }
