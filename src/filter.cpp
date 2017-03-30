040f54bd85d8b466f69a7f8d6b29272c7e5398be
Kirill Müller <krlmlr+r@mailbox.org>
2017-03-30 16:14:41 +0200

    move code

--- src/filter.cpp
+++ src/filter.cpp
@@ -15,14 +15,6 @@ using namespace Rcpp;
 using namespace dplyr;
 
 inline
-void strip_index(DataFrame x) {
-  x.attr("indices") = R_NilValue;
-  x.attr("group_sizes") = R_NilValue;
-  x.attr("biggest_group_size") = R_NilValue;
-  x.attr("labels") = R_NilValue;
-}
-
-inline
 SEXP empty_subset(const DataFrame& df, const CharacterVector& classes) {
   DataFrame res = DataFrameSubsetVisitors(df).subset(EmptySubset(), classes);
   strip_index(res);
