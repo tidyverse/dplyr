5bd2a0feffef837de15ec9b04276a374742f23c3
Kirill Müller <krlmlr@users.noreply.github.com>
2017-04-11 18:04:32 +0200

    New interface registration, and Rcpp::interfaces (#2626)
    * use Rcpp::interfaces() instead of GRAB_CALLABLE
    
    * don't export registerHybridHandler() for now
    
    * get rid of unused init function
    
    * use getter function to obtain address of is_bind_spliceable()
    
    @lionel-: I've had major trouble to make this work with new-style registration. I'm resorting to a manual workaround for now, but I'm not at all sure we should abuse registration to pass around function pointers with nonstandard signatures.
    
    * registration of native symbols
    
    * ignore autogenerated file
    
    * use set_attrs() instead of struct()
    
    * pass naked xptr
    
    * clarify reason for wrapping
    
    * Revert "pass naked xptr"
    
    This reverts commit 29f2f43c23f342e8602df155f018c85b7f792466.
    
    * adapt rlang ref
    
    * Use rlang's function pointers for flatten_if() plugin
    
    * Temporarily switch to rlang_squash_if()
    
    This commit should be reverted after the release
    
    * tweaks

--- src/init.cpp
+++ src/init.cpp
@@ -2,11 +2,10 @@
 #include <dplyr/main.h>
 
 #include <dplyr/registration.h>
+#include <dplyr/HybridHandler.h>
 
 using namespace Rcpp;
 using namespace dplyr;
-
-#define DPLYR_REGISTER(__FUN__) do { R_RegisterCCallable( "dplyr", #__FUN__, (DL_FUNC)__FUN__ ); } while(0);
 
 SEXP get_cache() {
   static SEXP cache = 0;
@@ -23,18 +22,19 @@ SEXP get_cache() {
   return cache;
 }
 
+// [[Rcpp::interfaces(cpp)]]
+// [[Rcpp::export]]
 SEXP get_date_classes() {
   return VECTOR_ELT(get_cache(), 0);
 }
+
+// [[Rcpp::interfaces(cpp)]]
+// [[Rcpp::export]]
 SEXP get_time_classes() {
   return VECTOR_ELT(get_cache(), 1);
 }
 
-extern "C" void R_init_dplyr(BOOST_ATTRIBUTE_UNUSED DllInfo* info) {
-  DPLYR_REGISTER(build_index_cpp);
-  DPLYR_REGISTER(registerHybridHandler);
-
-  DPLYR_REGISTER(get_time_classes);
-  DPLYR_REGISTER(get_date_classes);
-}
-
+// work around a problem (?) in Rcpp
+// [[Rcpp::interfaces(cpp)]]
+// [[Rcpp::export]]
+DataFrame build_index_cpp(DataFrame data);
