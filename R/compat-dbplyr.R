38bf8e53e93358a43f8280fb42fb64897e338b91
hadley <h.wickham@gmail.com>
2017-04-11 15:19:25 -0500

    Generate code instead of functions

--- R/compat-dbplyr.R
+++ R/compat-dbplyr.R
@@ -5,34 +5,16 @@
 #' dbplyr. The generic functions stayed in dplyr (since there is no easy way
 #' to conditionally import a generic from different packages), but many other
 #' SQL and database helper functions moved. If you have written a backend,
-#' these functions make it easier for your backend to work with both dplyr 0.5.0
+#' these functions generate the code you need to work with both dplyr 0.5.0
 #' dplyr 0.6.0.
-#'
-#' Use `check_dbplyr()` if you need to give an informative message if dbplyr
-#' is not available. Use `dbplyr_fun()` if you need a function from dbplyr;
-#' use `dbplyr_obj()` if you need an object.
 #'
 #' @keywords internal
 #' @export
 #' @examples
 #' if (requireNamespace("dbplyr", quietly = TRUE)) {
 #'
-#' # Functions -------------------------------------------------------
-#' # If you need to access a function, you can call it directly
-#' dbplyr_fun("build_sql")("min(", 1, ")")
-#'
-#' # If you use it many times, save it to a local variable
-#' build_sql <- dbplyr_fun("build_sql")
-#' src_sql <- dbplyr_fun("src_sql")
-#'
-#' # Objects ---------------------------------------------------------
-#' # If you need to access an object like base_agg, you'll need
-#' # dbplyr_obj()
-#' dbplyr_obj("base_agg")
-#'
-#' # Do NOT save this to a variable inside your package as it will
-#' # cache the value at build time, not run time, causing bugs that
-#' # extremely hard to track down.
+#' wrap_dbplyr_obj("build_sql")
+#' wrap_dbplyr_obj("base_agg")
 #' }
 check_dbplyr <- function() {
   check_pkg("dbplyr", "communicate with database backends")
@@ -40,34 +22,35 @@ check_dbplyr <- function() {
 
 #' @export
 #' @rdname check_dbplyr
-dbplyr_fun <- function(fun_name) {
-  args <- formals(getExportedValue("dbplyr", fun_name))
-  pass_on <- map(set_names(names(args)), as_symbol)
-  fun <- as_symbol(fun_name)
+wrap_dbplyr_obj <- function(obj_name) {
+  obj <- getExportedValue("dbplyr", obj_name)
+  obj_sym <- as_symbol(obj_name)
 
-  pkg_fun <- function(pkg, fun) {
-    new_language("::", as_symbol(pkg), as_symbol(fun))
+  dbplyr_sym <- new_language("::", expr(dbplyr), obj_sym)
+  dplyr_sym <- new_language("::", expr(dplyr), obj_sym)
+
+  if (is.function(obj)) {
+    args <- formals()
+    pass_on <- map(set_names(names(args)), as_symbol)
+
+    dbplyr_call <- expr(UQ(dbplyr_sym)(!!!pass_on))
+    dplyr_call <- expr(UQ(dbplyr_sym)(!!!pass_on))
+  } else {
+    args <- list()
+
+    dbplyr_call <- dbplyr_sym
+    dplyr_call <- dplyr_sym
   }
 
   body <- expr({
     if (utils::packageVersion("dplyr") > "0.5.0") {
       dplyr::check_dbplyr()
-      UQ(pkg_fun("dbplyr", fun))(!!!pass_on)
+      UQ(dbplyr_call)
     } else {
-      UQ(pkg_fun("dplyr", fun))(!!!pass_on)
+      UQ(dplyr_call)
     }
   })
+  wrapper <- new_function(args, body, caller_env())
 
-  new_function(args, body, caller_env())
-}
-
-#' @export
-#' @rdname check_dbplyr
-dbplyr_obj <- function(obj_name) {
-  if (utils::packageVersion("dplyr") > "0.5.0") {
-    dplyr::check_dbplyr()
-    getExportedValue("dbplyr", obj_name)
-  } else {
-    getExportedValue("dplyr", obj_name)
-  }
+  expr(UQ(obj_sym) <- UQE(wrapper))
 }
