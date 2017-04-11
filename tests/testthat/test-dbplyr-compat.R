f8130fea609bc09c0413b59270d7a068e889da79
hadley <h.wickham@gmail.com>
2017-04-11 16:10:02 -0500

    Eliminate no longer relevant test

--- tests/testthat/test-dbplyr-compat.R
+++
@@ -1,6 +0,0 @@
-context("dbplyr-compat")
-
-test_that("compat methods work with latest dplyr", {
-  expect_type(dbplyr_obj("base_agg"), "environment")
-  expect_s3_class(dbplyr_fun("build_sql")("x"), "sql")
-})
