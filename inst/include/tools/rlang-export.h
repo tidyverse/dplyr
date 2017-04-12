fd57942c54dcb3ff083dc596cc3b94f46866c733
Lionel Henry <lionel.hry@gmail.com>
2017-04-12 10:36:38 +0200

    Update rlang-export
    Closes #2633

--- inst/include/tools/rlang-export.h
+++ inst/include/tools/rlang-export.h
@@ -3,15 +3,16 @@
 
 #define R_NO_REMAP
 #include <Rinternals.h>
+#include <Rversion.h>
+#include <R_ext/Rdynload.h>
 
-typedef void* (*DL_FUNC)();
-typedef union {
-  void* p;
-  DL_FUNC fn;
-} fn_ptr;
 
-SEXP rlang_MakeExternalPtrFn(DL_FUNC p, SEXP tag, SEXP prot);
-DL_FUNC rlang_ExternalPtrAddrFn(SEXP s);
+#if (defined(R_VERSION) && R_VERSION < R_Version(3, 4, 0))
+typedef union {void* p; DL_FUNC fn;} fn_ptr;
+SEXP R_MakeExternalPtrFn(DL_FUNC p, SEXP tag, SEXP prot);
+DL_FUNC R_ExternalPtrAddrFn(SEXP s);
+#endif
+
 void rlang_register_pointer(const char* ns, const char* ptr_name, DL_FUNC fn);
 
 #endif
--- src/rlang-export.c
+++ src/rlang-export.c
@@ -1,19 +1,21 @@
 #define R_NO_REMAP
 #include <Rinternals.h>
+#include <Rversion.h>
 
 #include <tools/rlang-export.h>
 
-// Compat for R 3.4.0
-SEXP rlang_MakeExternalPtrFn(DL_FUNC p, SEXP tag, SEXP prot) {
+#if (defined(R_VERSION) && R_VERSION < R_Version(3, 4, 0))
+SEXP R_MakeExternalPtrFn(DL_FUNC p, SEXP tag, SEXP prot) {
   fn_ptr ptr;
   ptr.fn = p;
   return R_MakeExternalPtr(ptr.p, tag, prot);
 }
-DL_FUNC rlang_ExternalPtrAddrFn(SEXP s) {
+DL_FUNC R_ExternalPtrAddrFn(SEXP s) {
   fn_ptr ptr;
-  ptr.p =  EXTPTR_PTR(s);
+  ptr.p = EXTPTR_PTR(s);
   return ptr.fn;
 }
+#endif
 
 SEXP rlang_namespace(const char* ns) {
   SEXP call = PROTECT(Rf_lang2(Rf_install("getNamespace"), Rf_mkString(ns)));
@@ -23,14 +25,13 @@ SEXP rlang_namespace(const char* ns) {
 }
 
 void rlang_register_pointer(const char* ns, const char* ptr_name, DL_FUNC fn) {
-  SEXP ptr = PROTECT(rlang_MakeExternalPtrFn(fn, R_NilValue, R_NilValue));
+  SEXP ptr = PROTECT(R_MakeExternalPtrFn(fn, R_NilValue, R_NilValue));
 
   SEXP ptr_obj = PROTECT(Rf_allocVector(VECSXP, 1));
   SET_VECTOR_ELT(ptr_obj, 0, ptr);
 
-  SEXP class = PROTECT(Rf_mkString("fn_pointer"));
-  Rf_setAttrib(ptr_obj, R_ClassSymbol, class);
+  Rf_setAttrib(ptr_obj, R_ClassSymbol, Rf_mkString("fn_pointer"));
 
   Rf_defineVar(Rf_install(ptr_name), ptr_obj, rlang_namespace(ns));
-  UNPROTECT(3);
+  UNPROTECT(2);
 }
