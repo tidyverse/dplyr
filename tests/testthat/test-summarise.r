ad8929f30b4ff045a641ffc876def78614b7f7b1
Kirill Müller <krlmlr+r@mailbox.org>
2017-04-19 23:19:03 +0200

    adapt tests

--- tests/testthat/test-summarise.r
+++ tests/testthat/test-summarise.r
@@ -200,9 +200,9 @@ test_that("summarise propagate attributes (#194)", {
 
 })
 
-test_that("summarise strips names (#2231)", {
+test_that("summarise strips names, but only if grouped (#2231, #2675)", {
   data <- data_frame(a = 1:3) %>% summarise(b = setNames(nm = a[[1]]))
-  expect_null(names(data$b))
+  expect_equal(names(data$b), "1")
 
   data <- data_frame(a = 1:3) %>% rowwise %>% summarise(b = setNames(nm = a))
   expect_null(names(data$b))
