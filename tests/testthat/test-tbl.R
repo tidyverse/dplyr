84286aa1194c0eb0490dfdb1245f7faa226c423b
Lionel Henry <lionel.hry@gmail.com>
2017-04-26 17:30:32 +0200

    Handle grouped tibbles in tbl_rows_n()

--- tests/testthat/test-tbl.R
+++ tests/testthat/test-tbl.R
@@ -7,3 +7,8 @@ test_that("tbl_nongroup_vars() excludes group variables", {
   gdf <- group_by(mtcars, cyl)
   expect_identical(tbl_nongroup_vars(gdf), setdiff(tbl_vars(gdf), "cyl"))
 })
+
+test_that("tbl_rows_n() handles grouped data frames", {
+  gdf <- group_by(mtcars, cyl)
+  expect_identical(tbl_rows_n(gdf), 32L)
+})
