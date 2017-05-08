43d5b9d6fe93a811df453a868523ec18e5c3f2bc
Kirill Müller <krlmlr+r@mailbox.org>
2017-05-08 17:05:25 +0200

    use R_SHORT_LEN_MAX

--- inst/include/dplyr/workarounds/xlen.h
+++ inst/include/dplyr/workarounds/xlen.h
@@ -5,7 +5,7 @@ namespace Rcpp {
 
 template <>
 inline SEXP wrap(const ptrdiff_t& x) {
-  if (x < -2147483647 || x > 2147483647) {
+  if (x < -R_SHORT_LEN_MAX || x > R_SHORT_LEN_MAX) {
     return Rf_ScalarReal(static_cast<double>(x));
   }
   else {
