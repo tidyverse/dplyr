8378c7f50f246f1fd48c75f8e587f16460f8b254
Kirill Müller <krlmlr+r@mailbox.org>
2017-05-09 10:46:24 +0200

    wrap R_xlen_t only if long vector support is available
    otherwise `ptrdiff_t` is indistinguishable from `int` again

--- inst/include/dplyr/workarounds/xlen.h
+++ inst/include/dplyr/workarounds/xlen.h
@@ -1,11 +1,13 @@
 #ifndef DPLYR_WORKAROUND_XLEN_H
 #define DPLYR_WORKAROUND_XLEN_H
 
+#ifdef LONG_VECTOR_SUPPORT
+
 namespace Rcpp {
 
 template <>
-inline SEXP wrap(const ptrdiff_t& x) {
-  if (x < -R_LEN_T_MAX || x > R_LEN_T_MAX) {
+inline SEXP wrap(const R_xlen_t& x) {
+  if (x < -R_SHORT_LEN_MAX || x > R_SHORT_LEN_MAX) {
     return Rf_ScalarReal(static_cast<double>(x));
   }
   else {
@@ -14,5 +16,7 @@ inline SEXP wrap(const ptrdiff_t& x) {
 }
 
 }
+
+#endif
 
 #endif
