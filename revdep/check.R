f8c6402ec17affdf4a18ab3074cb29c542fd6cdb
Kirill Müller <krlmlr@users.noreply.github.com>
2017-05-18 22:15:02 +0200

    Revdep checks (#2802)
    * copy diff script from DBI
    
    * revdep update with v0.5.0 results
    
    * revdep update for clean state
    
    * revdep update with f-revdep results
    
    * max cores
    
    * revdep update for clean state
    
    * revdep update with v0.5.0 results
    
    * run only current version
    
    * revdep update with f-revdep results
    
    * revdep update for clean state
    
    * revdep update with f-revdep results
    
    * test old version again
    
    * revdep update with v0.5.0 results
    
    * revdep update for clean state
    
    * revdep update with f-revdep results
    
    * don't diff
    
    * revdep update for clean state
    
    * revdep update with f-revdep results
    
    * skipping broom for now because checks seem to hang
    
    * run CRAN version again
    
    * revdep update with v0.5.0 results
    
    * revdep update for clean state
    
    * revdep update with f-revdep results
    
    * revdep update with v0.5.0 results
    
    * revdep update with f-revdep results
    
    * revdep update with v0.5.0 results
    
    * revdep update for clean state
    
    * no diff
    
    * revdep update with f-revdep results
    
    * revdep update for clean state
    
    * revdep update with f-revdep results
    
    * tbl_df always inherits from tbl
    
    * fix test
    
    * NEWS
    
    * deprecation messages
    
    for cleaner CRAN checks
    
    * configure revdep check
    
    * remove installed pkgs
    
    * try-catch
    
    * revdep update with v0.5.0 results
    
    * revdep update for clean state
    
    * don't texpect warnings
    
    * prefer inform()
    
    * don't test old
    
    * revdep update with f-revdep results
    
    * fix slice() error with gctorture()
    
    * add test
    
    * return DataFrame instead of SEXP or RObject
    
    to avoid destruction when calling `~GroupedHybridEnv()` via `~GroupedCallProxy()` aka `~CallProxy()`
    
    * revdep update for clean state
    
    * install rlang from GitHub
    
    * revdep update with f-revdep results
    
    * fix common_by() message for many columns
    
    * revdep update for clean state
    
    * revdep update with f-revdep results
    
    * compare with base
    
    * also prune rlang
    
    * revdep update with v0.5.0 results
    
    * revdep update for clean state
    
    * revdep update with f-revdep results
    
    * summarize reasons for downstream failures

--- revdep/check.R
+++ revdep/check.R
@@ -1,6 +1,7 @@
 library("devtools")
 
-revdep_check(threads = 6)
+Sys.setenv(NOT_CRAN = FALSE)
+revdep_check(threads = parallel::detectCores())
 revdep_check_save_summary()
 revdep_check_print_problems()
 
