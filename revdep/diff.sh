bff0a27b3ff33e7208fe33e15f01c111d4370a2f
Kirill Müller <krlmlr+r@mailbox.org>
2017-05-18 11:35:37 +0000

    also prune rlang

--- revdep/diff.sh
+++ revdep/diff.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-R -q -e 'remove.packages(c("dplyr", "tibble"))' || true
+R -q -e 'remove.packages(c("dplyr", "tibble", "rlang"))' || true
 
 old_tag=v0.5.0
 branch=$(git symbolic-ref --short HEAD)
