d3870fbe59a13b150a3d70f7813f1539d3c8be84
Kirill Müller <krlmlr+r@mailbox.org>
2017-06-08 16:39:00 +0000

    enable gctorture

--- tests/testthat/helper-zzz.R
+++ tests/testthat/helper-zzz.R
@@ -3,5 +3,5 @@ if (any(grepl("testthat::test_dir", sapply(sys.calls(), as.character)))) {
   print(sys.calls())
   print("Enabling gctorture")
   bindrcpp:::init_logging("VERB")
-  #gctorture2(33)
+  gctorture2(33)
 }
