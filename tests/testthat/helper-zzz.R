ffb23cc5c02e9812f1b1707ce3ae9323b3dfdfd2
Kirill Müller <krlmlr+r@mailbox.org>
2017-06-14 07:15:50 +0000

    gctorture always

--- tests/testthat/helper-zzz.R
+++ tests/testthat/helper-zzz.R
@@ -1,7 +1,4 @@
 print("Sourcing helper")
-if (any(grepl("testthat::test_dir", sapply(sys.calls(), as.character)))) {
-  print(sys.calls())
-  print("Enabling gctorture")
-  bindrcpp:::init_logging("VERB")
-  gctorture2(33)
-}
+print("Enabling gctorture")
+bindrcpp:::init_logging("VERB")
+gctorture2(33)
