4ca525e18c2b498c1686f485c755c2e0b061b6f1
Lionel Henry <lionel.hry@gmail.com>
2017-07-10 16:35:08 +0200

    Move tests to tidyselect

---
+++ tests/testthat/test-colwise.R
@@ -0,0 +1,17 @@
+context("colwise utils")
+
+test_that("tbl_at_vars() errs on bad input", {
+  expect_error(
+    tbl_at_vars(iris, raw(3)),
+    "`.vars` must be a character/numeric vector or a `vars()` object, not raw",
+    fixed = TRUE
+  )
+})
+
+test_that("tbl_if_vars() errs on bad input", {
+  expect_error(
+    tbl_if_vars(iris, funs(identity, force), environment()),
+    "`.predicate` must have length 1, not 2",
+    fixed = TRUE
+  )
+})
