cc3c1f26c9c82eb2110752feea95460cada36f04
Lionel Henry <lionel.hry@gmail.com>
2017-11-03 15:59:08 +0100

    Reexport all tidy eval functions

---
+++ R/utils-tidy-eval.R
@@ -0,0 +1,16 @@
+#' Tidy eval helpers
+#'
+#' These six functions provide tidy eval-compatible ways to capture
+#' symbols (`sym()`, `syms()`), expressions (`expr()`,
+#' `exprs()`, `enexpr()`), and quosures (`quo()`, `quos()`, `enquo()`).
+#' To learn more about tidy eval and how to use these tools, read
+#' <http://rlang.tidyverse.org/articles/tidy-evaluation.html>
+#'
+#' @md
+#' @name tidyeval
+#' @keywords internal
+#' @aliases          quo quos enquo sym syms expr exprs enexpr quo_name
+#' @export           quo quos enquo quo_name
+#' @export           sym syms
+#' @export           expr exprs enexpr
+NULL
