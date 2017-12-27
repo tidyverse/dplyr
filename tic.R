42af60744e5f731b7f4886e93663ef7d5c3850c6
Kirill Müller <krlmlr+r@mailbox.org>
2017-12-27 23:03:24 +0100

    write ~/.R/Makevars

--- tic.R
+++ tic.R
@@ -1,1 +1,10 @@
 add_package_checks()
+
+get_stage("before_script") %>%
+  add_code_step({
+    dir.create("~/.R", showWarnings = FALSE)
+    writeLines(
+      "PKG_CXXFLAGS := ${PKG_CXXFLAGS} $${TRAVIS_CXXFLAGS}",
+      "~/.R/Makevars"
+    )
+  })
