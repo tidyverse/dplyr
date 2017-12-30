7dbb4393821747a6b0b9d35efbc49fb5504af863
Lionel Henry <lionel.hry@gmail.com>
2017-12-30 15:47:08 +0100

    Anticipate specific quosure handling in type_of()

--- R/utils-expr.R
+++ R/utils-expr.R
@@ -23,6 +23,7 @@ node_walk_replace <- function(node, old, new) {
 expr_substitute <- function(expr, old, new) {
   expr <- duplicate(expr)
   switch_type(expr,
+    quosure = node_walk_replace(quo_get_expr(expr), old, new),
     formula = ,
     language = node_walk_replace(node_cdr(expr), old, new),
     symbol = if (identical(expr, old)) return(new)
