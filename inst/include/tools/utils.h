5e7440d0755ed17fc717f154b541defe70266d46
Lionel Henry <lionel.hry@gmail.com>
2018-01-12 16:45:59 +0100

    Import rlang API for data masks

--- inst/include/tools/utils.h
+++ inst/include/tools/utils.h
@@ -55,6 +55,9 @@ extern SEXP (*rlang_quo_set_env)(SEXP quo, SEXP env);
 extern SEXP (*rlang_new_quosure)(SEXP expr, SEXP env);
 extern SEXP (*rlang_is_quosure)(SEXP x);
 extern SEXP (*rlang_as_data_pronoun)(SEXP data);
+extern SEXP (*rlang_as_data_mask)(SEXP data, SEXP parent);
+extern SEXP (*rlang_new_data_mask)(SEXP bottom, SEXP top, SEXP parent);
+extern SEXP (*rlang_data_mask_clean)(SEXP mask);
 
 } // internal
 
