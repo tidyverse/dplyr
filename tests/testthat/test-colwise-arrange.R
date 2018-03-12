4447814a53e40f4ca433ad8a1bfba22430e02e61
Romain Francois <romain@purrple.cat>
2018-03-12 11:33:47 +0100

    additional test for arrange_at related to #3351

--- tests/testthat/test-colwise-arrange.R
+++ tests/testthat/test-colwise-arrange.R
@@ -11,3 +11,13 @@ test_that("scoped arrange is identical to manual arrange", {
 test_that(".funs is applied to variables before sorting", {
   expect_identical(arrange_all(df, `-`), arrange(df, -mpg, -cyl, -disp))
 })
+
+test_that("arrange_at can arrange by grouping variables (#3351)", {
+  tbl <- data_frame(gr1 = rep(1:2, 4), gr2 = rep(1:2, each = 4), x = 1:8) %>%
+    group_by(gr1)
+
+  expect_identical(
+    arrange_at(tbl, vars(gr1)),
+    arrange(tbl, gr1)
+  )
+})
