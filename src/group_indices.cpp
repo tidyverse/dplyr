3d99e8361a8805b8f0eb15c6a08b9e1bc7a68950
Kirill Müller <krlmlr+r@mailbox.org>
2018-03-16 23:58:47 +0100

    only update class if not grouped already
    to retain existing classes

--- src/group_indices.cpp
+++ src/group_indices.cpp
@@ -89,11 +89,18 @@ DataFrame build_index_cpp(DataFrame data) {
     biggest_group = std::max(biggest_group, (int)chunk.size());
   }
 
+  // The attributes are injected into data without duplicating it!
+  // The object is mutated, violating R's usual copy-on-write semantics.
+  // This is safe here, because the indices are an auxiliary data structure
+  // that is rebuilt as necessary. Updating the object in-place saves costly
+  // recomputations, but we need to be careful with the "class" attribute.
   data.attr("indices") = indices;
   data.attr("group_sizes") = group_sizes;
   data.attr("biggest_group_size") = biggest_group;
   data.attr("labels") = labels;
-  set_class(data, CharacterVector::create("grouped_df", "tbl_df", "tbl", "data.frame"));
+  if (r_match(CharacterVector("grouped_df"), get_class(data))[0] < 0) {
+    set_class(data, CharacterVector::create("grouped_df", "tbl_df", "tbl", "data.frame"));
+  }
   return data;
 }
 
