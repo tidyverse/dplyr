7d5cef783fbabe472f3abb79f5d3da456e248586
Romain Francois <romain@purrple.cat>
2018-03-26 22:03:10 +0200

    - `ntile` can now be called with only one argument inside `mutate`. In that case `ntile(n)` is equivalent to `ntile(row_number(), n)` (#3418)
    ---
    
    closes #3418

--- tests/testthat/test-mutate-windowed.R
+++ tests/testthat/test-mutate-windowed.R
@@ -130,6 +130,14 @@ test_that("min_rank handles columns full of NaN (#726)", {
   expect_true(all(is.na(data$rank)))
 })
 
+test_that("ntile works with one argument (#3418)", {
+  df <- data.frame(x=1:42)
+  expect_identical(
+    mutate( df, nt = ntile(9)),
+    mutate( df, nt = ntile(row_number(), 9))
+  )
+})
+
 test_that("rank functions deal correctly with NA (#774)", {
   data <- data_frame(x = c(1, 2, NA, 1, 0, NA))
   res <- data %>% mutate(
