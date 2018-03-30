3ace34bf8eaec8295d5a7f09ab19947ced3438cd
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-03-30 16:38:21 -0400

    fixes #3238

--- R/sets.r
+++ R/sets.r
@@ -2,7 +2,8 @@
 #'
 #' These functions override the set functions provided in base to make them
 #' generic so that efficient versions for data frames and other tables can be
-#' provided. The default methods call the base versions.
+#' provided. The default methods call the base versions. Beware that
+#' intersect, union and setdiff remove duplicates.
 #'
 #' @param x,y objects to perform set function on (ignoring order)
 #' @param ... other arguments passed on to methods
@@ -19,6 +20,22 @@
 #'
 #' union_all(first, second)
 #' setequal(mtcars, mtcars[32:1, ])
+#'
+#' # duplicates handling
+#' a <- data.frame(column=c(1:10, 10))
+#' b <- data.frame(column=c(1:5, 5))
+#'
+#' # intersection is 1 to 5, duplicates removed (5)
+#' intersect(a, b)
+#'
+#' # union is 1 to 10, duplicates removed (5 and 10)
+#' union(a, b)
+#'
+#' # set difference, duplicates removed (10)
+#' setdiff(a, b)
+#'
+#' # union all does not remove duplicates
+#' union_all(a, b)
 NULL
 
 #' @rdname setops
