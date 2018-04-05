d4f0c6d126e65dfab0799fbbdf4a5e55d078ad9f
Romain Francois <romain@purrple.cat>
2018-04-05 17:21:13 +0200

    making `gr2` a numeric for finer testing

--- tests/testthat/test-colwise-group-by.R
+++ tests/testthat/test-colwise-group-by.R
@@ -16,7 +16,7 @@ test_that("group_by_ verbs accept optional operations", {
 })
 
 test_that("group_by variants can group by an already grouped by data (#3351)", {
-  tbl <- data_frame(gr1 = rep(1:2, 4), gr2 = rep(1:2, each = 4), x = 1:8) %>%
+  tbl <- data_frame(gr1 = rep(1:2, 4), gr2 = rep(c(1,2), each = 4), x = 1:8) %>%
     group_by(gr1)
 
   expect_identical(
