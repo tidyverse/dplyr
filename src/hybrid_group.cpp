826b41a1106f8070838674fd0d9122c2bcdfd2fe
Romain Francois <romain@purrple.cat>
2018-04-10 09:38:56 +0200

    The `group` method for overloads of `SlicingIndex` return 0 instead of -1 for ungrouped data.

--- src/hybrid_group.cpp
+++ src/hybrid_group.cpp
@@ -24,16 +24,14 @@ public:
   }
 
   SEXP process(const SlicingIndex& i) {
-    return IntegerVector(i.size(), clean_group(i.group()));
+    return IntegerVector(i.size(), i.group() + 1);
   }
 
-private:
-  int clean_group(int i) {
-    return i < 0 ? 1 : (i + 1);
-  }
 };
 
-Result* group_prototype(SEXP call, const ILazySubsets&, int nargs) {
+Result* group_indices_prototype(SEXP call, const ILazySubsets&, int nargs) {
+  // if there are arguments, give up on hybrid evaluation
+  // and let R handle things, perhaps it will be a group_indices(data, ...)
   if (nargs != 0)
     return 0;
 
@@ -42,5 +40,5 @@ Result* group_prototype(SEXP call, const ILazySubsets&, int nargs) {
 
 void install_group_handlers(HybridHandlerMap& handlers) {
   Environment ns_dplyr = Environment::namespace_env("dplyr");
-  handlers[ Rf_install("group_indices") ] = HybridHandler(group_prototype, HybridHandler::DPLYR, ns_dplyr["group_indices"]);
+  handlers[ Rf_install("group_indices") ] = HybridHandler(group_indices_prototype, HybridHandler::DPLYR, ns_dplyr["group_indices"]);
 }
