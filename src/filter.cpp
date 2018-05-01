d5eacf501d922a260a5e7943a49c6f1cecb67d44
Romain Francois <romain@purrple.cat>
2018-05-01 12:38:51 +0200

    comments

--- src/filter.cpp
+++ src/filter.cpp
@@ -20,13 +20,6 @@ using namespace Rcpp;
 using namespace dplyr;
 
 inline
-SEXP empty_subset(const DataFrame& df, const CharacterVector& classes) {
-  DataFrame res = DataFrameSubsetVisitors(df).subset(EmptySubset(), classes);
-  strip_index(res);
-  return res;
-}
-
-inline
 void check_result_length(const LogicalVector& test, int n) {
   if (test.size() != n) {
     stop("Result must have length %d, not %d", n, test.size());
@@ -41,15 +34,26 @@ SEXP check_result_lgl_type(SEXP tmp) {
   return tmp;
 }
 
+// class to collect indices for each group
 template <typename Index>
 class GroupFilterIndices {
 public:
   int ngroups;
 
+  // the old indices
   std::vector<Index> old_indices;
+
+  // the results of the test expression for each group
+  // we only keep those that we need
   Rcpp::List tests;
+
+  // The new indices
   Rcpp::List new_indices;
+
+  // The group sizes
   Rcpp::IntegerVector group_sizes;
+
+  // size of the biggest group
   int biggest_group_size ;
 
 private:
@@ -59,20 +63,45 @@ private:
 public:
 
   GroupFilterIndices(int ngroups_) :
-  ngroups(ngroups_),
+    ngroups(ngroups_),
 
-  old_indices(ngroups),
-  tests(ngroups),
-  new_indices(ngroups),
-  group_sizes(ngroups),
-  biggest_group_size(0),
-  k(0)
+    old_indices(ngroups),
+    tests(ngroups),
+    new_indices(ngroups),
+    group_sizes(ngroups),
+    biggest_group_size(0),
+    k(0)
   {}
 
+  // set the group i to be empty
   void empty_group(int i){
     group_sizes[i] = 0;
     new_indices[i] = Rcpp::IntegerVector::create();
   }
+
+  // the group i contains all the data from the original
+  void add_dense_group(int i, const Index& old_idx, int n) {
+    add_group(i, old_idx, n);
+  }
+
+  // the group i contains some data, available in g_test
+  void add_group_lgl(int i, const Index& old_idx, int n, Rcpp::LogicalVector g_test){
+    add_group(i, old_idx, n) ;
+    tests[i] = g_test;
+  }
+
+  // the total number of rows
+  // only makes sense when the object is fully trained
+  inline int size() const {
+    return k;
+  }
+
+  // is the group i dense
+  inline bool is_dense(int i) const {
+    return group_sizes[i] == old_indices[i].size();
+  }
+
+private:
 
   void add_group(int i, const Index& old_idx, int n) {
     old_indices[i] = old_idx;
@@ -80,19 +109,6 @@ public:
     new_indices[i] = Rcpp::seq(k, k+n-1) ;
     if (biggest_group_size < n) biggest_group_size = n;
     k += n ;
-  }
-
-  void add_group_lgl(int i, const Index& old_idx, int n, Rcpp::LogicalVector g_test){
-    add_group(i, old_idx, n) ;
-    tests[i] = g_test;
-  }
-
-  inline int size() const {
-    return k;
-  }
-
-  inline bool is_full(int i) const {
-    return group_sizes[i] == old_indices[i].size();
   }
 
 };
@@ -106,6 +122,7 @@ public:
   virtual SEXP subset(const GroupFilterIndices<Index>&) = 0;
 };
 
+// subset a vector using indices collected in an Index
 template <int RTYPE, typename Index>
 class FilterVectorVisitorImpl : public FilterVectorVisitor<Index> {
 public:
@@ -122,15 +139,21 @@ public:
 
     for(int i=0; i<idx.ngroups; i++){
       int group_size = idx.group_sizes[i];
+      // because there is nothing to do when the group is empty
       if (group_size > 0) {
+        // the indices relevant to the original data
         const Index& old_idx = idx.old_indices[i];
 
+        // the new indices
         IntegerVector new_idx = idx.new_indices[i];
-        if (idx.is_full(i)) {
+        if (idx.is_dense(i)) {
+          // in that case we can just all the data
           for (int j=0; j<group_size; j++) {
             out[new_idx[j]] = data[old_idx[j]];
           }
         } else {
+          // otherwise we copy only the data for which test is TRUE
+          // so we discard FALSE and NA
           LogicalVector test = idx.tests[i];
           for (int j=0, k=0; j<group_size; j++) {
             while(test[k] != TRUE) k++ ;
@@ -146,6 +169,7 @@ private:
   Vec data;
 };
 
+// subset `data` with indices collected in `idx`
 template <typename Index>
 inline SEXP filter_visit(SEXP data, const GroupFilterIndices<Index>& idx) {
   switch (TYPEOF(data)) {
@@ -166,6 +190,8 @@ inline SEXP filter_visit(SEXP data, const GroupFilterIndices<Index>& idx) {
   return R_NilValue;
 }
 
+// template class to rebuild the attributes
+// in the general case there is nothing to do
 template <typename Index, typename SlicedTibble>
 class SlicedTibbleRebuilder{
 public:
@@ -173,6 +199,8 @@ public:
   void reconstruct(List& out){}
 };
 
+// specific case for GroupedDataFrame
+// we need to take care of the attributes `indices`, `labels`, `vars`, `group_sizes`, `biggest_group_size`
 template <typename Index>
 class SlicedTibbleRebuilder<Index, GroupedDataFrame>{
 public:
@@ -205,36 +233,43 @@ SEXP filter_impl(const SlicedTibble& gdf, const NamedQuosure& quo) {
   Proxy call_proxy(quo.expr(), gdf, quo.env()) ;
   GroupIterator git = gdf.group_begin();
 
+  const DataFrame& data = gdf.data() ;
   int ngroups = gdf.ngroups() ;
+
+  // tracking the indices for each group
   GroupFilterIndices<Index> group_indices(ngroups);
 
+  // traverse each group and fill `group_indices`
   for (int i = 0; i < ngroups; i++, ++git) {
     const Index& indices = *git;
     int chunk_size = indices.size();
 
-    // special case with empty group size. no need to evaluate the expression
+    // empty group size. no need to evaluate the expression
     if (chunk_size == 0) {
       group_indices.empty_group(i) ;
       continue;
     }
 
+    // the result of the expression in the group
     LogicalVector g_test = check_result_lgl_type(call_proxy.get(indices));
     if (g_test.size() == 1) {
-      // recycle
+      // we get length 1 so either we have an empty group, or a dense group, i.e.
+      // a group that has all the rows from the original data
       if (g_test[0] == TRUE){
-        group_indices.add_group(i, indices, chunk_size) ;
+        group_indices.add_dense_group(i, indices, chunk_size) ;
       } else {
         group_indices.empty_group(i);
       }
     } else {
+      // any other size, so we check that it is consistent with the group size
       check_result_length(g_test, chunk_size);
       int yes = std::count(g_test.begin(), g_test.end(), TRUE);
       group_indices.add_group_lgl(i, indices, yes, g_test);
     }
   }
 
-  const DataFrame& data = gdf.data() ;
 
+  // create the result data frame
   int nc = data.size();
   List out(data.size());
 
@@ -250,26 +285,12 @@ SEXP filter_impl(const SlicedTibble& gdf, const NamedQuosure& quo) {
   }
 
   // set the specific attributes
+  // currently this only does anything for SlicedTibble = GroupedDataFrame
+  // i.e. retain the indices, labels, ... attributes
   SlicedTibbleRebuilder<Index,SlicedTibble>(group_indices, data).reconstruct(out);
 
   return out;
 }
-
-// DataFrame filter_ungrouped(DataFrame df, const NamedQuosure& quo) {
-//   CallProxy proxy(quo.expr(), df, quo.env());
-//   LogicalVector test = check_result_lgl_type(proxy.eval());
-//
-//   if (test.size() == 1) {
-//     if (test[0] == TRUE) {
-//       return df;
-//     } else {
-//       return empty_subset(df, classes_not_grouped());
-//     }
-//   } else {
-//     check_result_length(test, df.nrows());
-//     return subset(df, test, classes_not_grouped());
-//   }
-// }
 
 // [[Rcpp::export]]
 SEXP filter_impl(DataFrame df, NamedQuosure quo) {
