d5f79777d215f3e2d6b5f95e9f130101e6ebe56e
Romain Francois <romain@purrple.cat>
2018-05-04 14:38:49 +0200

    group_by gives a warning about implicit NA in fcators

--- tests/testthat/test-group-by.r
+++ tests/testthat/test-group-by.r
@@ -71,7 +71,7 @@ test_that("group_by uses shallow copy", {
 
 test_that("group_by handles NA in factors #341", {
   d <- tibble(x = 1:3, f = factor(c("a", "b", NA)))
-  g <- group_by(d, f)
+  expect_warning(g <- group_by(d, f), "Column `f` is a factor with implicit NA")
   expect_equal(group_size(g), rep(1L, 3L))
 
   d <- tibble(
@@ -79,9 +79,11 @@ test_that("group_by handles NA in factors #341", {
     f2 = factor(c(1,2,1,NA)),
     x  = 1:4
   )
-  expect_equal(
-    group_size(group_by(d, f1, f2)),
-    c(1L,1L,1L,0L,1L)
+  expect_warning(
+    expect_equal(
+      group_size(group_by(d, f1, f2)),
+      c(1L,1L,1L,0L,1L)
+    )
   )
 })
 
