d7d1e1d596da7fbe7c03742228c6a0f13fb1dfe8
Romain Francois <romain@purrple.cat>
2018-05-09 21:19:57 +0200

    :heavy_plus_sign: doc

--- R/rows.R
+++ R/rows.R
@@ -1,4 +1,14 @@
 
+#' List of rows of each group
+#'
+#' @param .data a tibble
+#'
+#' @return a list of integer vectors, giving the indices of the rows of each group
+#'
+#' @examples
+#' df <- tibble(x = c(1,1,2,2))
+#' rows(df)
+#' rows(group_by(df, x))
 #' @export
 rows <- function(.data) {
   UseMethod("rows")
