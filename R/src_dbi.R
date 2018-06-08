ac759c9bd256862d786a79548ce03f6f20f60b0a
hadley <h.wickham@gmail.com>
2018-06-08 16:56:54 -0500

    Correct src_sqlite error message
    Fixes #3400

--- R/src_dbi.R
+++ R/src_dbi.R
@@ -143,7 +143,7 @@ src_sqlite <- function(path, create = FALSE) {
   check_dbplyr()
 
   if (!create && !file.exists(path)) {
-    bad_args("path", "must not already exist, unless `create` = TRUE")
+    bad_args("path", "must already exist, unless `create` = TRUE")
   }
 
   con <- DBI::dbConnect(RSQLite::SQLite(), path)
