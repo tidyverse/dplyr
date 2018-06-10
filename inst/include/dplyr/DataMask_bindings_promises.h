f692e4f51d5dcad91c9437ab292e9027e4b99c2a
Romain Francois <romain@purrple.cat>
2018-06-10 16:23:14 -0500

    Greedily evaluate the subsets for subsequent groups once the promise has been forced.

--- inst/include/dplyr/DataMask_bindings_promises.h
+++ inst/include/dplyr/DataMask_bindings_promises.h
@@ -19,7 +19,10 @@ public:
   DataMask_bindings_promises(SEXP parent_env, Subsets& subsets_) :
     mask_bindings(child_env(parent_env)),
     subsets(subsets_),
-    promises()
+    n_subsets(subsets.size()),
+    promises(),
+    forced(n_subsets, false),
+    names(subsets.get_variable_names().get_vector())
   {}
 
   inline operator SEXP() {
@@ -38,7 +41,10 @@ public:
 private:
   Environment mask_bindings;
   Subsets& subsets ;
+  int n_subsets;
   std::vector<promise> promises;
+  std::vector<bool> forced;
+  CharacterVector names;
 
   inline SEXP get_subset_expr(int i, const Index& indices) {
     static SEXP symb_bracket = Rcpp::traits::same_type<Data, RowwiseDataFrame>::value ? R_Bracket2Symbol : R_BracketSymbol ;
@@ -50,28 +56,36 @@ private:
   }
 
   void set_promises(const Index& indices) {
-    CharacterVector names = subsets.get_variable_names().get_vector();
-    int n = names.size();
-    promises.reserve(n);
-    for (int i = 0; i < n; i++) {
+    promises.reserve(n_subsets);
+    for (int i = 0; i < n_subsets; i++) {
       promises.push_back(promise(names[i], mask_bindings, get_subset_expr(i, indices)));
     }
   }
 
   void update_promises(const Index& indices) {
     for (int i = 0; i < subsets.size(); i++) {
+
       promise& p = promises[i];
 
       if (p.was_forced()) {
-        // it has been evaluated, install a new promise
-        // would maybe be better to do either of:
-        // - reset it to unforced, but SET_PRVALUE(p, R_UnboundValue) does not work and gives this error: Evaluation error: 'rho' must be an environment not NULL: detected in C-level eval.
-        // - promote the promise to its value and recalculate it upfront for each group
-
-        p.install(get_subset_expr(i, indices));
+        forced[i] = true ;
       } else {
         // otherwise just need to update the expression
         update_promise_index(p, indices);
+        continue;
+      }
+
+      if (forced[i]) {
+        // we know for sure that we want this column, so evaluate the
+        // subset and set it in the environment
+        SEXP expr = PROTECT(get_subset_expr(i, indices));
+        SEXP result = PROTECT(Rcpp_eval(expr, R_BaseEnv));
+        Rf_defineVar(
+          Rf_installChar(names[i]),
+          result,
+          mask_bindings
+        );
+        UNPROTECT(2);
       }
 
     }
