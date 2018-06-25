d7d76a29dd4f09e47516e5d09c8cebceb8e75a17
Kirill Müller <krlmlr+r@mailbox.org>
2018-06-25 22:58:33 +0200

    Extract RObject-s to variables

--- src/test.cpp
+++ src/test.cpp
@@ -109,19 +109,24 @@ List test_matches() {
 LogicalVector test_length_wrap() {
   R_xlen_t small = R_LEN_T_MAX / 2;
 
-  bool small_ok = as<double>(RObject(wrap(small))) == (double)small;
+  RObject wrap_small(wrap(small));
 
 #ifdef LONG_VECTOR_SUPPORT
   R_xlen_t large = (R_xlen_t)(R_LEN_T_MAX * 2.0);
   R_xlen_t missing = NA_INTEGER;
 
-  bool large_ok = as<double>(RObject(wrap(large))) == (double)large;
-  bool missing_ok = as<double>(RObject(wrap(missing))) == (double)missing;
+  RObject wrap_large(wrap(large));
+  RObject wrap_missing(wrap(missing));
 
   return
-    LogicalVector::create(small_ok, large_ok, missing_ok);
+    LogicalVector::create(
+      as<double>(wrap_small) == (double)small,
+      as<double>(wrap_large) == (double)large,
+      as<double>(wrap_missing) == (double)missing
+    );
 #else
-  return
-    LogicalVector::create(small_ok);
+  LogicalVector::create(
+    as<double>(wrap_small) == (double)small
+  );
 #endif
 }
