0871d5c6927a216ca5d1d169e9afae4431116e17
Romain Francois <romain@purrple.cat>
2018-08-20 10:00:02 +0200

    - no longer used (with/without)_hybrid

--- tests/testthat/helper-hybrid.R
+++ tests/testthat/helper-hybrid.R
@@ -1,11 +1,3 @@
-expect_predicate <- function(actual, expected, label) {
-  if (is.function(expected)) {
-    expect_true((!!expected)(actual), label = label)
-  } else {
-    expect_identical(actual, !!expected, label = label)
-  }
-}
-
 expect_hybrid <- function(data, expr, info = NULL, label = NULL) {
   expr <- enquo(expr)
   expect_true(hybrid_call(data, !!expr), info = info, label = label)
@@ -15,30 +7,3 @@ expect_not_hybrid <- function(data, expr, info = NULL, label = NULL) {
   expr <- enquo(expr)
   expect_false(hybrid_call(data, !!expr), info = info, label = label)
 }
-
-# check_hybrid_result <- function(expr, ..., expected, test_eval = TRUE) {
-#   expr <- enquo(expr)
-#   expect_predicate(with_hybrid(!!expr, ...), expected, label = rlang::quo_label(expr))
-#   if (test_eval) {
-#     expect_predicate(eval_dots(!!expr, ...), expected, label = rlang::quo_label(expr))
-#   }
-# }
-#
-# check_not_hybrid_result <- function(expr, ..., expected, test_eval = TRUE) {
-#   expr <- enquo(expr)
-#   expect_predicate(without_hybrid(!!expr, ...), expected, label = rlang::quo_label(expr))
-#   if (test_eval) {
-#     expect_predicate(eval_dots(!!expr, ...), expected, label = rlang::quo_label(expr))
-#   }
-# }
-
-expect_not_hybrid_error <- function(expr, ..., error) {
-  expr <- enquo(expr)
-  expect_error(
-    without_hybrid(!!expr, ...),
-    error,
-    label = rlang::quo_label(expr)
-  )
-}
-
-bad_hybrid_handler <- function(...) stop("Expected hybrid evaluation")
