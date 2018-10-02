e11ddf532c0bf10d1f05974fdd1028d1e8040726
Lionel Henry <lionel.hry@gmail.com>
2018-10-02 16:17:30 +0200

    Fix unit test following rlang update
    Construct expression manually because .data[[ is now an unquote operator

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -16,5 +16,6 @@ test_that("quo_is_variable_reference handles .data",{
   expect_true(quo_is_variable_reference(quo(x)))
   expect_true(quo_is_variable_reference(quo(.data$x)))
   expect_true(quo_is_variable_reference(quo(.data[["x"]])))
-  expect_false(quo_is_variable_reference(quo(.data[[identity("x")]])))
+  quo <- new_quosure(quote(.data[[identity("x")]]))
+  expect_false(quo_is_variable_reference(quo))
 })
