3e9db37ebd170ee9f9c7034d473b2ad273ba26ea
Romain Francois <romain@purrple.cat>
2018-11-13 09:15:20 +0100

    s/nest_by/group_nest/

---
+++ R/group_nest.R
@@ -0,0 +1,96 @@
+
+group_nest_impl <- function(.data, key_var){
+  to_nest <- select(ungroup(.data), setdiff(tbl_vars(.data), group_vars(.data)))
+
+  out <- .data %>%
+    group_data() %>%
+    mutate(!!key_var := map(.data$.rows, function(.x) to_nest[.x,, drop = FALSE])) %>%
+    select(-".rows")
+
+  attr(out[[key_var]], "ptype") <- to_nest[integer(), , drop = FALSE]
+  out
+}
+
+#' Nest a tibble using a grouping specification
+#'
+#' \badgeexperimental
+#'
+#' Conceptually, `group_nest` is the same as `group_by()` + `tidyr::nest()`.
+#'
+#' @param .data a tbl
+#' @param ... Variables to nest by, ignored if `.data` is already a grouped_df.
+#' @param .key the name of the list column
+#'
+#' @details
+#' When `.data` is an ungrouped data frame, the `...` specifiy the columns to group *by* are are forwarded
+#' to either [group_by()], [group_by_at()] or [group_by_if()].
+#'
+#' When `.data` is a grouped data frame already, its grouping is used. A warning is issued if `...`
+#' is used with a grouped data frame.
+#'
+#' @return A tbl with one row per unique combination of the grouping variables. The first columns are the grouping variables,
+#' followed by a list column of tibbles with matching rows of the remaining columns.
+#'
+#' @keywords internal
+#' @examples
+#'
+#' # using group_nest() or its column wise wariants on a ungrouped data frame
+#' starwars %>%
+#'   group_nest(species, homeworld)
+#'
+#' starwars %>%
+#'   group_nest_at(vars(ends_with("_color")))
+#'
+#' starwars %>%
+#'   group_by_if(is.numeric)
+#'
+#' # using group_nest() on a grouped data frame
+#' starwars %>%
+#'   group_by(species, homeworld) %>%
+#'   group_nest()
+#'
+#' # can be useful when the grouped data has been altered before the nesting
+#' # in this example the output is nested by homeworld after the summarise
+#' # has peeled off one level of grouping
+#' starwars %>%
+#'   group_by(homeworld, species) %>%
+#'   summarise(n = n()) %>%
+#'   group_nest()
+#'
+#' @export
+group_nest <- function(.data, ..., .key = "data"){
+  UseMethod("group_nest")
+}
+
+#' @export
+group_nest.default <- function(.data, ..., .key = "data") {
+  abort("unsupported")
+}
+
+#' @export
+group_nest.data.frame <- function(.data, ..., .key = "data") {
+  group_nest_impl(
+    group_by(.data, ...),
+    key_var = quo_name(enexpr(.key))
+  )
+}
+
+#' @export
+group_nest.grouped_df <- function(.data, ..., .key = "data") {
+  if (dots_n(...)) {
+    warn("... is not supported in group_nest(<grouped_df>), consider group_by(..., add = TRUE) %>% group_nest()")
+  }
+  group_nest_impl(.data, key_var = quo_name(enexpr(.key)))
+}
+
+#' @rdname group_nest
+#' @export
+group_nest_at <- function(.data, ..., .key = "data") {
+  group_nest(group_by_at(.data, ...), .key = quo_name(enexpr(.key)))
+}
+
+#' @rdname group_nest
+#' @export
+group_nest_if <- function(.data, ..., .key = "data") {
+  group_nest(group_by_if(.data, ...), .key = quo_name(enexpr(.key)))
+}
---
+++ tests/testthat/test-group_nest.R
@@ -0,0 +1,34 @@
+context("group_nest")
+
+test_that("group_nest() works", {
+  grouped <- group_by(starwars, species, homeworld)
+  gdata <- group_data(grouped)
+
+  res <- group_nest(starwars, species, homeworld)
+  expect_is(pull(res), "list")
+  expect_equal(select(res, -last_col()), select(gdata, -last_col()))
+})
+
+test_that("group_nest() works on grouped data frames", {
+  grouped <- group_by(starwars, species, homeworld)
+  gdata <- group_data(grouped)
+
+  res <- group_nest(grouped)
+  expect_is(pull(res), "list")
+  expect_equal(select(res, -last_col()), select(gdata, -last_col()))
+})
+
+test_that("group_nest_at() works", {
+  gdata <- group_data(group_by(starwars, species, homeworld))
+
+  res <- group_nest_at(starwars, vars(c("species", "homeworld")))
+  expect_is(pull(res), "list")
+  expect_equal(select(res, -last_col()), select(gdata, -last_col()))
+})
+
+test_that("group_nest_if() works", {
+  res <- group_nest_if(iris, is.factor)
+  gdata <- group_data(group_by(iris, Species))
+  expect_is(pull(res), "list")
+  expect_equal(select(res, -last_col()), select(gdata, -last_col()))
+})
