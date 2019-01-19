dab6679838b3a9272e92ef7a4d69a631ed51147d
Tony ElHabr <anthonyelhabr@gmail.com>
2019-01-19 10:43:02 -0600

    addressing issue 4071 (add cumall documentation)

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -241,6 +241,42 @@ quo_is_data_pronoun <- function(quo) {
 #' @param x For `cumall()` and `cumany()`, a logical vector; for
 #'   `cummean()` an integer or numeric vector
 #' @export
+#' @examples
+#' x <- c(1, 3, 5, 2, 2)
+#'
+#' # `cummean()` returns a numeric/integer vector of the same length
+#' # as the input vector.
+#' cummean(x)
+#'
+#' # `cumall()` and `cumany()` return logicals, not numerics/integers.
+#' cumall(x)
+#' cumany(x)
+#'
+#'
+#' # An NA only affects all elements after it for `cumall()` and `cummean()`,
+#' # but NOT for `cumany()`
+#' x <- c(1, 3, 5, NA, 2, 2)
+#'
+#' # NAs in every position after the corresponding NA position in the vector.
+#' cumall(x)
+#' cummean(x)
+#'
+#' # No NAs at all!
+#' cumany(x)
+#'
+#' # An NA at the beginning of the vector causes all elements after to be NA.
+#' x <- c(NA, 1, 3, 5, 2, 2)
+#'
+#' cummean(x)
+#' cumall(x)
+#' cumany(x)
+#'
+#' # NULLs are thrown out.
+#' x <- c(1, 3, 5, NULL, 2, 2)
+#'
+#' cummean(x)
+#' cumall(x)
+#' cumany(x)
 cumall <- function(x) {
     .Call(`_dplyr_cumall`, x)
 }
--- src/window.cpp
+++ src/window.cpp
@@ -10,6 +10,42 @@
 //' @param x For `cumall()` and `cumany()`, a logical vector; for
 //'   `cummean()` an integer or numeric vector
 //' @export
+//' @examples
+//' x <- c(1, 3, 5, 2, 2)
+//'
+//' # `cummean()` returns a numeric/integer vector of the same length
+//' # as the input vector.
+//' cummean(x)
+//'
+//' # `cumall()` and `cumany()` return logicals, not numerics/integers.
+//' cumall(x)
+//' cumany(x)
+//'
+//'
+//' # An NA only affects all elements after it for `cumall()` and `cummean()`,
+//' # but NOT for `cumany()`
+//' x <- c(1, 3, 5, NA, 2, 2)
+//'
+//' # NAs in every position after the corresponding NA position in the vector.
+//' cumall(x)
+//' cummean(x)
+//'
+//' # No NAs at all!
+//' cumany(x)
+//'
+//' # An NA at the beginning of the vector causes all elements after to be NA.
+//' x <- c(NA, 1, 3, 5, 2, 2)
+//'
+//' cummean(x)
+//' cumall(x)
+//' cumany(x)
+//'
+//' # NULLs are thrown out.
+//' x <- c(1, 3, 5, NULL, 2, 2)
+//'
+//' cummean(x)
+//' cumall(x)
+//' cumany(x)
 // [[Rcpp::export]]
 LogicalVector cumall(LogicalVector x) {
   int n = x.length();
