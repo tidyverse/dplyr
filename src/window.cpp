1ab547f98dc1042d93d12d0dc9d7face0faa6bf0
Tony ElHabr <anthonyelhabr@gmail.com>
2019-01-26 09:09:53 -0600

    Improve cumany/cumall/cummean docs (#4115)
    Fixes #4071

--- src/window.cpp
+++ src/window.cpp
@@ -3,13 +3,42 @@
 
 //' Cumulativate versions of any, all, and mean
 //'
-//' dplyr adds `cumall()`, `cumany()`, and `cummean()` to complete
-//' R's set of cumulate functions to match the aggregation functions available
-//' in most databases
+//' dplyr provides `cumall()`, `cumany()`, and `cummean()` to complete R's set
+//' of cumulative functions.
+//'
+//' @section Cumulative logical functions:
+//'
+//' These are particularly useful in conjunction with `filter()`:
+//'
+//' * `cumall(x)`: all cases until the first `FALSE`.
+//' * `cumall(!x)`: all cases until the first `TRUE`.
+//' * `cumany(x)`: all cases after the first `TRUE`.
+//' * `cumany(!x)`: all cases after the first `FALSE`.
 //'
 //' @param x For `cumall()` and `cumany()`, a logical vector; for
-//'   `cummean()` an integer or numeric vector
+//'   `cummean()` an integer or numeric vector.
+//' @return A vector the same length as `x`.
 //' @export
+//' @examples
+//' # `cummean()` returns a numeric/integer vector of the same length
+//' # as the input vector.
+//' x <- c(1, 3, 5, 2, 2)
+//' cummean(x)
+//' cumsum(x) / seq_along(x)
+//'
+//' # `cumall()` and `cumany()` return logicals
+//' cumall(x < 5)
+//' cumany(x == 3)
+//'
+//' # `cumall()` vs. `cumany()`
+//' df <- data.frame(
+//'   date = as.Date("2020-01-01") + 0:6,
+//'   balance = c(100, 50, 25, -25, -50, 30, 120)
+//' )
+//' # all rows after first overdraft
+//' df %>% filter(cumany(balance < 0))
+//' # all rows until first overdraft
+//' df %>% filter(cumall(!(balance < 0)))
 // [[Rcpp::export]]
 LogicalVector cumall(LogicalVector x) {
   int n = x.length();
