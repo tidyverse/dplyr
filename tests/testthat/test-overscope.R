2d14fc8c1144b54f283f545d4d4e5c146e1797db
Romain Francois <romain@rstudio.com>
2019-06-12 14:38:53 +0200

    specifically catch rlang_data_pronoun_not_found errors

--- tests/testthat/test-overscope.R
+++ tests/testthat/test-overscope.R
@@ -3,11 +3,13 @@ context("overscope")
 test_that(".data has strict matching semantics (#2591)", {
   expect_error(
     tibble(a = 1) %>% mutate(c = .data$b),
-    "data"
+    "data",
+    class = "rlang_data_pronoun_not_found"
   )
 
   expect_error(
     tibble(a = 1:3) %>% group_by(a) %>% mutate(c = .data$b),
-    "data"
+    "data",
+    class = "rlang_data_pronoun_not_found"
   )
 })
