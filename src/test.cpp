fd428581e1eef9b47055bbb09b0fe77816b98bb6
Romain François <romain@rstudio.com>
2019-09-04 08:11:36 +0200

    vctrs:: based joins (#4546)
    * explicit class on expect_error()
    
    * vctrs:: based left_join()
    
    * vctrs:: based right_join() very similar to left_join()
    
    * vctrs:: based semi_join()
    
    * vctrs:: based anti_join()
    
    * vctrs:: based inner_join()
    
    * vctrs:: based full_join()
    
    * Remove obsolete internal functions
    
    * Remove obsolete internal code that used to perform joins.
    
    * Remove more obsolete code
    
    * Remove more obsolete code
    
    * full_join() respects tibble ness

--- src/test.cpp
+++ src/test.cpp
@@ -4,8 +4,6 @@
 
 #include <dplyr/main.h>
 
-#include <dplyr/visitors/join/join_match.h>
-
 // [[Rcpp::export(rng = false)]]
 Rcpp::LogicalVector test_comparisons() {
   typedef dplyr::comparisons<REALSXP> comp;
@@ -19,90 +17,6 @@ Rcpp::LogicalVector test_comparisons() {
            comp::is_less(1.0, NA_REAL),
            comp::is_less(1.0, R_NaN)
          );
-}
-
-// [[Rcpp::export(rng = false)]]
-Rcpp::List test_matches() {
-  typedef dplyr::join_match<INTSXP, INTSXP, true> int_int_na;
-  typedef dplyr::join_match<REALSXP, REALSXP, true> real_real_na;
-  typedef dplyr::join_match<INTSXP, REALSXP, true> int_real_na;
-  typedef dplyr::join_match<REALSXP, INTSXP, true> real_int_na;
-  typedef dplyr::join_match<INTSXP, INTSXP, false> int_int;
-  typedef dplyr::join_match<REALSXP, REALSXP, false> real_real;
-  typedef dplyr::join_match<INTSXP, REALSXP, false> int_real;
-  typedef dplyr::join_match<REALSXP, INTSXP, false> real_int;
-  return
-    Rcpp::List::create(
-      Rcpp::LogicalVector::create(
-        int_int_na::is_match(1, 1),
-        !int_int_na::is_match(1, 2),
-        !int_int_na::is_match(1, NA_INTEGER),
-        !int_int_na::is_match(NA_INTEGER, 1),
-        int_int_na::is_match(NA_INTEGER, NA_INTEGER),
-        int_int::is_match(1, 1),
-        !int_int::is_match(1, 2),
-        !int_int::is_match(1, NA_INTEGER),
-        !int_int::is_match(NA_INTEGER, 1),
-        !int_int::is_match(NA_INTEGER, NA_INTEGER)
-      ),
-
-      Rcpp::LogicalVector::create(
-        real_real_na::is_match(1, 1),
-        !real_real_na::is_match(1, 2),
-        !real_real_na::is_match(1, NA_REAL),
-        !real_real_na::is_match(NA_REAL, 1),
-        !real_real_na::is_match(1, R_NaN),
-        !real_real_na::is_match(R_NaN, 1),
-        !real_real_na::is_match(R_NaN, NA_REAL),
-        !real_real_na::is_match(NA_REAL, R_NaN),
-        real_real_na::is_match(NA_REAL, NA_REAL),
-        real_real_na::is_match(R_NaN, R_NaN),
-        real_real::is_match(1, 1),
-        !real_real::is_match(1, 2),
-        !real_real::is_match(1, NA_REAL),
-        !real_real::is_match(NA_REAL, 1),
-        !real_real::is_match(1, R_NaN),
-        !real_real::is_match(R_NaN, 1),
-        !real_real::is_match(R_NaN, NA_REAL),
-        !real_real::is_match(NA_REAL, R_NaN),
-        !real_real::is_match(NA_REAL, NA_REAL),
-        !real_real::is_match(R_NaN, R_NaN)
-      ),
-
-      Rcpp::LogicalVector::create(
-        int_real_na::is_match(1, 1),
-        !int_real_na::is_match(1, 2),
-        !int_real_na::is_match(1, NA_REAL),
-        !int_real_na::is_match(NA_INTEGER, 1),
-        !int_real_na::is_match(1, R_NaN),
-        !int_real_na::is_match(NA_INTEGER, R_NaN),
-        int_real_na::is_match(NA_INTEGER, NA_REAL),
-        int_real::is_match(1, 1),
-        !int_real::is_match(1, 2),
-        !int_real::is_match(1, NA_REAL),
-        !int_real::is_match(NA_INTEGER, 1),
-        !int_real::is_match(1, R_NaN),
-        !int_real::is_match(NA_INTEGER, R_NaN),
-        !int_real::is_match(NA_INTEGER, NA_REAL)
-      ),
-
-      Rcpp::LogicalVector::create(
-        real_int_na::is_match(1, 1),
-        !real_int_na::is_match(1, 2),
-        !real_int_na::is_match(1, NA_INTEGER),
-        !real_int_na::is_match(NA_REAL, 1),
-        !real_int_na::is_match(R_NaN, 1),
-        !real_int_na::is_match(R_NaN, NA_INTEGER),
-        real_int_na::is_match(NA_REAL, NA_INTEGER),
-        real_int::is_match(1, 1),
-        !real_int::is_match(1, 2),
-        !real_int::is_match(1, NA_INTEGER),
-        !real_int::is_match(NA_REAL, 1),
-        !real_int::is_match(R_NaN, 1),
-        !real_int::is_match(R_NaN, NA_INTEGER),
-        !real_int::is_match(NA_REAL, NA_INTEGER)
-      )
-    );
 }
 
 // [[Rcpp::export(rng = false)]]
