6b3df91d708fc1d2dd9aed7c615be374702704a7
Romain François <romain@rstudio.com>
2019-09-12 14:37:05 +0200

    vctrs:: based versions of bind_*() (#4562)
    * Implementation of bind_rows() on top of vctrs::vec_rbind()
    
    * Trim obsolete code
    
    * C++ class NaturalDataFrame is obsolete
    
    * RowwiseDataFrame C++ class is obsolete
    
    * Simplify group_indices() implementation
    
    * R implementation of select_impl()
    
    * Simplify ungroup.grouped_df()
    
    * Remove obsolete code
    
    * Services from BH are no longer required
    
    * More obsolete code removal
    
    * Clean more obsolete code
    
    * Remove obsolete function
    
    * Remove unneeded copy_attrib() function
    
    * Remove obsolete code
    
    * Remove obsolete code and tests
    
    * Remove obsolete code
    
    * Simplify header file inclusion. No longer need dplyr.h
    
    * vctrs:: based impl of bind_cols()
    
    * Let go of cbind_all() internal legacy impl for bind_cols()
    
    * Simplifying bind_rows() further. More obsolete code trimming.
    
    * Remove documentation for removed function
    
    * Soft deprecate location() and changes() [ci skip]
    
    * Move quo_is_variable_reference() to rlang.cpp
    
    * No longer need to binary connect to rlang::
    
    * move quo_is_variable_reference() to R code
    
    * simplify symbols cache
    
    * Consolidate dplyr api things in dplyr.cpp
    
    * Move functions from the future funs package to funs.cpp
    
    * skipping tests

--- tests/testthat/test-select.r
+++ tests/testthat/test-select.r
@@ -67,14 +67,8 @@ test_that("select can be before group_by (#309)", {
 test_that("rename errors with invalid grouped data frame (#640)", {
   df <- tibble(a = 1:3, b = 2:4, d = 3:5) %>% group_by(a, b)
   df$a <- NULL
-  expect_error(
-    df %>% rename(e = d),
-    "not found in groups metadata"
-  )
-  expect_error(
-    df %>% rename(e = b),
-    "not found in groups metadata"
-  )
+  expect_error(df %>% rename(e = d))
+  expect_error(df %>% rename(e = b))
 })
 
 test_that("rename() handles data pronoun", {
