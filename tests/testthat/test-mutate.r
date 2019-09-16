aa43eb98ab74e9c62fb782b4f9479ba170f22dae
Romain Francois <romain@rstudio.com>
2019-09-16 08:20:38 +0200

    dev tibble does not allow matrix like indexing

--- tests/testthat/test-mutate.r
+++ tests/testthat/test-mutate.r
@@ -474,7 +474,8 @@ test_that("row_number handles empty data frames (#762)", {
 
 test_that("hybrid rank functions handle NA (#4427)", {
   df <- tibble(a = runif(1000, -1, 1), b = runif(1000, -1, 1))
-  df[df < 0] <- NA
+  df$a[df$a < 0] <- NA
+  df$b[df$b < 0] <- NA
   df <- df %>%
     mutate(
       gain = b - a,
