d8e91f6250ce2071520c9408104fccd97d91049f
Romain Francois <romain@rstudio.com>
2019-10-09 15:13:33 +0200

    pick() returns a data frame

--- R/each.R
+++ R/each.R
@@ -37,11 +37,6 @@ colwise <- function(fun, .name = "{var}") {
 }
 
 #' @export
-pick <- function(...) {
-  vars_select(peek_vars(), ...)
-}
-
-#' @export
 mapping <- function(vars, fun, ..., .name = "{var}") {
   fun <- as_function(fun)
   names(vars) <- glue::glue(.name, var = names(vars), idx = seq_along(vars))
@@ -51,14 +46,19 @@ mapping <- function(vars, fun, ..., .name = "{var}") {
 }
 
 #' @export
+pick <- function(...) {
+  vars <- vars_select(peek_vars(), ...)
+  quo <- quo(tibble(!!!syms(vars)))
+  peek_mask()$internal_eval(quo)
+}
+
+#' @export
 colwise <- function(fun, .name = "{var}") {
   fun <- as_function(fun)
-  function(...) {
-    vars <- pick(...)
-    names(vars) <- glue::glue(.name, var = names(vars), idx = seq_along(vars))
-
-    quo <- quo(tibble(!!!map(vars, function(.x) expr((!!fun)(!!sym(.x))))))
-    peek_mask()$internal_eval(quo)
+  function(df, ...) {
+    out <- map(df, fun, ...)
+    names(out) <- glue::glue(.name, var = names(out), idx = seq_len(ncol(df)))
+    tibble(!!!out)
   }
 }
 
