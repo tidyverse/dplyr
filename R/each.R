0403fcaa3bcc46f9a174839b473679f8b0653dda
Romain Francois <romain@rstudio.com>
2019-10-10 14:45:02 +0200

    added $pick() to DataMask class

--- R/each.R
+++ R/each.R
@@ -44,17 +44,6 @@ mapping <- function(df, fun, ..., .name = "{var}") {
 #' @export
 pick <- function(...) {
   vars <- vars_select(peek_vars(), ...)
-  quo <- quo(tibble(!!!syms(vars)))
-  peek_mask()$internal_eval(quo)
-}
-
-#' @export
-colwise <- function(fun, .name = "{var}") {
-  fun <- as_function(fun)
-  function(df, ...) {
-    out <- map(df, fun, ...)
-    names(out) <- glue::glue(.name, var = names(out), idx = seq_len(ncol(df)))
-    tibble(!!!out)
-  }
+  peek_mask()$pick(vars)
 }
 
