69965d73d03f2568ef7457585b545d6d3b8ad402
Romain Francois <romain@rstudio.com>
2019-11-19 09:49:51 +0100

    test updates

--- tests/testthat/test-mutate.r
+++ tests/testthat/test-mutate.r
@@ -217,9 +217,6 @@ test_that("mutate handles POSIXlt", {
     NA
   )
 
-test_that("mutate can handle POSIXlt columns (#3854)", {
-  skip("until https://github.com/tidyverse/tibble/pull/626")
-
   df <- data.frame(g=c(1,1,3))
   df$created <- strptime(c("2014/1/1", "2014/1/2", "2014/1/2"), format = "%Y/%m/%d")
 
