d98a8eabc39a0006ec1fdd166cbe5810a32f0220
Romain Francois <romain@rstudio.com>
2019-11-28 16:21:01 +0100

    Remove pick() and simplify documentation for across()

--- R/each.R
+++ R/each.R
@@ -1,144 +1,3 @@
-#' Select variables from the current group
-#'
-#' Select a tibble made of a tidy selection of columns
-#' and the rows of the current group
-#'
-#' @param ... tidy selection of columns
-#'
-#' @examples
-#' iris %>%
-#'   group_by(Species) %>%
-#'   summarise(sepal = head(pick(starts_with("Sepal")), 2L))
-#'
-#' @export
-pick <- function(...) {
-  mask <- peek_mask()
-  vars <- vars_select(peek_vars(), ...)
-  mask$pick(vars)
-}
-
-#' @importFrom tidyselect peek_vars vars_select
-#' @export
-by_column <- function(df, funs = identity) {
-  colwise(funs)(df)
-}
-
-#' Apply a set of functions to a set of columns
-#'
-#' Creates a data frame by applying a set of functions to a tidy
-#' selection of columns in the current slice
-#'
-#' @param select tidy selection of columns, forwarded to [pick()]
-#' @param funs Functions to apply to each of the selected columns. Possible
-#'   values are:
-#'
-#'   - A single function
-#'   - A single quosure style lambda, e.g. `~ mean(.x, na.rm = TRUE)`
-#'   - A named list of functions and/or lambdas
-#'
-#'  @param .name A [glue::glue()] pattern used to name the result columns.
-#'    When unspecified, the default naming depend on the other arguments
-#'
-#'  @param .unpack Whether to un pack the results in a flat data frame
-#'    or keep the results of each function packed together.
-#'
-#'  @return A tibble
-#'
-#' @examples
-#' # A single function, results are unpacked and named after the selected columns
-#' iris %>%
-#'   group_by(Species) %>%
-#'   summarise(across(starts_with("Sepal"), mean))
-#' iris %>%
-#'   group_by(Species) %>%
-#'   summarise(across(starts_with("Sepal"), ~mean(.x, na.rm = TRUE)))
-#'
-#'
-#' # If we leave things packed, both columns are packed in the "fn" column
-#' iris %>%
-#'   group_by(Species) %>%
-#'   summarise(across(starts_with("Sepal"), mean, .unpack = FALSE))
-#' iris %>%
-#'   group_by(Species) %>%
-#'   summarise(across(starts_with("Sepal"), ~mean(.x, na.rm = TRUE)), .unpack = FALSE))
-#'
-#' # we can have control on the name of pack either by using a named list of
-#' # functions or by giving a name to the expression in summarise()
-#' iris %>%
-#'   group_by(Species) %>%
-#'   summarise(packname = across(starts_with("Sepal"), mean))
-#'
-#' # With a named list, if there are multiple columns selected
-#' # and the results are unpacked, the default name includes both the
-#' # function name and the column name, separated by an underscore
-#' iris %>%
-#'   group_by(Species) %>%
-#'   summarise(
-#'     across(starts_with("Sepal"), list(mean = mean, sd = sd))
-#'   )
-#' # this is also the case when the list has only one function
-#' iris %>%
-#'   group_by(Species) %>%
-#'   summarise(
-#'     across(starts_with("Sepal"), list(mean = mean))
-#'   )
-#'
-#' # If there is only one selected column, the results are named
-#' # after the function names
-#' iris %>%
-#'   group_by(Species) %>%
-#'   summarise(
-#'     across("Sepal.Length", list(mean = mean, sd = sd))
-#'   )
-#'
-#' # When results are left packed, the packs are named after the functions
-#' # and the results within the packs are named after the columns
-#' iris %>%
-#'   group_by(Species) %>%
-#'   summarise(
-#'     across(starts_with("Sepal"), list(mean = mean, sd = sd), .unpack = FALSE)
-#'   )
-#'
-#' # The .name parameter can be used to overwrite the defaults as seen above
-#' # with a glue pattern that may use {fn} and {var} to stand for the current
-#' # function name and column
-#' iris %>%
-#'   group_by(Species) %>%
-#'   summarise(across(starts_with("Sepal"), mean, .name = "{toupper(var)}"))
-#'
-#' iris %>%
-#'   group_by(Species) %>%
-#'   summarise(packname = across(starts_with("Sepal"), mean, .name = "{toupper(var)}"))
-#'
-#' iris %>%
-#'   group_by(Species) %>%
-#'   summarise(
-#'     across(starts_with("Sepal"), list(mean = mean, sd = sd),
-#'             .name = "{toupper(fn)}_{sub('Sepal.', '', var)}"
-#'     )
-#'   )
-#'
-#' # overwritting the default so that the name of the column still appear
-#' # even though there is only one column selected
-#' iris %>%
-#'   group_by(Species) %>%
-#'   summarise(
-#'     across("Sepal.Length", list(mean = mean, sd = sd), .name = "{fn}_{var}")
-#'   )
-#'
-#' # when results are left packed, the default is to not include the function in the
-#' # names of columns that are packed
-#' iris %>%
-#'   group_by(Species) %>%
-#'   summarise(
-#'     across(starts_with("Sepal"), list(mean = mean, sd = sd), .unpack = FALSE, .name = "{fn}_{var}")
-#'   )
-#'
-#' @export
-across <- function(select, funs = identity) {
-  colwise(funs)(pick({{select}}))
-}
-
 #' @export
 current_key <- function() {
   peek_mask()$current_key()
@@ -159,8 +18,52 @@ current_column <- function() {
   context_env[["..current_column_name"]] %||% abort("No current column name registered, current_column() only makes sense inside across()")
 }
 
+#' Apply a function or a set of functions to a set of columns
+#'
+#' Creates a data frame by applying a set of functions to a tidy
+#' selection of columns in the current slice
+#'
+#' @param select tidy selection of columns, forwarded to [pick()]
+#' @param funs Functions to apply to each of the selected columns. Possible
+#'   values are:
+#'
+#'   - A single function or a single quosure style lambda, e.g. `~ mean(.x, na.rm = TRUE)`
+#'   - A named list of functions and/or lambdas
+#'
+#'  @return A tibble
+#'
+#'  @details
+#'
+#'  When a single function is given, it is applied to each of the selected columns
+#'  and the output columns are named after the selected input columns.
+#'
+#'  When a named list of functions is given, the result is made of as many columns
+#'  as the number of functions, each of these output columns is a pack, i.e.
+#'  a data frame column made from the result of the function applied across the
+#'  selected columns.
+#'
+#' @examples
+#'
+#' # A single function
+#' iris %>%
+#'   group_by(Species) %>%
+#'   summarise(across(starts_with("Sepal"), mean))
+#'
+#' iris %>%
+#'   group_by(Species) %>%
+#'   summarise(across(starts_with("Sepal"), ~mean(.x, na.rm = TRUE)))
+#'
+#' # a named list of functions
+#' iris %>%
+#'   group_by(Species) %>%
+#'   summarise(across(starts_with("Sepal"), list(mean = mean, sd = sd)))
+#'
 #' @export
-colwise <- function(funs = identity) {
+across <- function(select, funs = identity) {
+  mask <- peek_mask()
+  vars <- vars_select(peek_vars(), ...)
+  data <- mask$pick({{vars}})
+
   single_function <- is.function(funs) || is_formula(funs)
 
   if (single_function) {
@@ -172,7 +75,7 @@ colwise <- function(funs = identity) {
     funs <- map(funs, as_function)
   }
 
-  function(df) {
+  f <- function(df) {
     if (single_function) {
       as_tibble(imap(df, function(.x, .y) {
         old <- poke_current_column(.y)
@@ -190,4 +93,6 @@ colwise <- function(funs = identity) {
       tibble(!!!results)
     }
   }
+
+  f(data)
 }
