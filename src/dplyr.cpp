30dc1f094ddc4d69d4565045ba8708deb0caaffd
Romain Francois <romain@rstudio.com>
2019-12-12 18:39:33 +0100

    astyle()

--- src/dplyr.cpp
+++ src/dplyr.cpp
@@ -735,11 +735,11 @@ SEXP dplyr_group_indices(SEXP data, SEXP s_nr) {
 
   SEXP indices = PROTECT(Rf_allocVector(INTSXP, nr));
   int* p_indices = INTEGER(indices);
-  for (R_xlen_t i=0; i<ng; i++) {
+  for (R_xlen_t i = 0; i < ng; i++) {
     SEXP rows_i = VECTOR_ELT(rows, i);
     R_xlen_t n_i = XLENGTH(rows_i);
     int* p_rows_i = INTEGER(rows_i);
-    for (R_xlen_t j=0; j<n_i; j++, ++p_rows_i) {
+    for (R_xlen_t j = 0; j < n_i; j++, ++p_rows_i) {
       p_indices[*p_rows_i - 1] = i + 1;
     }
   }
