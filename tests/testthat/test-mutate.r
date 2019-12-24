9128dddd245592092e68703d8cbc181b492ed69b
Kirill Müller <krlmlr@users.noreply.github.com>
2019-12-24 16:15:24 +0100

    WIP: Fix character encoding problems (#4453)
    * Un-skip tests
    
    * using obj_addrs() instead of strings_addresses()
    
    Co-authored-by: Romain François <romain@purrple.cat>

--- tests/testthat/test-mutate.r
+++ tests/testthat/test-mutate.r
@@ -489,7 +489,7 @@ test_that("hybrid rank functions handle NA (#4427)", {
 })
 
 test_that("no utf8 invasion (#722)", {
-  skip("fails on windows, but also on one cran machine")
+  skip_if_not(l10n_info()$"UTF-8")
 
   source("utf-8.txt", local = TRUE, encoding = "UTF-8")
 })
