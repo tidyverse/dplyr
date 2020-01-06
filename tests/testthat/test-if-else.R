808cbbcd13fb55f2292c11dd1626d986954a050d
Romain Francois <romain@rstudio.com>
2020-01-06 21:12:11 +0100

    use verify_output(). Remove some tests that are tracked in https://github.com/r-lib/funs/issues/37 for the next version of if_else()

--- tests/testthat/test-if-else.R
+++ tests/testthat/test-if-else.R
@@ -1,42 +1,5 @@
 context("if_else")
 
-test_that("first argument must be logical", {
-  expect_error(
-    if_else(1:10, 1, 2),
-    "`condition` must be a logical vector, not an integer vector",
-    fixed = TRUE
-  )
-})
-
-test_that("true and false must be same length as condition (or length 1)", {
-  expect_error(
-    if_else(1:3 < 2, 1:2, 1:3),
-    "`true` must be length 3 (length of `condition`) or one, not 2",
-    fixed = TRUE
-  )
-  expect_error(
-    if_else(1:3 < 2, 1:3, 1:2),
-    "`false` must be length 3 (length of `condition`) or one, not 2",
-    fixed = TRUE
-  )
-})
-
-test_that("true and false must be same type and same class", {
-  expect_error(
-    if_else(1:3 < 2, 1, 1L),
-    "`false` must be a double vector, not an integer vector",
-    fixed = TRUE
-  )
-
-  x <- factor("x")
-  y <- ordered("x")
-  expect_error(
-    if_else(1:3 < 2, x, y),
-    "`false` must have class `factor`, not class `ordered/factor`",
-    fixed = TRUE
-  )
-})
-
 test_that("scalar true and false are vectorised", {
   x <- c(TRUE, TRUE, FALSE, FALSE)
   expect_equal(if_else(x, 1, 2), c(1, 1, 2, 2))
@@ -62,74 +25,18 @@ test_that("works with lists", {
   )
 })
 
-test_that("better factor support (#2197)", {
-  skip("Currently failing")
+# Errors ------------------------------------------------------------------
 
-  test_that("gives proper error messages for factor class (#2197)", {
-    x <- factor(1:3, labels = letters[1:3])
+test_that("if_else() give meaningful errors", {
+  verify_output(test_path("test-if-else-errors.txt"), {
 
-    expect_error(
-      if_else(x == "a", "b", x),
-      "asdf",
-      fixed = TRUE
-    )
-    expect_error(
-      if_else(x == "a", 1L, x),
-      "asdf",
-      fixed = TRUE
-    )
-    expect_error(
-      if_else(x == "a", 1., x),
-      "asdf",
-      fixed = TRUE
-    )
-    expect_error(
-      if_else(x == "a", TRUE, x),
-      "asdf",
-      fixed = TRUE
-    )
-    expect_error(
-      if_else(x == "a", Sys.Date(), x),
-      "asdf",
-      fixed = TRUE
-    )
+    if_else(1:10, 1, 2)
+    if_else(1:3 < 2, 1:2, 1:3)
+    if_else(1:3 < 2, 1:3, 1:2)
+    if_else(1:3 < 2, 1, 1L)
 
-    expect_error(
-      if_else(x == "a", x, "b"),
-      "asdf",
-      fixed = TRUE
-    )
-    expect_error(
-      if_else(x == "a", x, 1L),
-      "asdf",
-      fixed = TRUE
-    )
-    expect_error(
-      if_else(x == "a", x, 1.),
-      "asdf",
-      fixed = TRUE
-    )
-    expect_error(
-      if_else(x == "a", x, TRUE),
-      "asdf",
-      fixed = TRUE
-    )
-    expect_error(
-      if_else(x == "a", x, Sys.Date()),
-      "asdf",
-      fixed = TRUE
-    )
-  })
-
-  test_that("works with factors as both `true` and `false` (#2197)", {
-    x <- factor(1:3, labels = letters[1:3])
-    y <- factor(1:3, labels = letters[c(1, 2, 4)])
-
-    expect_equal(if_else(x == "a", x[[2]], x), x[c(2, 2, 3)])
-
-    expect_error(
-      if_else(x == "a", x, y),
-      "asdf levels in `false` don't match levels in `true`"
-    )
+    x <- factor("x")
+    y <- ordered("x")
+    if_else(1:3 < 2, x, y)
   })
 })
