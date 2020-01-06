72a2eb66452a3425b31976a80c4ade971d853353
Romain Francois <romain@rstudio.com>
2020-01-06 14:12:29 +0100

    More verify_output()

--- tests/testthat/test-group_data.R
+++ tests/testthat/test-group_data.R
@@ -12,6 +12,7 @@ test_that("group_data() returns a tidy tibble (#3489)", {
 
   expect_is(group_data(df), "tbl_df")
   expect_equal(names(group_data(df)), ".rows")
+
   expect_equivalent(group_rows(df), list(1:4))
 
   gd <- group_by(df,x)
@@ -48,36 +49,37 @@ test_that("group_rows and group_data work with 0 rows data frames (#3489)", {
   )
 })
 
-test_that("GroupDataFrame checks the structure of the groups attribute", {
-  df <- group_by(tibble(x = 1:4, g = rep(1:2, each = 2)), g)
-  groups <- attr(df, "groups")
+# Errors ------------------------------------------------------------------
+
+test_that("group_data() give meaningful errors", {
+  df1 <- group_by(tibble(x = 1:4, g = rep(1:2, each = 2)), g)
+  groups <- attr(df1, "groups")
   groups[[2]] <- 1:2
-  attr(df, "groups") <- groups
-  expect_error(group_data(df), "The `groups` attribute is not a data frame with its last column called `.rows`", class = "dplyr_grouped_df_corrupt")
+  attr(df1, "groups") <- groups
 
-  df <- group_by(tibble(x = 1:4, g = rep(1:2, each = 2)), g)
-  groups <- attr(df, "groups")
+  df2 <- group_by(tibble(x = 1:4, g = rep(1:2, each = 2)), g)
+  groups <- attr(df2, "groups")
   names(groups) <- c("g", "not.rows")
-  attr(df, "groups") <- groups
-  expect_error(group_data(df), "The `groups` attribute is not a data frame with its last column called `.rows`", class = "dplyr_grouped_df_corrupt")
+  attr(df2, "groups") <- groups
 
-  attr(df, "groups") <- tibble()
-  expect_error(group_data(df), "The `groups` attribute is not a data frame with its last column called `.rows`", class = "dplyr_grouped_df_corrupt")
+  df3 <- df2
+  attr(df3, "groups") <- tibble()
 
-  attr(df, "groups") <- NA
-  expect_error(group_data(df), "The `groups` attribute is not a data frame with its last column called `.rows`", class = "dplyr_grouped_df_corrupt")
-})
+  df4 <- df3
+  attr(df4, "groups") <- NA
 
-test_that("older style grouped_df is no longer supported", {
-  df <- tibble(x = 1:4, g = rep(1:2, each = 2))
-  attr(df, "vars") <- "g"
-  attr(df, "class") <- c("grouped_df", "tbl_df", "tbl", "data.frame")
-  expect_error(validate_grouped_df(df), class = "dplyr_grouped_df_corrupt")
+  df5 <- tibble(x = 1:4, g = rep(1:2, each = 2))
+  attr(df5, "vars") <- "g"
+  attr(df5, "class") <- c("grouped_df", "tbl_df", "tbl", "data.frame")
 
-  df <- structure(
-    data.frame(x=1),
-    class = c("grouped_df", "tbl_df", "tbl", "data.frame"),
-    vars = list(sym("x"))
-  )
-  expect_error(validate_grouped_df(df), class = "dplyr_grouped_df_corrupt")
+  verify_output(test_path("test-group_data.txt"), {
+    "# Invalid `groups` attribute"
+    group_data(df1)
+    group_data(df2)
+    group_data(df3)
+    group_data(df4)
+
+    "# Older style grouped_df"
+    validate_grouped_df(df5)
+  })
 })
