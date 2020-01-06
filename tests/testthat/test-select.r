c5b766e764d103a4c18c5a54630a23c812d0da7a
Romain Francois <romain@rstudio.com>
2020-01-06 12:25:25 +0100

    use verify_output() for rename() errors

--- tests/testthat/test-select.r
+++ tests/testthat/test-select.r
@@ -148,3 +148,16 @@ test_that("select works on NA names (#3601)", {
   colnames(df) <- c(NA,"y","z")
   expect_identical(select(df, y)$y, 2)
 })
+
+
+# Errors ------------------------------------------------------------------
+
+test_that("rename errors with invalid grouped data frame (#640)", {
+  df <- tibble(a = 1:3, b = 2:4, d = 3:5) %>% group_by(a, b)
+  df$a <- NULL
+
+  verify_output(test_path("test-rename-errors.txt"), {
+    df %>% rename(e = d)
+    df %>% rename(e = b)
+  })
+})
