18556c0e77f2bde6d4e9ddee0814de1bd781ab13
Romain Francois <romain@rstudio.com>
2020-01-06 08:23:11 +0100

    merge two skipped tests

--- tests/testthat/test-rbind.R
+++ tests/testthat/test-rbind.R
@@ -58,10 +58,7 @@ test_that("bind_rows can handle lists (#1104)", {
   expect_equal(nrow(res), 2L)
   expect_is(res$x, "numeric")
   expect_is(res$y, "character")
-})
 
-test_that("bind_rows can handle lists (#1104)", {
-  skip("to be discussed")
   res <- bind_rows(list(x = 1, y = "a"), list(x = 2, y = "b"))
   expect_equal(nrow(res), 2L)
   expect_is(res$x, "numeric")
