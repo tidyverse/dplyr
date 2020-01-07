a1afe287dcfa9a180d9c83ee7920be9507b086ec
Hadley Wickham <h.wickham@gmail.com>
2020-01-07 11:47:03 -0600

    Use class name when testing error

--- tests/testthat/test-grouped-df.r
+++ tests/testthat/test-grouped-df.r
@@ -4,7 +4,10 @@ test_that("can selectively ungroup", {
   expect_equal(gf %>% ungroup() %>% group_vars(), character())
   expect_equal(gf %>% ungroup(everything()) %>% group_vars(), character())
   expect_equal(gf %>% ungroup(x) %>% group_vars(), "y")
-  expect_error(gf %>% ungroup(z) %>% group_vars(), "z")
+  expect_error(
+    gf %>% ungroup(z) %>% group_vars(),
+    class = "tidyselect_error_index_oob_names"
+  )
 })
 
 
