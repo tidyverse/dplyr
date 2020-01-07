db9e3f3b9a27e91aaa636ef929968db4cbc7a59e
Hadley Wickham <h.wickham@gmail.com>
2020-01-07 09:40:31 -0600

    Document id() and failwith() together

--- R/defunct.r
+++ R/defunct.r
@@ -1,6 +1,7 @@
 #' Defunct functions
 #'
 #' \Sexpr[results=rd, stage=render]{lifecycle::badge("defunct")}
+#' Executing these functions will tell you which function replaces them.
 #'
 #' @keywords internal
 #' @name defunct
@@ -10,4 +11,10 @@ NULL
 #' @rdname defunct
 id <- function(.variables, drop = FALSE) {
   lifecycle::deprecate_stop("0.5.0", "id()", "vctrs::vec_group_id()")
+}
+
+#' @export
+#' @rdname defunct
+failwith <- function(default = NULL, f, quiet = FALSE) {
+  lifecycle::deprecate_stop("0.7.0", "failwith()", "purrr::possibly()")
 }
