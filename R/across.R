8fd961aec2bd116fd79eec0186dd50b02d5f8807
Hadley Wickham <h.wickham@gmail.com>
2020-01-07 07:37:12 -0600

    across() tweaks
    * Improve docs a little
    * Rename `select` to `cols`
    * Faster NULL case
    * Move n(), current_key() and current_columns() to context.R
    * Start file of tests for across()
    
    cc @romainfrancois

--- R/across.R
+++ R/across.R
@@ -1,86 +1,64 @@
-#' @rdname across
-#' @export
-current_key <- function() {
-  peek_mask()$current_key()
-}
-
-set_current_column <- function(name) {
-  context_env[["..current_column_name"]] <- name
-}
-
-poke_current_column <- function(name) {
-  old <- context_env[["..current_column_name"]]
-  set_current_column(name)
-  old
-}
-
-#' @rdname across
-#' @export
-current_column <- function() {
-  context_env[["..current_column_name"]] %||% abort("No current column name registered, current_column() only makes sense inside across()")
-}
-
-#' Apply a function or a set of functions to a set of columns
+#' Apply a function (or a set of functions) to a set of columns
 #'
-#' Creates a data frame by applying a set of functions to a tidy
-#' selection of columns in the current slice
+#' `across()` makes it easy to apply the same transformation to multiple
+#' columns, allowing you to use [select()] semantics inside in [summarise()] and
+#' [mutate()]. `across()` replaces the family of "scoped variants" like
+#' `summarise_at()`, `summarise_if()`, and `summarise_all()`.
 #'
-#' This allows you to use [select()] semantics inside in [summarise()] and [mutate()]
+#' @param cols <[`tidy-select`][dplyr_tidy_select]> Columns to transform.
+#'   Because `across()` is used within functions like `summarise()` and
+#'   `mutate()`, you can't grouping variables.
+#' @param fns Functions to apply to each of the selected columns.
+#'   Possible values are:
 #'
-#' @param select tidy selection of columns
-#' @param fns Functions to apply to each of the selected columns. Possible
-#'   values are:
+#'   - `NULL`, to returns the columns untransformed.
+#'   - A function, e.g. `mean`.
+#'   - A purrr-style lambda, e.g. `~ mean(.x, na.rm = TRUE)`
+#'   - A named list of functions/lambdas, e.g.
+#'     `list(mean = mean, n_miss = ~ sum(is.na(.x))`
 #'
-#'   - A single function or a single quosure style lambda, e.g. `~ mean(.x, na.rm = TRUE)`
-#'   - A named list of functions and/or lambdas
+#'   Within these functions you can use [current_column()] and [current_key()]
+#'   to access the current column and grouping keys respectively.
+#' @returns A tibble.
 #'
-#' @return A tibble
+#'   When `fns` is a single function, it will have one column for each
+#'   column in `cols`
 #'
-#' @details
-#'
-#' When a single function is given, it is applied to each of the selected columns
-#' and the output columns are named after the selected input columns.
-#'
-#' When a named list of functions is given, the result is made of as many columns
-#' as the number of functions, each of these output columns is a pack, i.e.
-#' a data frame column made from the result of the function applied across the
-#' selected columns.
-#'
-#' `current_column()` gives the name of the column currently being processed.
-#'
-#' `current_key()` return metadata about the "current" element: a single row tibble, with one column per grouping variable
-#'
+#'   When `fns` is a named list, it will have one column for each element
+#'   of `fns`. Each column will be a df-column that contains one column
+#'   for each column in `cols`.
 #' @examples
-#'
-#' # A single function
+#' # A function
 #' iris %>%
 #'   group_by(Species) %>%
 #'   summarise(across(starts_with("Sepal"), mean))
+#' iris %>%
+#'   as_tibble() %>%
+#'   mutate(across(is.factor, as.character))
 #'
+#' # A purrr-style formula
 #' iris %>%
 #'   group_by(Species) %>%
 #'   summarise(across(starts_with("Sepal"), ~mean(.x, na.rm = TRUE)))
 #'
-#' # a named list of functions
+#' # A named list of functions
 #' iris %>%
 #'   group_by(Species) %>%
 #'   summarise(across(starts_with("Sepal"), list(mean = mean, sd = sd)))
-#'
-#' @importFrom tidyselect eval_select
 #' @export
-across <- function(select, fns = identity) {
+across <- function(cols, fns = NULL) {
   mask <- peek_mask()
   data <- mask$full_data()
 
-  vars <- eval_select(
-    expr({{select}}),
+  vars <- tidyselect::eval_select(
+    expr({{ cols }}),
     data[, setdiff(names(data), group_vars(data))]
   )
   data <- mask$pick(names(vars))
 
-  single_function <- is.function(fns) || is_formula(fns)
-
-  if (single_function) {
+  if (is.null(fns)) {
+    data
+  } else if (is.function(fns) || is_formula(fns)) {
     fns <- as_function(fns)
 
     as_tibble(imap(data, function(.x, .y) {
@@ -88,10 +66,7 @@ across <- function(select, fns = identity) {
       on.exit(set_current_column(old))
       fns(.x)
     }))
-  } else {
-    if (is.null(names(fns))) {
-      abort("funs should be a single function, a single formula, or a named list of functions or formulas")
-    }
+  } else if (is.list(fns) && is_named(fns)) {
     fns <- map(fns, as_function)
 
     as_tibble(map(fns, function(f) {
@@ -101,5 +76,7 @@ across <- function(select, fns = identity) {
         f(.x)
       }))
     }))
+  } else {
+    abort("`fns` must be NULL, a function, a formula, or a named list of functions/formulas")
   }
 }
--- R/context.R
+++ R/context.R
@@ -1,3 +1,56 @@
+#' Context dependent expressions
+#'
+#' @description
+#' These functions return information about the "current" group or "current"
+#' variable, so only work inside specific contexts
+#'
+#' * `n()` gives the current group size.
+#' * `current_key()` gives the group keys, a tibble with one row and one column
+#'   for each grouping variable.
+#' * `current_column()` gives the current column being used in [across()].
+#'
+#' @examples
+#' df <- data.frame(g = rep(1:3, 1:3), x = runif(6), y = runif(6))
+#'
+#' df %>%
+#'   group_by(g) %>%
+#'   summarise(n = n())
+#'
+#' df %>%
+#'   group_by(g) %>%
+#'   mutate(across(everything(), ~ paste(current_column(), round(.x, 2))))
+#' @name context
+NULL
+
+#' @rdname context
+#' @export
+n <- function() {
+  from_context("..group_size")
+}
+
+#' @rdname context
+#' @export
+current_key <- function() {
+  peek_mask()$current_key()
+}
+
+#' @rdname context
+#' @export
+current_column <- function() {
+  context_env[["..current_column_name"]] %||%
+    abort("No current column name registered, current_column() only makes sense inside across()")
+}
+
+set_current_column <- function(name) {
+  context_env[["..current_column_name"]] <- name
+}
+
+poke_current_column <- function(name) {
+  old <- context_env[["..current_column_name"]]
+  set_current_column(name)
+  old
+}
+
 context_env <- new_environment()
 
 from_context <- function(what){
