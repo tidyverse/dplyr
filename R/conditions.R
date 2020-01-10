89b55af6bb4da1c34cbb240a93270617f6c4477c
Romain Francois <romain@rstudio.com>
2020-01-10 08:38:14 +0100

    factor out glue_c() and just call abort() instead of stop_dplyr()

--- R/conditions.R
+++ R/conditions.R
@@ -1,18 +1,17 @@
-stop_dplyr <- function(.subclass = character(), ..., .envir = parent.frame()) {
-  msg <- map_chr(c(...), glue, .envir = .envir)
-  abort(msg, .subclass = c(.subclass, "dplyr_error"))
+glue_c <- function(..., .envir = caller_env()) {
+  map_chr(c(...), glue, .envir = .envir)
 }
 
 stop_filter_incompatible_size <- function(index_expression, index_group, size, expected_size, data) {
-  stop_dplyr("dplyr_error_filter_incompatible_size",
+  abort(glue_c(
     "`filter()` argument `..{index_expression}` is incorrect",
     x = "It must be of size {expected_size} or 1, not size {size}",
     i = if(is_grouped_df(data)) "The error occured in group {index_group}"
-  )
+  ))
 }
 
 stop_filter_incompatible_type <- function(index_expression, index_column_name, index_group, result, data) {
-  stop_dplyr("dplyr_error_filter_type",
+  abort(glue_c(
     if (!is.null(index_column_name)) {
       "`filter()` argument `..{index_expression}${index_column_name}` is incorrect"
     } else {
@@ -20,24 +19,24 @@ stop_filter_incompatible_type <- function(index_expression, index_column_name, i
     },
     x = "It must be a logical vector, not a {vec_ptype_full(result)}",
     i = if(is_grouped_df(data)) "The error occured in group {index_group}"
-  )
+  ))
 }
 
 stop_filter_eval_tidy <- function(e, index_expression) {
   data  <- peek_mask()$full_data()
   group <- peek_mask()$get_current_group()
 
-  stop_dplyr("dplyr_error_filter_eval",
+  abort(glue_c(
     "`filter()` argument `..{index_expression}` errored",
     x = conditionMessage(e),
     i = if(is_grouped_df(data)) "The error occured in group {group}"
-  )
+  ))
 }
 
 stop_filter_named <- function(index, expr, name) {
-  stop_dplyr("dplyr_error_filter_named",
+  abort(glue_c(
     "`filter()` argument `..{index}` is named",
     i = "This usually means that you've used `=` instead of `==`",
     i = "Did you mean `{name} == {as_label(expr)}` ?"
-  )
+  ))
 }
