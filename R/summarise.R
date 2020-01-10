14f89e37e2febf135f587569aeec3b5da51fed1c
Romain Francois <romain@rstudio.com>
2020-01-10 14:02:34 +0100

    Also promote vctrs_error so that they have more context

--- R/summarise.R
+++ R/summarise.R
@@ -136,47 +136,49 @@ summarise_new_cols <- function(.data, ...) {
       # and maybe update .size
       .size <- .Call(`dplyr_validate_summarise_sizes`, .size, chunks[[i]])
 
-      },
-      simpleError = function(e) {
-        stop_eval_tidy(e, index = i, quo = quo, fn = "summarise")
-      },
-      dplyr_summarise_unsupported_type = function(cnd) {
-        stop_summarise_unsupported_type(result = cnd$result, index = i, quo = quo)
-      },
-      dplyr_summarise_incompatible_size = function(cnd) {
-        stop_incompatible_size(size = cnd$size, index = i, expected_sizes = .size, quo = quo)
+      result_type <- vec_ptype_common(!!!chunks[[i]])
+
+      if ((is.null(dots_names) || dots_names[i] == "") && is.data.frame(result_type)) {
+        # remember each result separately
+        map2(seq_along(result_type), names(result_type), function(j, nm) {
+          mask$add(nm, pluck(chunks[[i]], j))
+        })
+      } else {
+        # remember
+        mask$add(auto_named_dots[i], chunks[[i]])
       }
-    )
 
-    result_type <- vec_ptype_common(!!!chunks[[i]])
+      # materialize columns
+      for (i in seq_along(dots)) {
+        if (!identical(.size, 1L)) {
+          sizes <- .Call(`dplyr_vec_sizes`, chunks[[i]])
+          if (!identical(sizes, .size)) {
+            chunks[[i]] <- map2(chunks[[i]], .size, vec_recycle, x_arg = glue("..{i}"))
+          }
+        }
 
-    if ((is.null(dots_names) || dots_names[i] == "") && is.data.frame(result_type)) {
-      # remember each result separately
-      map2(seq_along(result_type), names(result_type), function(j, nm) {
-        mask$add(nm, pluck(chunks[[i]], j))
-      })
-    } else {
-      # remember
-      mask$add(auto_named_dots[i], chunks[[i]])
-    }
-  }
+        result <- vec_c(!!!chunks[[i]])
 
-  # materialize columns
-  for (i in seq_along(dots)) {
-    if (!identical(.size, 1L)) {
-      sizes <- .Call(`dplyr_vec_sizes`, chunks[[i]])
-      if (!identical(sizes, .size)) {
-        chunks[[i]] <- map2(chunks[[i]], .size, vec_recycle, x_arg = glue("..{i}"))
+        if ((is.null(dots_names) || dots_names[i] == "") && is.data.frame(result)) {
+          cols[names(result)] <- result
+        } else {
+          cols[[ auto_named_dots[i] ]] <-  result
+        }
       }
-    }
 
-    result <- vec_c(!!!chunks[[i]])
-
-    if ((is.null(dots_names) || dots_names[i] == "") && is.data.frame(result)) {
-      cols[names(result)] <- result
-    } else {
-      cols[[ auto_named_dots[i] ]] <-  result
-    }
+    },
+    vctrs_error = function(e) {
+      stop_eval_tidy(e, index = i, quo = quo, fn = "summarise")
+    },
+    simpleError = function(e) {
+      stop_eval_tidy(e, index = i, quo = quo, fn = "summarise")
+    },
+    dplyr_summarise_unsupported_type = function(cnd) {
+      stop_summarise_unsupported_type(result = cnd$result, index = i, quo = quo)
+    },
+    dplyr_summarise_incompatible_size = function(cnd) {
+      stop_incompatible_size(size = cnd$size, index = i, expected_sizes = .size, quo = quo)
+    })
   }
 
   list(new = cols, size = .size)
