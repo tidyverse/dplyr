6b3fe5dd1a8076afdb920a8e571fe9fdb8732237
Romain Francois <romain@rstudio.com>
2020-01-12 13:47:45 +0100

    additional test where the recycling size is different across groups.

--- tests/testthat/test-summarise.r
+++ tests/testthat/test-summarise.r
@@ -1024,6 +1024,12 @@ test_that("summarise() recycles", {
     tibble(a = 1:2) %>% group_by(a) %>% summarise(x = 1, y = 1:3, z = 1),
     tibble(a = rep(1:2, each = 3), x = 1, y = c(1:3, 1:3), z = 1)
   )
+  expect_equal(
+    tibble(a = 1:2) %>%
+      group_by(a) %>%
+      summarise(x = seq_len(a), y = 1),
+    tibble(a = c(1L, 2L, 2L), x = c(1L, 1L, 2L), y = 1)
+  )
 })
 
 test_that("summarise() give meaningful errors", {
