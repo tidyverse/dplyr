2af745b41223d84760ab268c3956ac3c215801fc
Hadley Wickham <h.wickham@gmail.com>
2020-01-16 17:08:12 -0600

    Remove old deprecation warnings

--- tests/testthat/test-deprec-lazyeval.R
+++ tests/testthat/test-deprec-lazyeval.R
@@ -185,46 +185,6 @@ test_that("group_by_ works", {
     group_by_(df, .dots = list(~ -a)),
     group_by(df, -a)
   )
-
-  expect_warning(
-    expect_equal(
-      group_by_(df %>% rowwise(), ~ a),
-      group_by(df %>% rowwise(), a)
-    ),
-    "rowwise"
-  )
-
-  expect_warning(
-    expect_equal(
-      group_by_(df %>% rowwise(), ~ -a),
-      group_by(df %>% rowwise(), -a)
-    ),
-    "rowwise"
-  )
-
-  expect_warning(
-    expect_equal(
-      group_by_(df %>% rowwise(), .dots = "a"),
-      group_by(df %>% rowwise(), a)
-    ),
-    "rowwise"
-  )
-
-  expect_warning(
-    expect_equal(
-      group_by_(df %>% rowwise(), .dots = list(quote(-a))),
-      group_by(df %>% rowwise(), -a)
-    ),
-    "rowwise"
-  )
-
-  expect_warning(
-    expect_equal(
-      group_by_(df %>% rowwise(), .dots = list(~ -a)),
-      group_by(df %>% rowwise(), -a)
-    ),
-    "rowwise"
-  )
 })
 
 test_that("mutate_ works", {
