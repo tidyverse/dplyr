668bc3b8756edf67114e2ffb310c75cc835ea805
Hadley Wickham <h.wickham@gmail.com>
2020-01-16 17:27:10 -0600

    Test that rowwiseness is preserved

--- tests/testthat/test-rowwise.r
+++ tests/testthat/test-rowwise.r
@@ -1,3 +1,35 @@
+test_that("rowwise status preserved by major verbs", {
+  rf <- rowwise(tibble(x = 1:5, y = 5:1), "x")
+
+  out <- arrange(rf, y)
+  expect_s3_class(out, "rowwise_df")
+  expect_equal(group_vars(out), "x")
+
+  out <- filter(rf, x < 3)
+  expect_s3_class(out, "rowwise_df")
+  expect_equal(group_vars(out), "x")
+
+  out <- mutate(rf, x = x + 1)
+  expect_s3_class(out, "rowwise_df")
+  expect_equal(group_vars(out), "x")
+
+  out <- rename(rf, X = x)
+  expect_s3_class(out, "rowwise_df")
+  expect_equal(group_vars(out), "X")
+
+  out <- select(rf, "x")
+  expect_s3_class(out, "rowwise_df")
+  expect_equal(group_vars(out), "x")
+
+  out <- slice(rf, c(1, 1))
+  expect_s3_class(out, "rowwise_df")
+  expect_equal(group_vars(out), "x")
+
+  out <- summarise(rf, z = mean(x, y))
+  expect_s3_class(out, "rowwise_df")
+  expect_equal(group_vars(out), "x")
+})
+
 test_that("rowwise nature preserved by subsetting ops", {
   rf <- rowwise(tibble(x = 1:5, y = 1:5), "x")
 
