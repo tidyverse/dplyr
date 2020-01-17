f4d5fba9a0d5b630308be71a54cda5805928c718
Hadley Wickham <h.wickham@gmail.com>
2020-01-17 15:56:55 -0600

    Fix buglet; better examples

--- R/morph.R
+++ R/morph.R
@@ -1,11 +1,10 @@
 #' @examples
-#' mtcars %>% morph(vs = vs + am)
-#'
-#'
+#' mtcars %>% as_tibble() %>% morph(vs2 = vs + am, mpgcyl = paste0(mpg, cyl))
+#' mtcars %>% as_tibble() %>% morph(vs = vs + am)
 morph <- function(.data, ...) {
   cols <- mutate_cols(.data, ..., .track_usage = TRUE)
 
-  used <- names(.data)[attr(cols, "usage")]
+  used <- setdiff(names(.data)[attr(cols, "usage")], names(cols))
   cols[used] <- rep_along(used, list(NULL))
 
   dplyr_col_modify(.data, cols)
