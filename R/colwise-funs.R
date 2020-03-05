566f82dda70fb3d5d4058e6b5e05ddd5734e307c
Kirill Müller <krlmlr+r@mailbox.org>
2020-03-05 05:41:26 +0100

    Soft-deprecation message for using funs() with a quosure

--- R/colwise-funs.R
+++ R/colwise-funs.R
@@ -24,10 +24,12 @@ as_fun_list <- function(.funs, .env, ...) {
   funs <- map(.funs, function(.x){
     if (is_formula(.x)) {
       if (is_quosure(.x)) {
-        warn(c(
-          "Using quosures if funs() is deprecated",
-          "Please use a one-sided formula, a function, or a function name"
-        ))
+        lifecycle::deprecate_soft(
+          "1.0.0",
+          "dplyr::funs(... = 'can\\'t be a quosure')",
+          details = "Please use a one-sided formula, a function, or a function name",
+          env = .env
+        )
         .x <- new_formula(NULL, quo_squash(.x), quo_get_env(.x))
       }
       .x <- as_inlined_function(.x, env = .env)
