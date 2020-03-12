219120b09e192e2be5d5f497ef2f741ba80a5b54
Romain Francois <romain@rstudio.com>
2020-03-12 14:39:31 +0100

    recalculate which_used on removes.
    closes #4974

--- R/data-mask.R
+++ R/data-mask.R
@@ -92,7 +92,7 @@ DataMask <- R6Class("DataMask",
     remove = function(name) {
       pos <- which(names(private$resolved) == name)
       private$resolved[[name]] <- NULL
-      private$which_used <- setdiff(private$which_used, pos)
+      private$which_used <- which(!map_lgl(private$resolved, is.null))
       rm(list = name, envir = private$bindings)
     },
 
