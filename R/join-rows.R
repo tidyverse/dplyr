ebe3de053b3bb05eda77cf3f5142b460f29aa522
Romain François <romain@rstudio.com>
2020-03-26 13:46:11 +0100

    join_rows() giving meaningful error message (#5025)
    * join_rows() giving meaningful message instead of factory message from vec_match()
    
    close #5012
    
    * Distance from needles and haystack, use a regex instead.

--- R/join-rows.R
+++ R/join-rows.R
@@ -3,7 +3,21 @@ join_rows <- function(x_key, y_key, type = c("inner", "left", "right", "full"), 
 
   # Find matching rows in y for each row in x
   y_split <- vec_group_loc(y_key)
-  matches <- vec_match(x_key, y_split$key, na_equal = na_equal)
+  tryCatch(
+    matches <- vec_match(x_key, y_split$key, na_equal = na_equal),
+    vctrs_error_incompatible_type = function(cnd) {
+      rx <- "^[^$]+[$]"
+      x_name <- sub(rx, "", cnd$x_arg)
+      y_name <- sub(rx, "", cnd$y_arg)
+
+      abort(c(
+        glue("Can't join on `x${x_name}` x `y${y_name}` because of incompatible types. "),
+        i = glue("`x${x_name}` is of type <{x_type}>>.", x_type = vec_ptype_full(cnd$x)),
+        i = glue("`y${y_name}` is of type <{y_type}>>.", y_type = vec_ptype_full(cnd$y))
+      ))
+    }
+  )
+
   y_loc <- y_split$loc[matches]
 
   if (type == "left" || type == "full") {
