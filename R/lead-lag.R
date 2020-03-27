f21b84e0c0f34e8832b98483365e91afbd37fc09
Romain François <romain@rstudio.com>
2020-03-27 10:48:00 +0100

    use vec_sze() instead of length() in lead() and lag() (#5050)
    close #5028

--- R/lead-lag.R
+++ R/lead-lag.R
@@ -60,7 +60,7 @@ lag <- function(x, n = 1L, default = NA, order_by = NULL, ...) {
   }
   if (n == 0) return(x)
 
-  xlen <- length(x)
+  xlen <- vec_size(x)
   n <- pmin(n, xlen)
 
   vec_c(
@@ -83,7 +83,7 @@ lead <- function(x, n = 1L, default = NA, order_by = NULL, ...) {
   }
   if (n == 0) return(x)
 
-  xlen <- length(x)
+  xlen <- vec_size(x)
   n <- pmin(n, xlen)
 
   vec_c(
