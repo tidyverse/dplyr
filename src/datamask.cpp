97b1e1a8bbcad37ff594e5099404f8f5b507a6b6
Romain Francois <romain@rstudio.com>
2020-04-07 21:39:06 +0200

    get the STRING_PTR() once

--- src/datamask.cpp
+++ src/datamask.cpp
@@ -4,12 +4,13 @@ SEXP dplyr_promises_args(SEXP names_bindings, SEXP promise_fn) {
   PROTECT_INDEX ipx;
   SEXP args = R_NilValue;
   PROTECT_WITH_INDEX(args, &ipx);
+  SEXP* p_names_bindings = STRING_PTR(names_bindings);
 
   for (R_len_t n = LENGTH(names_bindings); n > 0; n--) {
     SEXP i = PROTECT(Rf_ScalarInteger(n));
     SEXP call = PROTECT(Rf_lang2(promise_fn, i));
     REPROTECT(args = Rf_cons(call, args), ipx);
-    SET_TAG(args, Rf_installChar(STRING_ELT(names_bindings, n-1)));
+    SET_TAG(args, Rf_installChar(p_names_bindings[n-1]));
     UNPROTECT(2);
   }
   UNPROTECT(1);
