3f94d52107cde1f3ca8efdf841800e42b736a4d7
Hadley Wickham <h.wickham@gmail.com>
2020-04-17 08:03:04 -0500

    Tweak group_split tests()

--- tests/testthat/test-group_split.R
+++ tests/testthat/test-group_split.R
@@ -3,28 +3,23 @@ context("group_split")
 test_that("group_split() keeps the grouping variables by default", {
   tbl <- tibble(x = 1:4, g = factor(rep(c("a", "b"), each = 2)))
   res <- group_split(tbl, g)
-  expect_equivalent(res, list(tbl[1:2,], tbl[3:4,]))
-  expect_is(res, "vctrs_list_of")
-  expect_equal(attr(res, "ptype"), tibble(x = integer(), g = factor(levels = c("a", "b"))))
+
+  expect_equal(res, list_of(tbl[1:2,], tbl[3:4,]))
 })
 
 test_that("group_split() can discard the grouping variables with keep = FALSE", {
   tbl <- tibble(x = 1:4, g = factor(rep(c("a", "b"), each = 2)))
   res <- group_split(tbl, g, keep = FALSE)
-  expect_equivalent(res, list(tbl[1:2, 1, drop = FALSE], tbl[3:4,1, drop = FALSE]))
-  expect_is(res, "vctrs_list_of")
-  expect_equal(attr(res, "ptype"), tibble(x = integer()))
+  expect_equal(res, list_of(tbl[1:2, 1, drop = FALSE], tbl[3:4,1, drop = FALSE]))
 })
 
 test_that("group_split() respects empty groups", {
   tbl <- tibble(x = 1:4, g = factor(rep(c("a", "b"), each = 2), levels = c("a", "b", "c")))
   res <- group_split(tbl, g)
-  expect_equivalent(res, list(tbl[1:2,], tbl[3:4,]))
-  expect_is(res, "vctrs_list_of")
-  expect_equal(attr(res, "ptype"), tibble(x = integer(), g = factor(levels = c("a", "b", "c"))))
+  expect_equal(res, list_of(tbl[1:2,], tbl[3:4,]))
 
   res <- group_split(tbl, g, .drop = FALSE)
-  expect_equivalent(res, list(tbl[1:2,], tbl[3:4,], tbl[integer(), ]))
+  expect_equal(res, list_of(tbl[1:2,], tbl[3:4,], tbl[integer(), ]))
 })
 
 test_that("group_split.grouped_df() warns about ...", {
@@ -55,11 +50,11 @@ test_that("group_split / bind_rows round trip", {
 })
 
 test_that("group_split() works if no grouping column", {
-  expect_equivalent(group_split(iris), list(iris))
+  expect_error(group_split(iris), NA)
 })
 
 test_that("group_split(keep=FALSE) does not try to remove virtual grouping columns (#4045)", {
-  iris3 <- iris[1:3,]
+  iris3 <- as_tibble(iris[1:3,])
   rows <- list(c(1L, 3L, 2L), c(3L, 2L, 3L))
   df <- new_grouped_df(
     iris3,
@@ -67,10 +62,10 @@ test_that("group_split(keep=FALSE) does not try to remove virtual grouping colum
   )
   res <- group_split(df, keep = FALSE)
 
-  expect_equivalent(
+  expect_equal(
     res,
-    list(iris3[rows[[1L]],], iris3[rows[[2L]],])
-    )
+    list_of(iris3[rows[[1L]],], iris3[rows[[2L]],])
+  )
 })
 
 test_that("group_split() respects .drop", {
