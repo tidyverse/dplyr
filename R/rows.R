24bea1032e398a7e69f2f09fda0c95617a163691
Kirill Müller <krlmlr+r@mailbox.org>
2020-04-23 13:02:33 +0200

    Tweak and document

--- R/rows.R
+++ R/rows.R
@@ -33,7 +33,27 @@
 #'   On mutable backends, same as `x` if `inplace = TRUE`.
 #'
 #' @name rows
-#' @example example/rows-d.R
+#' @examples
+#' data <- tibble(a = 1:3, b = letters[c(1:2, NA)], c = 0.5 + 0:2)
+#' data
+#'
+#' # Insert
+#' rows_insert(data, tibble(a = 4, b = "z"))
+#' try(rows_insert(data, tibble(a = 3, b = "z")))
+#'
+#' # Update
+#' rows_update(data, tibble(a = 2:3, b = "z"))
+#' rows_update(data, tibble(b = "z", a = 2:3), by = "a")
+#'
+#' # Variants: patch and upsert
+#' rows_patch(data, tibble(a = 2:3, b = "z"))
+#' rows_upsert(data, tibble(a = 2:4, b = "z"))
+#'
+#' # Delete and truncate
+#' rows_delete(data, tibble(a = 2:4))
+#' rows_delete(data, tibble(a = 2:4, b = "b"))
+#' rows_delete(data, tibble(a = 2:4, b = "b"), by = c("a", "b"))
+#' rows_truncate(data)
 NULL
 
 
--- tests/testthat/test-rows.R
+++ tests/testthat/test-rows.R
@@ -1,16 +1,28 @@
 scoped_options(lifecycle_verbosity = "quiet")
 
-verify_output("out/rows-d.txt", {
+verify_output("test-rows.txt", {
   data <- tibble(a = 1:3, b = letters[c(1:2, NA)], c = 0.5 + 0:2)
   data
 
+  "# Insert"
   rows_insert(data, tibble(a = 4, b = "z"))
+  rows_insert(data, tibble(a = 3, b = "z"))
+
+  "# Update"
   rows_update(data, tibble(a = 2:3, b = "z"))
   rows_update(data, tibble(b = "z", a = 2:3), by = "a")
+
+  "# Variants: patch and upsert"
   rows_patch(data, tibble(a = 2:3, b = "z"))
   rows_upsert(data, tibble(a = 2:4, b = "z"))
+
+  "# Delete and truncate"
   rows_delete(data, tibble(a = 2:4))
   rows_delete(data, tibble(a = 2:4, b = "b"))
   rows_delete(data, tibble(a = 2:4, b = "b"), by = c("a", "b"))
   rows_truncate(data)
+
+  "# Errors"
+  rows_insert(data, tibble(d = 4))
+  rows_insert(data, tibble(a = 4, b = "z"), key = "e")
 })
