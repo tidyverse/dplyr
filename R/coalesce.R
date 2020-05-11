2c04355c3e15dcb5675cd5e19529a0ecc0f57928
Hadley Wickham <h.wickham@gmail.com>
2020-05-11 16:54:45 -0500

    Convert coalesece to use vctrs (#5187)
    Fixes #5186

--- R/coalesce.R
+++ R/coalesce.R
@@ -4,8 +4,9 @@
 #' at each position. This is inspired by the SQL `COALESCE` function
 #' which does the same thing for `NULL`s.
 #'
-#' @param ... <[`dynamic-dots`][rlang::dyn-dots]> Vectors. All inputs should either be length 1, or the
-#'   same length as the first argument.
+#' @param ... <[`dynamic-dots`][rlang::dyn-dots]> Vectors. Inputs should be
+#'    recyclable (either be length 1 or same length as the longest vector) and
+#'    coercible to a common type.
 #' @return A vector the same length as the first `...` argument with
 #'   missing values replaced by the first non-missing value.
 #' @seealso [na_if()] to replace specified values with a `NA`.
@@ -21,7 +22,7 @@
 #' z <- c(NA, NA, 3, 4, 5)
 #' coalesce(y, z)
 #'
-#' # Supply lists by splicing them into dots:
+#' # Supply lists by with dynamic dots
 #' vecs <- list(
 #'   c(1, 2, NA, NA, 5),
 #'   c(NA, NA, 3, 4, 5)
@@ -33,15 +34,15 @@ coalesce <- function(...) {
   }
 
   values <- list2(...)
+  values <- vec_cast_common(!!!values)
+  values <- vec_recycle_common(!!!values)
+
   x <- values[[1]]
   values <- values[-1]
 
   for (i in seq_along(values)) {
-    x <- replace_with(
-      x, is.na(x), values[[i]],
-      glue("Argument {i + 1}"),
-      glue("length of {fmt_args(~x)}")
-    )
+    x_miss <- is.na(x)
+    vec_slice(x, x_miss) <- vec_slice(values[[i]], x_miss)
   }
   x
 }
