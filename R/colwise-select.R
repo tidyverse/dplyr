09f61457059c4b00d8b9134cfa55f00255a14bf2
Lionel Henry <lionel.hry@gmail.com>
2020-05-20 19:46:58 +0200

    Use `where()` in remaining documentation (#5259)

--- R/colwise-select.R
+++ R/colwise-select.R
@@ -26,7 +26,7 @@
 #' is_whole <- function(x) all(floor(x) == x)
 #' mtcars %>% rename_if(is_whole, toupper)
 #' # ->
-#' mtcars %>% rename_with(toupper, is_whole)
+#' mtcars %>% rename_with(toupper, where(is_whole))
 #'
 #' mtcars %>% rename_at(vars(mpg:hp), toupper)
 #' # ->
@@ -41,7 +41,7 @@
 #' # Selection drops unselected variables:
 #' mtcars %>% select_if(is_whole, toupper)
 #' # ->
-#' mtcars %>% select(is_whole) %>% rename_with(toupper)
+#' mtcars %>% select(where(is_whole)) %>% rename_with(toupper)
 #'
 #' mtcars %>% select_at(vars(-contains("ar"), starts_with("c")), toupper)
 #' # ->
