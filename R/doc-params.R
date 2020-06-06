e9ccd304fde875c108a5a335f891414ef1496e0d
Salim B <salim@posteo.de>
2020-06-06 13:32:09 +0000

    Fix small typo

--- R/doc-params.R
+++ R/doc-params.R
@@ -3,7 +3,7 @@
 #' @description
 #' This page the describes the `<data-masking>` argument modifier which
 #' indicates that the argument uses tidy evaluation with **data masking**.
-#' If you've never head of tidy evaluation before, start with
+#' If you've never heard of tidy evaluation before, start with
 #' `vignette("programming")`.
 #'
 #' # Key terms
