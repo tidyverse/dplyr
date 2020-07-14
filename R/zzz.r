d7c1d910b2dd63e43a2b2a872255dfc4401e4d6f
Lionel Henry <lionel.hry@gmail.com>
2020-07-14 22:37:08 +0200

    Improve detection of modern order proxy

--- R/zzz.r
+++ R/zzz.r
@@ -1,3 +1,13 @@
+
+# FIXME: Temporary util until the API change from
+# https://github.com/r-lib/vctrs/pull/1155 has been on CRAN for long
+# enough to reasonably expect that users have upgraded
+dplyr_proxy_order <- function(x) NULL
+
+# Hack to pass CRAN check with vctrs 0.3.1,
+# where `vec_proxy_order()` doesn't exist
+utils::globalVariables("vec_proxy_order")
+
 .onLoad <- function(libname, pkgname) {
   op <- options()
   op.dplyr <- list(
@@ -6,8 +16,16 @@
   toset <- !(names(op.dplyr) %in% names(op))
   if (any(toset)) options(op.dplyr[toset])
 
-  ns <- ns_env("dplyr")
-  .Call(dplyr_init_library, ns_env())
+  if ("relax" %in% names(formals(vec_proxy_compare))) {
+    dplyr_proxy_order <<- function(x, ..., relax = TRUE) {
+      vec_proxy_compare(x, ..., relax = relax)
+    }
+  } else {
+    dplyr_proxy_order <<- vec_proxy_order
+  }
+
+  .Call(dplyr_init_library, ns_env("dplyr"))
+
   invisible()
 }
 
