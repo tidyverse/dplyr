b6e1f0f268b9a51af27864de96c2c6a26e0034a4
Romain Francois <romain@rstudio.com>
2020-09-02 09:28:15 +0200

    use na_if() and document()

--- R/na_if.R
+++ R/na_if.R
@@ -24,13 +24,13 @@
 #' y <- c("abc", "def", "", "ghi")
 #' na_if(y, "")
 #'
-#' # na_if is particularly useful inside mutate,
+#' # na_if() is particularly useful inside mutate(),
 #' # and is meant for use with vectors rather than entire data frames
 #' starwars %>%
 #'   select(name, eye_color) %>%
 #'   mutate(eye_color = na_if(eye_color, "unknown"))
 #'
-#' # na_if can also be used with mutate and across
+#' # na_if() can also be used with mutate() and across()
 #' # to mutate multiple columns
 #' starwars %>%
 #'    mutate(across(where(is.character), ~na_if(., "unknown")))
