4d1bee77e7e97c5b4bedf3c9c8fa0195625ce46d
Neal Richardson <neal.p.richardson@gmail.com>
2020-10-05 08:09:44 -0700

    Fix typo in inline comment

--- R/progress.R
+++ R/progress.R
@@ -100,7 +100,7 @@ Progress <- R6::R6Class("Progress",
     },
 
     print = function(...) {
-      if (!isTRUE(getOption("dplyr.show_progress")) || # user sepecifies no progress
+      if (!isTRUE(getOption("dplyr.show_progress")) || # user specifies no progress
         !interactive() || # not an interactive session
         !is.null(getOption("knitr.in.progress"))) { # dplyr used within knitr document
         return(invisible(self))
