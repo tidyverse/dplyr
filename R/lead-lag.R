a1a6635353fc5b6f12f9fdc60c5477a34e015b2a
Romain François <romain@rstudio.com>
2020-12-18 09:49:42 +0100

    lead() and lag() assert the size of default= (#5653)
    closes #5641

--- R/lead-lag.R
+++ R/lead-lag.R
@@ -60,6 +60,10 @@ lag <- function(x, n = 1L, default = NA, order_by = NULL, ...) {
   }
   if (n == 0) return(x)
 
+  if (vec_size(default) != 1L) {
+    abort(glue("`default` must be size 1, not size {vec_size(default)}"))
+  }
+
   xlen <- vec_size(x)
   n <- pmin(n, xlen)
 
@@ -84,6 +88,10 @@ lead <- function(x, n = 1L, default = NA, order_by = NULL, ...) {
     )
   }
   if (n == 0) return(x)
+
+  if (vec_size(default) != 1L) {
+    abort(glue("`default` must be size 1, not size {vec_size(default)}"))
+  }
 
   xlen <- vec_size(x)
   n <- pmin(n, xlen)
