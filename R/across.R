114fed06c60e7e73b7ff9a06f1aac71a93c798b6
Romain Francois <romain@rstudio.com>
2021-01-28 14:11:42 +0100

    using names2()

--- R/across.R
+++ R/across.R
@@ -376,7 +376,7 @@ top_across <- function(.cols = everything(), .fns = NULL, ..., .names = NULL) {
 
 dplyr_quosures <- function(...) {
   quosures <- enquos(..., .ignore_empty = "all")
-  names_given <- names(quosures) %||% rep("", length(quosures))
+  names_given <- names2(quosures)
   names_auto  <- names(enquos(..., .named = TRUE, .ignore_empty = "all"))
 
   for (i in seq_along(quosures)) {
