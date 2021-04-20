3fd436fce94155aa5140c734d859f54db55b6b90
Romain Francois <romain@rstudio.com>
2021-04-20 10:29:04 +0200

    simplify error

--- R/across.R
+++ R/across.R
@@ -249,7 +249,7 @@ abort_invalid_cols_arg <- function(.cols, fn = "across") {
   abort(c(
     "Predicate used in lieu of column selection.",
     i = "The first argument `.cols` selects a set of columns.",
-    i = "The second argument `.fns` operates on each selected columns to choose rows.",
+    i = "The second argument `.fns` operates on each selected columns.",
     i = glue("You most likely meant: `{fn}(everything(), {as_label(.cols)})`."),
     i = glue("If this was truly meant as a column selection, you must wrap with: `{fn}(where({as_label(.cols)}))`.")
   ), class = "dplyr:::across_cols_formula")
