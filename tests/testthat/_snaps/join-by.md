393bc969080f33c52f6b63790da6baf99c7133a5
DavisVaughan <davis@rstudio.com>
2021-06-18 13:53:10 -0400

    Add `join_by()` tests

--- tests/testthat/_snaps/join-by.md
+++ tests/testthat/_snaps/join-by.md
@@ -1,3 +1,112 @@
+# has an informative print method
+
+    Code
+      join_by()
+    Output
+      Join By:
+
+---
+
+    Code
+      join_by(a, b)
+    Output
+      Join By:
+      - a
+      - b
+
+---
+
+    Code
+      join_by("a", "b")
+    Output
+      Join By:
+      - "a"
+      - "b"
+
+---
+
+    Code
+      join_by(a == a, b >= c)
+    Output
+      Join By:
+      - a == a
+      - b >= c
+
+---
+
+    Code
+      join_by(a == a, b >= "c")
+    Output
+      Join By:
+      - a == a
+      - b >= "c"
+
+---
+
+    Code
+      join_by(a == a, max(b >= "c"), min(d < e))
+    Output
+      Join By:
+      - a == a
+      - max(b >= "c")
+      - min(d < e)
+
+# has informative error messages
+
+    Code
+      join_by(a = b)
+    Error <rlang_error>
+      `join_by()` expressions can't be named.
+      i Did you use `=` rather than `==`?
+
+---
+
+    Code
+      join_by(NULL)
+    Error <rlang_error>
+      Join by expressions can't be empty.
+      x Expression 1 is empty.
+
+---
+
+    Code
+      join_by(foo(x > y))
+    Error <rlang_error>
+      `join_by()` expressions of length 2 must contain either `max()` or `min()`.
+      x Expression 1 is `foo(x > y)`.
+
+---
+
+    Code
+      join_by(max(!x))
+    Error <rlang_error>
+      Each `join_by()` condition must be length 1 or length 3, and be composed of a left-hand side column name, a condition, and a right-hand side column name.
+      x Expression 1 is length 2 and is `!x`.
+
+---
+
+    Code
+      join_by(x == y, x^y)
+    Error <rlang_error>
+      Each `join_by()` condition must be separated by one of: `==`, `>`, `>=`, `<`, or `<=`.
+      x Expression 2 is `x^y`.
+
+---
+
+    Code
+      join_by(x + 1 == y)
+    Error <rlang_error>
+      The left-hand side of each `join_by()` condition must be a string or an unquoted column name.
+      i The left-hand side of condition 1 is `x + 1`.
+
+---
+
+    Code
+      join_by(x == y + 1)
+    Error <rlang_error>
+      The right-hand side of each `join_by()` condition must be a string or an unquoted column name.
+      i The right-hand side of condition 1 is `y + 1`.
+
 # as_join_by() emits useful errors
 
     Code
--- tests/testthat/test-join-by.R
+++ tests/testthat/test-join-by.R
@@ -1,4 +1,92 @@
 # ------------------------------------------------------------------------------
+# `join_by()`
+
+test_that("works with equi conditions", {
+  by <- join_by(x == y, a == b)
+
+  expect_identical(by$x, c("x", "a"))
+  expect_identical(by$y, c("y", "b"))
+  expect_identical(by$condition, c("==", "=="))
+  expect_identical(by$filter, c("none", "none"))
+})
+
+test_that("works with non-equi conditions", {
+  by <- join_by(x == y, a > b, a >= b, a < b, a <= b)
+
+  expect_identical(by$x, c("x", rep("a", 4)))
+  expect_identical(by$y, c("y", rep("b", 4)))
+  expect_identical(by$condition, c("==", ">", ">=", "<", "<="))
+})
+
+test_that("works with rolling conditions", {
+  by <- join_by(x == y, max(a > b))
+
+  expect_identical(by$x, c("x", "a"))
+  expect_identical(by$y, c("y", "b"))
+  expect_identical(by$filter, c("none", "max"))
+
+  by <- join_by(x == y, min(a > b))
+
+  expect_identical(by$x, c("x", "a"))
+  expect_identical(by$y, c("y", "b"))
+  expect_identical(by$filter, c("none", "min"))
+})
+
+test_that("works with single arguments", {
+  by <- join_by(a, b)
+  expect_identical(by$x, c("a", "b"))
+  expect_identical(by$y, c("a", "b"))
+})
+
+test_that("works with character strings", {
+  by1 <- join_by("a", "b" == "c", max("d" >= "e"))
+  by2 <- join_by(a, b  == c, max(d >= e))
+
+  expect_identical(by1$condition, by2$condition)
+  expect_identical(by1$filter, by2$filter)
+  expect_identical(by1$x, by2$x)
+  expect_identical(by1$y, by2$y)
+})
+
+test_that("can join_by() nothing for a cross join", {
+  by <- join_by()
+  expect_identical(by$x, character())
+  expect_identical(by$y, character())
+})
+
+test_that("has an informative print method", {
+  expect_snapshot(join_by())
+  expect_snapshot(join_by(a, b))
+  expect_snapshot(join_by("a", "b"))
+  expect_snapshot(join_by(a == a, b >= c))
+  expect_snapshot(join_by(a == a, b >= "c"))
+  expect_snapshot(join_by(a == a, max(b >= "c"), min(d < e)))
+})
+
+test_that("has informative error messages", {
+  # `=` rather than `==`
+  expect_snapshot(error = TRUE, join_by(a = b))
+
+  # Empty expression
+  expect_snapshot(error = TRUE, join_by(NULL))
+
+  # Improper rolling specification
+  expect_snapshot(error = TRUE, join_by(foo(x > y)))
+
+  # Length two expression inside rolling specification
+  expect_snapshot(error = TRUE, join_by(max(!x)))
+
+  # Improper separator
+  expect_snapshot(error = TRUE, join_by(x == y, x ^ y))
+
+  # Improper LHS
+  expect_snapshot(error = TRUE, join_by(x + 1 == y))
+
+  # Improper RHS
+  expect_snapshot(error = TRUE, join_by(x == y + 1))
+})
+
+# ------------------------------------------------------------------------------
 # `as_join_by()`
 
 test_that("as_join_by() emits useful errors", {
