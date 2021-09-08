222c52d28a679b0cdabe2de75cc96575e2b26a93
DavisVaughan <davis@rstudio.com>
2021-09-08 10:00:14 -0400

    Update tests affected by change to `multiple` default

--- tests/testthat/test-empty-groups.R
+++ tests/testthat/test-empty-groups.R
@@ -58,11 +58,11 @@ test_that("joins respect zero length groups", {
   df2 <- tibble(f = factor( c(2,2,3,3), levels = 1:3), y = c(1,2,3,4)) %>%
     group_by(f)
 
-  expect_equal(group_size(left_join( df1, df2, by = "f")),  c(2,4))
-  expect_equal(group_size(right_join( df1, df2, by = "f")),  c(4,2))
-  expect_equal(group_size(full_join( df1, df2, by = "f")),  c(2,4,2))
+  expect_equal(group_size(left_join( df1, df2, by = "f", multiple = "all")),  c(2,4))
+  expect_equal(group_size(right_join( df1, df2, by = "f", multiple = "all")),  c(4,2))
+  expect_equal(group_size(full_join( df1, df2, by = "f", multiple = "all")),  c(2,4,2))
   expect_equal(group_size(anti_join( df1, df2, by = "f")),  c(2))
-  expect_equal(group_size(inner_join( df1, df2, by = "f")),  c(4))
+  expect_equal(group_size(inner_join( df1, df2, by = "f", multiple = "all")),  c(4))
 
 
   df1 <- tibble(f = factor( c(1,1,2,2), levels = 1:3), x = c(1,2,1,4)) %>%
@@ -70,11 +70,11 @@ test_that("joins respect zero length groups", {
   df2 <- tibble(f = factor( c(2,2,3,3), levels = 1:3), y = c(1,2,3,4)) %>%
     group_by(f, .drop = FALSE)
 
-  expect_equal(group_size(left_join( df1, df2, by = "f")),  c(2,4,0))
-  expect_equal(group_size(right_join( df1, df2, by = "f")),  c(0,4,2))
-  expect_equal(group_size(full_join( df1, df2, by = "f")),  c(2,4,2))
+  expect_equal(group_size(left_join( df1, df2, by = "f", multiple = "all")),  c(2,4,0))
+  expect_equal(group_size(right_join( df1, df2, by = "f", multiple = "all")),  c(0,4,2))
+  expect_equal(group_size(full_join( df1, df2, by = "f", multiple = "all")),  c(2,4,2))
   expect_equal(group_size(anti_join( df1, df2, by = "f")),  c(2,0,0))
-  expect_equal(group_size(inner_join( df1, df2, by = "f")),  c(0,4,0))
+  expect_equal(group_size(inner_join( df1, df2, by = "f", multiple = "all")),  c(0,4,0))
 })
 
 test_that("n_groups respects zero-length groups (#341)", {
