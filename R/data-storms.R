06e2fdd0f0cbc947dcb674bc9853031326cb3bb7
Steve Haroz <steveharoz@users.noreply.github.com>
2021-09-17 11:48:39 +0200

    Update storms data to 2020 (#6000)
    * Update storms data to 2020
    
    * try to make storms example pass the check
    
    * Add call to library(ggplot2) in example
    
    * Clarified storm diameter variables
    
    * NEWS
    
    Co-authored-by: Romain Francois <romain@rstudio.com>

--- R/data-storms.R
+++ R/data-storms.R
@@ -2,12 +2,12 @@
 #'
 #' This data is a subset of the NOAA Atlantic hurricane database best track
 #' data, \url{https://www.nhc.noaa.gov/data/#hurdat}. The data includes the
-#' positions and attributes of 198 tropical storms, measured every six hours
+#' positions and attributes of storms from 1975-2020, measured every six hours
 #' during the lifetime of a storm.
 #'
 #' @seealso The script to create the storms data set: \url{https://github.com/tidyverse/dplyr/blob/master/data-raw/storms.R}
 #'
-#' @format A tibble with 10,010 observations and 13 variables:
+#' @format A tibble with 11,859 observations and 13 variables:
 #' \describe{
 #' \item{name}{Storm Name}
 #' \item{year,month,day}{Date of report}
@@ -19,9 +19,20 @@
 #' -1 = Tropical Depression, 0 = Tropical Storm)}
 #' \item{wind}{storm's maximum sustained wind speed (in knots)}
 #' \item{pressure}{Air pressure at the storm's center (in millibars)}
-#' \item{ts_diameter}{Diameter of the area experiencing tropical storm strength winds (34 knots or above)}
-#' \item{hu_diameter}{Diameter of the area experiencing hurricane strength winds (64 knots or above)}
+#' \item{tropicalstorm_force_diameter}{Diameter (in nautical miles) of the area experiencing tropical storm strength winds (34 knots or above)}
+#' \item{hurricane_force_diameter}{Diameter (in nautical miles) of the area experiencing hurricane strength winds (64 knots or above)}
 #' }
 #' @examples
+#'
+#' # show a plot of the storm paths
+#' if (requireNamespace("ggplot2", quietly = TRUE)) {
+#'   library(ggplot2)
+#'   ggplot(storms) +
+#'     aes(x=long, y=lat, color=paste(year, name)) +
+#'     geom_path() +
+#'     guides(color='none') +
+#'     facet_wrap(~year)
+#' }
+#'
 #' storms
 "storms"
