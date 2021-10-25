eb5f8d87822b2a7019dd10231c59027067f05593
Kirill Müller <krlmlr+r@mailbox.org>
2021-10-25 06:43:19 +0200

    Avoid reexporting deprecated APIs

--- R/reexport-tibble.r
+++ R/reexport-tibble.r
@@ -1,34 +1,12 @@
 # dataframe ---------------------------------------------------------------
 
-#' @importFrom tibble data_frame
-#' @export
-tibble::data_frame
-
-#' @importFrom tibble data_frame_
-#' @export
-tibble::data_frame_
-
-#' @importFrom tibble as_data_frame
-#' @export
-tibble::as_data_frame
-
 #' @importFrom tibble lst
 #' @export
 tibble::lst
 
-#' @importFrom tibble lst_
-#' @export
-tibble::lst_
-
 #' @importFrom tibble add_row
 #' @export
 tibble::add_row
-
-# type_sum ----------------------------------------------------------------
-
-#' @importFrom tibble type_sum
-#' @export
-tibble::type_sum
 
 # glimpse -----------------------------------------------------------------
 
@@ -66,10 +44,6 @@ glimpse
 
 # frame-data --------------------------------------------------------------
 
-#' @importFrom tibble frame_data
-#' @export
-tibble::frame_data
-
 #' @importFrom tibble tribble
 #' @export
 tibble::tribble
@@ -84,9 +58,3 @@ tibble::as_tibble
 
 #' @importFrom tibble view
 tibble::view
-
-# utils -------------------------------------------------------------------
-
-#' @importFrom tibble tbl_sum
-#' @export
-tibble::tbl_sum
