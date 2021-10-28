123311b5b48fd765088260670fcb401aeffcbac5
Romain Francois <romain@rstudio.com>
2021-10-28 14:42:24 +0200

    remove some unused functions

--- R/utils.r
+++ R/utils.r
@@ -13,19 +13,6 @@ deparse_trunc <- function(x, width = getOption("width")) {
   paste0(substr(text[1], 1, width - 3), "...")
 }
 
-any_apply <- function(xs, f) {
-  for (x in xs) {
-    if (f(x)) return(TRUE)
-  }
-  FALSE
-}
-
-deparse_names <- function(x) {
-  x <- map_if(x, is_quosure, quo_squash)
-  x <- map_if(x, is_bare_formula, f_rhs)
-  map_chr(x, deparse)
-}
-
 commas <- function(...) paste0(..., collapse = ", ")
 
 in_travis <- function() identical(Sys.getenv("TRAVIS"), "true")
@@ -39,30 +26,6 @@ named <- function(...) {
   x
 }
 
-unique_name <- local({
-  i <- 0
-
-  function() {
-    i <<- i + 1
-    paste0("zzz", i)
-  }
-})
-
-succeeds <- function(x, quiet = FALSE) {
-  tryCatch( #
-    {
-      x
-      TRUE
-    },
-    error = function(e) {
-      if (!quiet) {
-        inform(paste0("Error: ", e$message))
-      }
-      FALSE
-    }
-  )
-}
-
 is_1d <- function(x) {
   # dimension check is for matrices and data.frames
   (is_atomic(x) || is.list(x)) && length(dim(x)) <= 1
@@ -70,20 +33,6 @@ is_1d <- function(x) {
 
 random_table_name <- function(n = 10) {
   paste0(sample(letters, n, replace = TRUE), collapse = "")
-}
-
-attr_equal <- function(x, y) {
-  attr_x <- attributes(x)
-  if (!is.null(attr_x)) {
-    attr_x <- attr_x[sort(names(attr_x))]
-  }
-
-  attr_y <- attributes(y)
-  if (!is.null(attr_y)) {
-    attr_y <- attr_y[sort(names(attr_y))]
-  }
-
-  isTRUE(all.equal(attr_x, attr_y))
 }
 
 unstructure <- function(x) {
