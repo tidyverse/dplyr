9232a40fb3bbfd9c608a73c5a1c8b9a02bd4b669
Arthur Yip <5713411+ahcyip@users.noreply.github.com>
2022-03-07 11:07:10 -0700

    update bind.r documentation
    Name/description of .id argument was a bit ambiguous

--- R/bind.r
+++ R/bind.r
@@ -18,7 +18,7 @@
 #'   When column-binding, rows are matched by position, so all data
 #'   frames must have the same number of rows. To match by value, not
 #'   position, see [mutate-joins].
-#' @param .id Data frame identifier.
+#' @param .id Name of data frame identifier column.
 #'
 #'   When `.id` is supplied, a new column of identifiers is
 #'   created to link each row to its original data frame. The labels
