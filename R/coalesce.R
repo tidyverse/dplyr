00f1a94c7bf50a3b0df849e0914092663a391157
DavisVaughan <davis@rstudio.com>
2022-05-11 16:50:36 -0400

    Port https://github.com/tidyverse/funs/pull/80 to `coalesce()`

--- R/coalesce.R
+++ R/coalesce.R
@@ -1,69 +1,61 @@
-#' Find first non-missing element
+#' Find the first non-missing element
 #'
-#' Given a set of vectors, `coalesce()` finds the first non-missing value
-#' at each position. This is inspired by the SQL `COALESCE` function
-#' which does the same thing for `NULL`s.
+#' Given a set of vectors, `coalesce()` finds the first non-missing value at
+#' each position. It's inspired by the SQL `COALESCE` function which does the
+#' same thing for SQL `NULL`s.
 #'
-#' @param ... <[`dynamic-dots`][rlang::dyn-dots]> Vectors. Inputs should be
-#'   recyclable (either be length 1 or same length as the longest vector) and
-#'   coercible to a common type. If data frames, they are coalesced column by
-#'   column.
-#' @return A vector the same length as the first `...` argument with
-#'   missing values replaced by the first non-missing value.
-#' @seealso [na_if()] to replace specified values with a `NA`.
-#' [tidyr::replace_na()] to replace `NA` with a value
+#' @param ... <[`dynamic-dots`][rlang::dyn-dots]> One or more vectors.
+#' @param .ptype The type to cast the vectors in `...` to. If `NULL`, the
+#'   vectors will be cast to their common type, which is consistent with SQL.
+#' @param .size The size to recycle the vectors in `...` to. If `NULL`, the
+#'   vectors will be recycled to their common size.
+#' @return A vector with the same type and length as the common type and length
+#'   of the vectors in `...`.
+#' @seealso [na_if()] to replace specified values with an `NA`.
+#'   [tidyr::replace_na()] to replace `NA` with a value.
 #' @export
 #' @examples
 #' # Use a single value to replace all missing values
 #' x <- sample(c(1:5, NA, NA, NA))
 #' coalesce(x, 0L)
 #'
-#' # Or match together a complete vector from missing pieces
+#' # The equivalent to a missing value in a list is NULL
+#' coalesce(list(1, 2, NULL), list(NA))
+#'
+#' # Or generate a complete vector from partially missing pieces
 #' y <- c(1, 2, NA, NA, 5)
 #' z <- c(NA, NA, 3, 4, 5)
 #' coalesce(y, z)
 #'
-#' # Supply lists by with dynamic dots
+#' # Supply lists by splicing them into dots:
 #' vecs <- list(
 #'   c(1, 2, NA, NA, 5),
 #'   c(NA, NA, 3, 4, 5)
 #' )
 #' coalesce(!!!vecs)
-coalesce <- function(...) {
-  if (missing(..1)) {
-    abort("At least one argument must be supplied.")
+coalesce <- function(..., .ptype = NULL, .size = NULL) {
+  args <- list2(...)
+  args <- discard(args, is.null)
+
+  if (length(args) == 0L) {
+    abort("`...` must contain at least one input.")
   }
 
-  values <- list2(...)
-  values <- fix_call(vec_cast_common(!!!values))
-  values <- fix_call(vec_recycle_common(!!!values))
+  args <- vec_cast_common(!!!args, .to = .ptype)
+  args <- vec_recycle_common(!!!args, .size = .size)
 
-  x <- values[[1]]
-  values <- values[-1]
+  out <- args[[1L]]
+  args <- args[-1L]
 
-  if (is.array(x) && length(dim(x)) > 1) {
-    abort("Can't coalesce matrices.")
-  }
-  if (is.data.frame(x)) {
-    df_coalesce(x, values)
-  } else {
-    vec_coalesce(x, values)
-  }
-}
+  for (arg in args) {
+    is_na <- vec_equal_na(out)
 
-vec_coalesce <- function(x, values) {
-  for (i in seq_along(values)) {
-    x_miss <- is.na(x)
-    vec_slice(x, x_miss) <- vec_slice(values[[i]], x_miss)
+    if (!any(is_na)) {
+      break
+    }
+
+    out <- vec_assign(out, is_na, vec_slice(arg, is_na))
   }
 
-  x
-}
-df_coalesce <- function(x, values) {
-  for (i in seq_along(x)) {
-    col_values <- map(values, `[[`, i)
-    x[[i]] <- vec_coalesce(x[[i]], col_values)
-  }
-
-  x
+  out
 }
--- tests/testthat/_snaps/coalesce.md
+++ tests/testthat/_snaps/coalesce.md
@@ -13,3 +13,35 @@
       Error in `coalesce()`:
       ! Can't combine `..1` <integer> and `..2` <character>.
 
+# `.size` overrides the common size
+
+    Code
+      coalesce(x, 1:2, .size = vec_size(x))
+    Condition
+      Error in `coalesce()`:
+      ! Can't recycle `..2` (size 2) to size 1.
+
+# must have at least one non-`NULL` vector
+
+    Code
+      coalesce()
+    Condition
+      Error in `coalesce()`:
+      ! `...` must contain at least one input.
+
+---
+
+    Code
+      coalesce(NULL, NULL)
+    Condition
+      Error in `coalesce()`:
+      ! `...` must contain at least one input.
+
+# inputs must be vectors
+
+    Code
+      coalesce(1, environment())
+    Condition
+      Error in `coalesce()`:
+      ! `..2` must be a vector, not an environment.
+
