b53d0ccde4ebcd67055d0ac094972f2e97045f87
DavisVaughan <davis@rstudio.com>
2022-06-01 08:59:14 -0400

    Implement `vec_case_when()`

---
+++ tests/testthat/_snaps/vec-case-when.md
@@ -0,0 +1,173 @@
+# odd numbered inputs can be size zero
+
+    Code
+      vec_case_when(logical(), 1:2)
+    Condition
+      Error:
+      ! `..2` must have size 0, not size 2.
+
+# even numbered inputs are cast to their common type
+
+    Code
+      vec_case_when(FALSE, 1, TRUE, "x")
+    Condition
+      Error:
+      ! Can't combine `..2` <double> and `..4` <character>.
+
+# even numbered inputs must be size 1 or same size as logical conditions
+
+    Code
+      vec_case_when(c(TRUE, FALSE, TRUE, TRUE), 1:3)
+    Condition
+      Error:
+      ! `..2` must have size 4, not size 3.
+
+# `.default` must be size 1 or same size as logical conditions (exact same as any other even numbered input)
+
+    Code
+      vec_case_when(FALSE, 1L, .default = 2:3)
+    Condition
+      Error:
+      ! `.default` must have size 1, not size 2.
+
+# `.default_arg` can be customized
+
+    Code
+      vec_case_when(FALSE, 1L, .default = 2:3, .default_arg = "foo")
+    Condition
+      Error:
+      ! `foo` must have size 1, not size 2.
+
+---
+
+    Code
+      vec_case_when(FALSE, 1L, .default = "x", .default_arg = "foo")
+    Condition
+      Error:
+      ! Can't combine `..2` <integer> and `foo` <character>.
+
+# `.default_arg` is validated
+
+    Code
+      vec_case_when(TRUE, 1, .default_arg = 1)
+    Condition
+      Error:
+      ! `.default_arg` must be a string.
+
+# odd numbered inputs must all be the same size
+
+    Code
+      vec_case_when(c(TRUE, FALSE), 1, TRUE, 2)
+    Condition
+      Error:
+      ! `..3` must have size 2, not size 1.
+
+---
+
+    Code
+      vec_case_when(c(TRUE, FALSE), 1, c(TRUE, FALSE, TRUE), 2)
+    Condition
+      Error:
+      ! Can't recycle `..1` (size 2) to match `..3` (size 3).
+
+# odd numbered inputs must be logical (and aren't cast to logical!)
+
+    Code
+      vec_case_when(1, 2)
+    Condition
+      Error:
+      ! `..1` must be a vector with type <logical>.
+      Instead, it has type <double>.
+
+---
+
+    Code
+      vec_case_when(TRUE, 2, 3.5, 4)
+    Condition
+      Error:
+      ! `..3` must be a vector with type <logical>.
+      Instead, it has type <double>.
+
+# `.size` overrides the odd numbered input sizes
+
+    Code
+      vec_case_when(TRUE, 1, .size = 5)
+    Condition
+      Error:
+      ! `..1` must have size 5, not size 1.
+
+---
+
+    Code
+      vec_case_when(c(TRUE, FALSE), 1, c(TRUE, FALSE, TRUE), 2, .size = 2)
+    Condition
+      Error:
+      ! `..3` must have size 2, not size 3.
+
+# `.ptype` overrides the even numbered input types
+
+    Code
+      vec_case_when(FALSE, 1, TRUE, 2, .ptype = character())
+    Condition
+      Error:
+      ! Can't convert `..2` <double> to <character>.
+
+# can't have an odd number of inputs
+
+    Code
+      vec_case_when(1)
+    Condition
+      Error:
+      ! `...` must contain an even number of inputs.
+      i 1 inputs were provided.
+
+---
+
+    Code
+      vec_case_when(1, 2, 3)
+    Condition
+      Error:
+      ! `...` must contain an even number of inputs.
+      i 3 inputs were provided.
+
+# can't have empty dots
+
+    Code
+      vec_case_when()
+    Condition
+      Error:
+      ! `...` can't be empty.
+
+---
+
+    Code
+      vec_case_when(.default = 1)
+    Condition
+      Error:
+      ! `...` can't be empty.
+
+# named dots show up in the error message
+
+    Code
+      vec_case_when(x = 1.5, 1)
+    Condition
+      Error:
+      ! `x` must be a vector with type <logical>.
+      Instead, it has type <double>.
+
+---
+
+    Code
+      vec_case_when(x = TRUE, 1, y = c(TRUE, FALSE), 2)
+    Condition
+      Error:
+      ! `x` must have size 2, not size 1.
+
+---
+
+    Code
+      vec_case_when(TRUE, 1, FALSE, x = "y")
+    Condition
+      Error:
+      ! Can't combine `..2` <double> and `x` <character>.
+
---
+++ tests/testthat/test-vec-case-when.R
@@ -0,0 +1,277 @@
+test_that("works with data frames", {
+  out <- vec_case_when(
+    c(FALSE, TRUE, FALSE, FALSE), vctrs::data_frame(x = 1, y = 2),
+    c(TRUE, TRUE, FALSE, FALSE), vctrs::data_frame(x = 3, y = 4),
+    c(FALSE, TRUE, FALSE, TRUE), vctrs::data_frame(x = 3:6, y = 4:7),
+  )
+
+  expect_identical(
+    out,
+    vctrs::data_frame(
+      x = c(3, 1, NA, 6),
+      y = c(4, 2, NA, 7)
+    )
+  )
+})
+
+test_that("first `TRUE` case wins", {
+  expect_identical(
+    vec_case_when(c(TRUE, FALSE), 1, c(TRUE, TRUE), 2, c(TRUE, TRUE), 3),
+    c(1, 2)
+  )
+})
+
+test_that("can replace missing values", {
+  x <- c(1:3, NA)
+
+  expect_identical(
+    vec_case_when(
+      x <= 1, 1,
+      x <= 2, 2,
+      is.na(x), 0
+    ),
+    c(1, 2, NA, 0)
+  )
+})
+
+test_that("Unused logical `NA` can still be cast to `...` ptype", {
+  # Requires that casting happen before recycling, because it recycles
+  # to size zero, resulting in a logical rather than an unspecified.
+  expect_identical(vec_case_when(TRUE, "x", FALSE, NA), "x")
+  expect_identical(vec_case_when(FALSE, "x", TRUE, NA), NA_character_)
+})
+
+test_that("odd numbered inputs can be size zero", {
+  expect_identical(
+    vec_case_when(
+      logical(), 1,
+      logical(), 2
+    ),
+    numeric()
+  )
+
+  expect_snapshot(error = TRUE, {
+    vec_case_when(logical(), 1:2)
+  })
+})
+
+test_that("retains names of inputs", {
+  value1 <- c(x = 1, y = 2)
+  value2 <- c(z = 3, w = 4)
+
+  out <- vec_case_when(
+    c(TRUE, FALSE), value1,
+    c(TRUE, TRUE), value2
+  )
+
+  expect_named(out, c("x", "w"))
+})
+
+test_that("even numbered inputs are cast to their common type", {
+  expect_identical(vec_case_when(FALSE, 1, TRUE, 2L), 2)
+  expect_identical(vec_case_when(FALSE, 1, TRUE, NA), NA_real_)
+
+  expect_snapshot(error = TRUE, {
+    vec_case_when(FALSE, 1, TRUE, "x")
+  })
+})
+
+test_that("even numbered inputs must be size 1 or same size as logical conditions", {
+  expect_identical(
+    vec_case_when(c(TRUE, TRUE), 1),
+    c(1, 1)
+  )
+  expect_identical(
+    vec_case_when(c(TRUE, FALSE), c(1, 2), c(TRUE, TRUE), c(3, 4)),
+    c(1, 4)
+  )
+
+  # Make sure input numbering is right in the error message!
+  expect_snapshot(error = TRUE, {
+    vec_case_when(c(TRUE, FALSE, TRUE, TRUE), 1:3)
+  })
+})
+
+test_that("Unhandled `NA` propagates through as a missing value", {
+  expect_identical(
+    vec_case_when(NA, 1, .default = 2),
+    NA_real_
+  )
+
+  expect_identical(
+    vec_case_when(
+      c(FALSE, NA, TRUE),
+      2,
+      c(NA, FALSE, TRUE),
+      3,
+      .default = 4
+    ),
+    c(NA, NA, 2)
+  )
+})
+
+test_that("`NA` is overridden by any `TRUE` values", {
+  x <- c(1, 2, NA, 3)
+  expect <- c("one", "not_one", "missing", "not_one")
+
+  # `TRUE` overriding before the `NA`
+  expect_identical(
+    vec_case_when(
+      is.na(x), "missing",
+      x == 1, "one",
+      .default = "not_one"
+    ),
+    expect
+  )
+
+  # `TRUE` overriding after the `NA`
+  expect_identical(
+    vec_case_when(
+      x == 1, "one",
+      is.na(x), "missing",
+      .default = "not_one"
+    ),
+    expect
+  )
+})
+
+test_that("works when there is a used `.default` and no missing values", {
+  expect_identical(vec_case_when(c(TRUE, FALSE), 1, .default = 3:4), c(1, 4))
+})
+
+test_that("works when there are missing values but no `.default`", {
+  expect_identical(vec_case_when(c(TRUE, NA), 1), c(1, NA))
+})
+
+test_that("A `NULL` `.default` fills in with missing values", {
+  expect_identical(
+    vec_case_when(c(TRUE, FALSE, FALSE), 1),
+    c(1, NA, NA)
+  )
+})
+
+test_that("`.default` fills in all unused slots", {
+  expect_identical(
+    vec_case_when(c(TRUE, FALSE, FALSE), 1, .default = 2),
+    c(1, 2, 2)
+  )
+})
+
+test_that("`.default` is initialized correctly in the logical / unspecified case", {
+  # i.e. `vec_ptype(NA)` is unspecified but the result should be finalized to logical
+  expect_identical(vec_case_when(FALSE, NA), NA)
+})
+
+test_that("`.default` can be vectorized, and is sliced to fit as needed", {
+  out <- vec_case_when(
+    c(FALSE, TRUE, FALSE, TRUE, FALSE), 1:5,
+    c(FALSE, TRUE, FALSE, FALSE, TRUE), 6:10,
+    .default = 11:15
+  )
+
+  expect_identical(out, c(11L, 2L, 13L, 4L, 10L))
+})
+
+test_that("`.default` must be size 1 or same size as logical conditions (exact same as any other even numbered input)", {
+  expect_snapshot(error = TRUE, {
+    vec_case_when(FALSE, 1L, .default = 2:3)
+  })
+})
+
+test_that("`.default` participates in common type determination (exact same as any other even numbered input)", {
+  expect_identical(vec_case_when(FALSE, 1L, .default = 2), 2)
+})
+
+test_that("`.default` that is an unused logical `NA` can still be cast to `...` ptype", {
+  # Requires that casting happen before recycling, because it recycles
+  # to size zero, resulting in a logical rather than an unspecified.
+  expect_identical(vec_case_when(TRUE, "x", .default = NA), "x")
+})
+
+test_that("`.default_arg` can be customized", {
+  expect_snapshot(error = TRUE, {
+    vec_case_when(FALSE, 1L, .default = 2:3, .default_arg = "foo")
+  })
+  expect_snapshot(error = TRUE, {
+    vec_case_when(FALSE, 1L, .default = "x", .default_arg = "foo")
+  })
+})
+
+test_that("`.default_arg` is validated", {
+  expect_snapshot(error = TRUE, {
+    vec_case_when(TRUE, 1, .default_arg = 1)
+  })
+})
+
+test_that("odd numbered inputs must all be the same size", {
+  expect_snapshot(error = TRUE, {
+    vec_case_when(c(TRUE, FALSE), 1, TRUE, 2)
+  })
+  expect_snapshot(error = TRUE, {
+    vec_case_when(c(TRUE, FALSE), 1, c(TRUE, FALSE, TRUE), 2)
+  })
+})
+
+test_that("odd numbered inputs must be logical (and aren't cast to logical!)", {
+  expect_snapshot(error = TRUE, {
+    vec_case_when(1, 2)
+  })
+
+  # Make sure input numbering is right in the error message!
+  expect_snapshot(error = TRUE, {
+    vec_case_when(TRUE, 2, 3.5, 4)
+  })
+})
+
+test_that("`.size` overrides the odd numbered input sizes", {
+  expect_snapshot(error = TRUE, {
+    vec_case_when(TRUE, 1, .size = 5)
+  })
+
+  # Make sure input numbering is right in the error message!
+  expect_snapshot(error = TRUE, {
+    vec_case_when(c(TRUE, FALSE), 1, c(TRUE, FALSE, TRUE), 2, .size = 2)
+  })
+})
+
+test_that("`.ptype` overrides the even numbered input types", {
+  expect_identical(
+    vec_case_when(FALSE, 1, TRUE, 2, .ptype = integer()),
+    2L
+  )
+
+  # Make sure input numbering is right in the error message!
+  expect_snapshot(error = TRUE, {
+    vec_case_when(FALSE, 1, TRUE, 2, .ptype = character())
+  })
+})
+
+test_that("can't have an odd number of inputs", {
+  expect_snapshot(error = TRUE, {
+    vec_case_when(1)
+  })
+  expect_snapshot(error = TRUE, {
+    vec_case_when(1, 2, 3)
+  })
+})
+
+test_that("can't have empty dots", {
+  expect_snapshot(error = TRUE, {
+    vec_case_when()
+  })
+  expect_snapshot(error = TRUE, {
+    vec_case_when(.default = 1)
+  })
+})
+
+test_that("named dots show up in the error message", {
+  expect_snapshot(error = TRUE, {
+    vec_case_when(x = 1.5, 1)
+  })
+  expect_snapshot(error = TRUE, {
+    vec_case_when(x = TRUE, 1, y = c(TRUE, FALSE), 2)
+  })
+  expect_snapshot(error = TRUE, {
+    vec_case_when(TRUE, 1, FALSE, x = "y")
+  })
+})
