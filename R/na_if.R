b52b766ec302d10fa292047a3d7de320743dfc1a
Davis Vaughan <davis@rstudio.com>
2022-07-19 15:20:34 -0400

    Rewrite `na_if()` using vctrs (#6329)
    * Rewrite `na_if()` using vctrs
    
    * NEWS bullet
    
    * NEWS bullet update based on revdep failure analysis
    
    * Use `na_equal = TRUE` specifically to allow replacement of `NaN`
    
    * NEWS bullet tweaks to mention `NaN` replacement

--- R/na_if.R
+++ R/na_if.R
@@ -1,18 +1,23 @@
-#' Convert values to NA
+#' Convert values to `NA`
 #'
-#' This is a translation of the SQL command `NULLIF`. It is useful
-#' if you want to convert an annoying value to `NA`.
+#' This is a translation of the SQL command `NULLIF`. It is useful if you want
+#' to convert an annoying value to `NA`.
 #'
 #' @param x Vector to modify
-#' @param y Value to replace with NA
+#' @param y Value or vector to compare against. When `x` and `y` are equal, the
+#'   value in `x` will be replaced with `NA`.
+#'
+#'   `y` is cast to the type of `x` before comparison.
+#'
+#'   `y` is [recycled][vctrs::vector_recycling_rules] to the size of `x` before
+#'   comparison. This means that `y` can be a vector with the same size as `x`,
+#'   but most of the time this will be a single value.
 #' @return A modified version of `x` that replaces any values that
-#'   are equal to `y` with NA.
+#'   are equal to `y` with `NA`.
 #' @seealso [coalesce()] to replace missing values with a specified
 #'   value.
 #'
 #'   [tidyr::replace_na()] to replace `NA` with a value.
-#'
-#'   [recode()] to more generally replace values.
 #' @export
 #' @examples
 #' na_if(1:5, 5:1)
@@ -24,19 +29,30 @@
 #' y <- c("abc", "def", "", "ghi")
 #' na_if(y, "")
 #'
-#' # na_if() is particularly useful inside mutate(),
+#' # `na_if()` allows you to replace `NaN` with `NA`,
+#' # even though `NaN == NaN` returns `NA`
+#' z <- c(1, NaN, NA, 2, NaN)
+#' na_if(z, NaN)
+#'
+#' # `na_if()` is particularly useful inside `mutate()`,
 #' # and is meant for use with vectors rather than entire data frames
 #' starwars %>%
 #'   select(name, eye_color) %>%
 #'   mutate(eye_color = na_if(eye_color, "unknown"))
 #'
-#' # na_if() can also be used with mutate() and across()
-#' # to mutate multiple columns
+#' # `na_if()` can also be used with `mutate()` and `across()`
+#' # to alter multiple columns
 #' starwars %>%
 #'    mutate(across(where(is.character), ~na_if(., "unknown")))
 na_if <- function(x, y) {
-  check_length(y, x, "`y`", glue("same as `x`"))
+  # Type and size stable on `x`
+  y <- vec_cast(x = y, to = x, x_arg = "y", to_arg = "x")
+  y <- vec_recycle(y, size = vec_size(x), x_arg = "y")
 
-  x[x == y] <- NA
+  na <- vec_init(x)
+  where <- vec_equal(x, y, na_equal = TRUE)
+
+  x <- vec_assign(x, where, na)
+
   x
 }
