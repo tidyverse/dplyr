d65051b2a3b721af5f92309f91c74c34619c32fa
Hadley Wickham <h.wickham@gmail.com>
2022-07-31 16:04:53 -0500

    Improve n_distinct() docs
    Fixes #6348

--- R/n_distinct.R
+++ R/n_distinct.R
@@ -1,19 +1,41 @@
-#' Efficiently count the number of unique values in a set of vectors
+#' Count unique combinations
 #'
-#' This is a faster and more concise equivalent of `length(unique(x))`
+#' `n_distinct()` counts the number of unique/distinct combinations in a set
+#' of one or more vectors. It's a faster and more concise equivalent to
+#' `nrow(unique(data.frame(...)))`.
 #'
-#' @param \dots vectors of values
-#' @param na.rm if `TRUE` missing values don't count
-#' @examples
-#' x <- sample(1:10, 1e5, rep = TRUE)
-#' length(unique(x))
-#' n_distinct(x)
+#' @param ... Unnamed vectors. If multiple vectors, supplied they should have
+#'   the same length. `n_distinct()` does technically respect the recycling
+#'   rules, but since a vector of length 1 won't increase the number of
+#'   distinct combinations, this is most of theoretical interest.
+#' @param na.rm If `TRUE`, exclude missing values from the count.
+#' @returns A single number..
 #' @export
+#' @examples
+#' n_distinct()
+#'
+#' x <- c(1, 1, 2, 2, 2)
+#' n_distinct(x)
+#'
+#' y <- c(3, 3, NA, 3, 3)
+#' n_distinct(y)
+#' n_distinct(y, na.rm = TRUE)
+#'
+#' # Pairs (1, 3), (2, 3), and (2, NA) are distinct
+#' n_distinct(x, y)
+#'
+#' # (2, NA) is dropped, leaving 2 distinct combinations
+#' n_distinct(x, y, na.rm = TRUE)
+#'
+#' # Also works with data frames
+#' n_distinct(data.frame(x, y))
 n_distinct <- function(..., na.rm = FALSE) {
+  check_dots_unnamed()
   data <- vctrs::data_frame(..., .name_repair = "minimal")
 
-  if (isTRUE(na.rm)){
-    data <- vec_slice(data, !reduce(map(data, vec_equal_na), `|`))
+  if (isTRUE(na.rm)) {
+    drop <- reduce(map(data, vec_equal_na), `|`)
+    data <- vec_slice(data, !drop)
   }
 
   vec_unique_count(data)
