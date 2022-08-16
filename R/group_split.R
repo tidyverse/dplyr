3f6b6fb333da15a83e0bc8dee59ccbe137ce9140
Hadley Wickham <h.wickham@gmail.com>
2022-08-16 07:06:16 -0500

    Minor tweaks to grouping function docs (#6400)

--- R/group_split.R
+++ R/group_split.R
@@ -2,71 +2,34 @@
 #'
 #' @description
 #' `r lifecycle::badge("experimental")`
-#' [group_split()] works like [base::split()] but
-#' - it uses the grouping structure from [group_by()] and therefore is subject to the data mask
-#' - it does not name the elements of the list based on the grouping as this typically
-#'   loses information and is confusing.
 #'
-#' [group_keys()] explains the grouping structure, by returning a data frame that has one row
-#' per group and one column per grouping variable.
+#' [group_split()] works like [base::split()] but:
 #'
-#' @section Grouped data frames:
+#' - It uses the grouping structure from [group_by()] and therefore is subject
+#'   to the data mask
 #'
-#' The primary use case for [group_split()] is with already grouped data frames,
-#' typically a result of [group_by()]. In this case [group_split()] only uses
-#' the first argument, the grouped tibble, and warns when `...` is used.
+#' - It does not name the elements of the list based on the grouping as this
+#'   only works well for a single character grouping variable. Instead,
+#'   use [group_keys()] to access a data frame that defines the groups.
 #'
-#' Because some of these groups may be empty, it is best paired with [group_keys()]
-#' which identifies the representatives of each grouping variable for the group.
+#' `group_split()` is primarily designed to work with grouped data frames.
+#' You can pass `...` to group and split an ungrouped data frame, but this
+#' is generally not very useful as you want have easy access to the group
+#' metadata.
 #'
-#' @section Ungrouped data frames:
-#'
-#' When used on ungrouped data frames, [group_split()] and [group_keys()] forwards the `...` to
-#' [group_by()] before the split, therefore the `...` are subject to the data mask.
-#'
-#' Using these functions on an ungrouped data frame only makes sense if you need only one or the
-#' other, because otherwise the grouping algorithm is performed each time.
-#'
-#' @section Rowwise data frames:
-#'
-#' [group_split()] returns a list of one-row tibbles is returned, and the `...` are ignored and warned against
-#'
-#' @param .tbl A tbl
-#' @param ... Grouping specification, forwarded to [group_by()]
-#' @param .keep Should the grouping columns be kept
-#' @return
-#' - [group_split()] returns a list of tibbles. Each tibble contains the rows of `.tbl` for the associated group and
-#'  all the columns, including the grouping variables.
-#'
-#' - [group_keys()] returns a tibble with one row per group, and one column per grouping variable
+#' @param .tbl A tbl.
+#' @param ... If `.tbl` is an ungrouped data frame, a grouping specification,
+#'   forwarded to [group_by()].
+#' @param .keep Should the grouping columns be kept?
+#' @returns A list of tibbles. Each tibble contains the rows of `.tbl` for the
+#'  associated group and all the columns, including the grouping variables.
 #' @family grouping functions
 #' @export
 #' @examples
-#' # ----- use case 1 : on an already grouped tibble
-#' ir <- iris %>%
-#'   group_by(Species)
+#' ir <- iris %>% group_by(Species)
 #'
 #' group_split(ir)
 #' group_keys(ir)
-#'
-#' # this can be useful if the grouped data has been altered before the split
-#' ir <- iris %>%
-#'   group_by(Species) %>%
-#'   filter(Sepal.Length > mean(Sepal.Length))
-#'
-#' group_split(ir)
-#' group_keys(ir)
-#'
-#' # ----- use case 2: using a group_by() grouping specification
-#'
-#' # both group_split() and group_keys() have to perform the grouping
-#' # so it only makes sense to do this if you only need one or the other
-#' iris %>%
-#'   group_split(Species)
-#'
-#' iris %>%
-#'   group_by(Species) %>%
-#'   group_keys()
 group_split <- function(.tbl, ..., .keep = TRUE) {
   lifecycle::signal_stage("experimental", "group_split()")
   UseMethod("group_split")
