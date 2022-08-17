bb1204b203870fb3c957bb0bfb19621dcad091d8
DavisVaughan <davis@rstudio.com>
2022-08-17 13:22:51 -0400

    Tweak `case_match()` docs based on code review

--- R/case-match.R
+++ R/case-match.R
@@ -1,9 +1,10 @@
 #' A general vectorised switch
 #'
 #' This function is a variant of [case_when()] that vectorises the idea behind
-#' [switch()]. It is an R equivalent of the SQL "simple" `CASE WHEN` statement
-#' that matches using values rather than logical expressions. If no cases match,
-#' a missing value is returned unless a `.default` is supplied.
+#' [switch()]. Use this to repeatedly recode one or more values in `.x` to a new
+#' value. It is an R equivalent of the SQL "simple" `CASE WHEN` statement that
+#' matches using values rather than logical expressions. If no cases match, a
+#' missing value is returned unless a `.default` is supplied.
 #'
 #' @param .x A vector to match against.
 #'
@@ -13,7 +14,8 @@
 #'
 #'   The LHS inputs must evaluate to vectors with the same type as `.x`. There
 #'   are no restrictions on the size of the LHS inputs, meaning that you can
-#'   map multiple values in `.x` to the same RHS value.
+#'   map multiple values in `.x` to the same RHS value. If a value in `.x` is
+#'   matched to multiple LHS inputs, the first match is used.
 #'
 #'   The RHS inputs will be coerced to their common type. Each RHS input will be
 #'   [recycled][vctrs::vector_recycling_rules] to the size of `.x`.
@@ -74,25 +76,32 @@
 #'   c("c", "d", "e") ~ "high"
 #' )
 #'
-#' # `.default` is allowed to be vectorized, and you can supply a `.ptype` to
-#' # force a particular output type. Combining these features together allows
-#' # you to create a type stable "replace match" helper.
-#' replace_match <- function(.x, ...) {
-#'   case_match(.x, ..., .default = .x, .ptype = .x)
-#' }
+#' # `case_match()` isn't limited to character input,
+#' # you can match against any vector type
+#' y <- c(1, 2, 1, 3, 1, NA, 2, 4)
 #'
-#' replace_match(x, NA ~ "foo")
+#' case_match(
+#'   y,
+#'   c(1, 3) ~ "odd",
+#'   c(2, 4) ~ "even",
+#'   .default = "missing"
+#' )
+#'
+#' # Setting `.default` to the original vector is a useful way to replace
+#' # selected values, leaving everything else as is
+#' case_match(y, NA ~ 0, .default = y)
 #'
 #' starwars %>%
 #'   mutate(
 #'     # Replace missings, but leave everything else alone
-#'     hair_color = replace_match(hair_color, NA ~ "unknown"),
+#'     hair_color = case_match(hair_color, NA ~ "unknown", .default = hair_color),
 #'     # Replace some, but not all, of the species
-#'     species = replace_match(
+#'     species = case_match(
 #'       species,
 #'       "Human" ~ "Humanoid",
 #'       "Droid" ~ "Robot",
-#'       c("Wookiee", "Ewok") ~ "Hairy"
+#'       c("Wookiee", "Ewok") ~ "Hairy",
+#'       .default = species
 #'     ),
 #'     .keep = "used"
 #'   )
