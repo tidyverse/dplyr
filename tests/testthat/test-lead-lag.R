d6fc85ca150b1553667939e2cfb99a8d5d1c4218
Hadley Wickham <h.wickham@gmail.com>
2022-08-18 14:07:03 -0500

    Refactor lead/lag tests

--- tests/testthat/test-lead-lag.R
+++ tests/testthat/test-lead-lag.R
@@ -1,9 +1,19 @@
-# ------------------------------------------------------------------------------
-# lead() / lag()
-
 test_that("`lead()` / `lag()` get the direction right", {
   expect_identical(lead(1:5), c(2:5, NA))
   expect_identical(lag(1:5), c(NA, 1:4))
+})
+
+test_that("If n = 0, lead and lag return x", {
+  x <- c(10L, 8L, 1L, 3L, 6L, 9L, 4L, 2L, 5L, 7L)
+  expect_equal(lead(x, 0), x)
+  expect_equal(lag(x, 0), x)
+})
+
+test_that("If n = length(x), returns all missing", {
+  x <- c(10L, 8L, 1L, 3L, 6L, 9L, 4L, 2L, 5L, 7L)
+
+  expect_equal(lead(x, length(x)), rep(NA_integer_, length(x)))
+  expect_equal(lag(x, length(x)), rep(NA_integer_, length(x)))
 })
 
 test_that("`lead()` / `lag()` catch negative `n`", {
@@ -80,43 +90,6 @@ test_that("lead and lag preserves dates and times", {
 
   expect_s3_class(lead(y), "POSIXct")
   expect_s3_class(lag(y), "POSIXct")
-})
-
-test_that("#925 is fixed", {
-  data <- tibble(
-    name = c("Rob", "Pete", "Rob", "John", "Rob", "Pete", "John", "Pete", "John", "Pete", "Rob", "Rob"),
-    time = c(3, 2, 5, 3, 2, 3, 2, 4, 1, 1, 4, 1)
-  )
-  res <- data %>% group_by(name) %>% mutate(lag_time = lag(time))
-  expect_equal(
-    res$lag_time[res$name == "Rob"],
-    c(NA, head(data$time[data$name == "Rob"], -1))
-  )
-  expect_equal(
-    res$lag_time[res$name == "Pete"],
-    c(NA, head(data$time[data$name == "Pete"], -1))
-  )
-  expect_equal(
-    res$lag_time[res$name == "John"],
-    c(NA, head(data$time[data$name == "John"], -1))
-  )
-})
-
-test_that("#937 is fixed", {
-  df <- tibble(
-    name = rep(c("Al", "Jen"), 3),
-    score = rep(c(100, 80, 60), 2)
-  )
-
-  res <- df %>% group_by(name) %>% mutate(next.score = lead(score))
-  expect_equal(
-    res$next.score[res$name == "Al"],
-    c(tail(df$score[df$name == "Al"], -1), NA)
-  )
-  expect_equal(
-    res$next.score[res$name == "Jen"],
-    c(tail(df$score[df$name == "Jen"], -1), NA)
-  )
 })
 
 test_that("`lead()` / `lag()` require that `x` is a vector", {
@@ -223,77 +196,4 @@ test_that("`order_by` must be the same size as `x`", {
   expect_snapshot(error = TRUE, {
     shift(1:5, order_by = 1:4)
   })
-})
-
-
-
-test_that("lead and lag simple hybrid version gives correct results (#133)", {
-  res <- group_by(mtcars, cyl) %>%
-    mutate(disp_lag_2 = lag(disp, 2), disp_lead_2 = lead(disp, 2)) %>%
-    summarise(
-      lag1 = all(is.na(head(disp_lag_2, 2))),
-      lag2 = all(!is.na(tail(disp_lag_2, -2))),
-
-      lead1 = all(is.na(tail(disp_lead_2, 2))),
-      lead2 = all(!is.na(head(disp_lead_2, -2)))
-    )
-
-  expect_true(all(res$lag1))
-  expect_true(all(res$lag2))
-
-  expect_true(all(res$lead1))
-  expect_true(all(res$lead2))
-})
-
-
-test_that("lag and lead work on factors inside mutate (#955)", {
-  test_factor <- factor(rep(c("A", "B", "C"), each = 3))
-  exp_lag  <- test_factor != lag(test_factor)
-  exp_lead <- test_factor != lead(test_factor)
-
-  test_df <- tibble(test = test_factor)
-  res <- test_df %>% mutate(
-    is_diff_lag  = (test != lag(test)),
-    is_diff_lead = (test != lead(test))
-  )
-  expect_equal(exp_lag, res$is_diff_lag)
-  expect_equal(exp_lead, res$is_diff_lead)
-})
-
-test_that("lag handles default argument in mutate (#915)", {
-  blah <- data.frame(x1 = c(5, 10, 20, 27, 35, 58, 5, 6), y = 8:1)
-  blah <- mutate(blah,
-    x2 = x1 - lag(x1, n = 1, default = 0),
-    x3 = x1 - lead(x1, n = 1, default = 0),
-    x4 = lag(x1, n = 1L, order_by = y),
-    x5 = lead(x1, n = 1L, order_by = y)
-  )
-  expect_equal(blah$x2, blah$x1 - lag(blah$x1, n = 1, default = 0))
-  expect_equal(blah$x3, blah$x1 - lead(blah$x1, n = 1, default = 0))
-  expect_equal(blah$x4, lag(blah$x1, n = 1L, order_by = blah$y))
-  expect_equal(blah$x5, lead(blah$x1, n = 1L, order_by = blah$y))
-})
-
-test_that("If n = 0, lead and lag return x", {
-  expect_equal(lead(1:2, 0), 1:2)
-  expect_equal(lag(1:2, 0), 1:2)
-})
-
-test_that("If n = length(x), returns all missing", {
-  miss <- rep(NA_integer_, 2)
-
-  expect_equal(lead(1:2, 2), miss)
-  expect_equal(lag(1:2, 2), miss)
-})
-
-
-test_that("lead/lag inside mutate handles expressions as value for default (#1411) ", {
-  df <- tibble(x = 1:3)
-  res <- mutate(df, leadn = lead(x, default = x[1]), lagn = lag(x, default = x[1]))
-  expect_equal(res$leadn, lead(df$x, default = df$x[1]))
-  expect_equal(res$lagn, lag(df$x, default = df$x[1]))
-
-  res <- mutate(df, leadn = lead(x, default = c(1)), lagn = lag(x, default = c(1)))
-  expect_equal(res$leadn, lead(df$x, default = 1))
-  expect_equal(res$lagn, lag(df$x, default = 1))
 })
