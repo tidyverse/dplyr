b6708de27d66836f30dab6ad267c5bb6a6cc6315
Hadley Wickham <h.wickham@gmail.com>
2022-08-18 14:47:59 -0500

    Move single use functions out of utils

--- R/slice.R
+++ R/slice.R
@@ -500,3 +500,10 @@ slice_rank_idx <- function(
   which[order(ranks[which])]
 }
 
+# Backports for R 3.5.0 utils
+...length2 <- function(frame = caller_env()) {
+  length(env_get(frame, "..."))
+}
+...elt2 <- function(i, frame = caller_env()) {
+  eval_bare(sym(paste0("..", i)), frame)
+}
