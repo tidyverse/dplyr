e0183370e74bf8c513512dd985a5156be0db8f30
Kirill Müller <krlmlr+r@mailbox.org>
2022-11-03 14:27:02 +0100

    Implement slice_head()

--- R/slice.R
+++ R/slice.R
@@ -148,14 +148,13 @@ slice_head <- function(.data, ..., n, prop, by = NULL) {
 #' @export
 slice_head.data.frame <- function(.data, ..., n, prop, by = NULL) {
   size <- get_slice_size(n = n, prop = prop)
-  idx <- function(n) {
-    seq2(1, size(n))
-  }
 
   dplyr_local_error_call()
-  dplyr_local_slice_by_arg("by")
 
-  slice(.data, idx(dplyr::n()), .by = {{ by }})
+  rel <- relational::duckdb_rel_from_df(.data)
+  out_rel <- relational::rel_limit(rel, n)
+  out <- relational::rel_to_df(out_rel)
+  dplyr_reconstruct(out, .data)
 }
 
 #' @export
