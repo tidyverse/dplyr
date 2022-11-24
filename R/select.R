6bbd488add59d3776ddaadefc519d07ae6981fef
Kirill Müller <krlmlr+r@mailbox.org>
2022-11-24 19:38:39 +0100

    select() uses rel_try()

--- R/select.R
+++ R/select.R
@@ -71,44 +71,23 @@ select.data.frame <- function(.data, ...) {
   )
   loc <- ensure_group_vars(loc, .data, notify = TRUE)
 
-  if (length(loc) == 0) {
-    # Special case: relational can't handle empty projections
-    return(.data[0])
-  }
-
-  if (typeof(.row_names_info(.data, 0L)) != "integer") {
-    # Fall back for row names
-    return(select_data_frame_fallback(.data, loc))
-  }
-
-  if (inherits(.data, "grouped_df")) {
-    # Fall back for grouped df
-    return(select_data_frame_fallback(.data, loc))
-  }
-
   loc_name <- names(.data)[loc]
   exprs <- map2(names(.data)[loc], names(loc), ~ relational::expr_reference(.x, alias = .y))
 
-  rel <- NULL
-  tryCatch(
-    {
+  rel_try({
+    if (length(loc) == 0) {
+      error_cnd(message = "Can't use relational with zero-column result set.")
+    } else {
       rel <- relational::duckdb_rel_from_df(.data)
-    },
-    error = function(e) {}
-  )
-
-  if (is.null(rel)) {
-    return(select_data_frame_fallback(.data, loc))
-  }
+    }
+  }, fallback = {
+    out <- dplyr_col_select(.data, loc)
+    return(set_names(out, names(loc)))
+  })
 
   out_rel <- relational::rel_project(rel, exprs)
   out <- relational::rel_to_df(out_rel)
   dplyr_reconstruct(out, .data)
-}
-
-select_data_frame_fallback <- function(.data, loc) {
-  out <- dplyr_col_select(.data, loc)
-  set_names(out, names(loc))
 }
 
 
