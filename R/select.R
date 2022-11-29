5651dbcfe3d85acd08c3eb27bf8b67ae648d5bea
DavisVaughan <davis@rstudio.com>
2022-11-29 10:01:22 -0500

    Remove options now set by roxygen2 automatically as of 7.2.1
    https://github.com/r-lib/roxygen2/blob/main/NEWS.md#roxygen2-721

--- R/select.R
+++ R/select.R
@@ -42,10 +42,7 @@
 #'   knitr::knit_child("man/rmd/select.Rmd"),
 #'   tibble.print_min = 4,
 #'   tibble.max_extra_cols = 8,
-#'   digits = 2,
-#'   crayon.enabled = FALSE,
-#'   cli.unicode = FALSE,
-#'   width = 80
+#'   digits = 2
 #' )
 #' cat(result, sep = "\n")
 #' ```
