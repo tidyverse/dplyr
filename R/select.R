6e484950c1bbd1f75d0bd23d1ec737810c7d9ed2
Kirill Müller <krlmlr+r@mailbox.org>
2022-12-11 09:55:42 +0100

    Don't need ensure_group_vars()

--- R/select.R
+++ R/select.R
@@ -68,9 +68,9 @@ select.data.frame <- function(.data, ...) {
     data = .data,
     error_call = error_call
   )
-  loc <- ensure_group_vars(loc, .data, notify = TRUE)
 
   # duckplyr: Backend-specific
+  loc <- ensure_group_vars(loc, .data, notify = TRUE)
   out <- dplyr_col_select(.data, loc)
   out <- set_names(out, names(loc))
 
