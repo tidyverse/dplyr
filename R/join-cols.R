aa6dc7288eaa13606c905d0e7c38be9266ccafe9
Davis Vaughan <davis@posit.co>
2024-06-26 09:11:20 -0400

    Move `vec_ptype_finalise()` into `join_ptype_common()`

--- R/join-cols.R
+++ R/join-cols.R
@@ -181,22 +181,23 @@ add_suffixes <- function(x, y, suffix) {
 }
 
 join_cast_common <- function(x, y, vars, error_call = caller_env()) {
-  ptype <- join_ptype_common(x, y, vars, error_call)
-
-  # Finalize unspecified columns (#6804)
-  ptype <- vec_ptype_finalise(ptype)
-
+  ptype <- join_ptype_common(x, y, vars, error_call = error_call)
   vec_cast_common(x = x, y = y, .to = ptype, .call = error_call)
 }
 
 join_ptype_common <- function(x, y, vars, error_call = caller_env()) {
   # Explicit `x/y_arg = ""` to avoid auto naming in `cnd$x_arg`
-  try_fetch(
+  ptype <- try_fetch(
     vec_ptype2(x, y, x_arg = "", y_arg = "", call = error_call),
     vctrs_error_incompatible_type = function(cnd) {
       rethrow_error_join_incompatible_type(cnd, vars, error_call)
     }
   )
+
+  # Finalize unspecified columns (#6804)
+  ptype <- vec_ptype_finalise(ptype)
+
+  ptype
 }
 
 rethrow_error_join_incompatible_type <- function(cnd, vars, call) {
