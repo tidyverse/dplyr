861c8a5d35cfa7cadfe3abad8eb885fc473b475b
Davis Vaughan <davis@rstudio.com>
2025-02-19 11:21:46 -0500

    Remove `archive/` folder (#7660)

--- archive/0.7.0-latest/.Rprofile
+++
@@ -1,3 +0,0 @@
-#### -- Packrat Autoloader (version 0.4.8-59) -- ####
-source("packrat/init.R")
-#### -- End Packrat Autoloader -- ####
--- archive/run-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Tested on Ubuntu.
-
-set -e
-
-dir=$(dirname $0)
-
-ls -d ${dir}/?.*.*-* | sort | parallel --keep-order --gnu -I"{}" -q ${dir}/run-in.sh "{}" "$@"
