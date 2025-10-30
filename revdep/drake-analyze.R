82eedad3186e0b099e4833d925e2974a666dd244
Davis Vaughan <davis@rstudio.com>
2025-10-30 15:31:17 -0400

    Cleanup dead files (#7755)
    * Remove unused `move.yml` workflow
    
    * Remove unused `lock.yml` workflow
    
    * Remove `CODEOWNERS`
    
    It would only automatically ping for review if someone touched the CODEOWNERS file itself?
    
    * Remove `inst/`
    
    * Remove `Doxyfile`
    
    * Remove unused `revdep/` files
    
    * Remove `.gitignore` from `vignettes/`
    
    * Trim down `.gitignore`
    
    * Trim down `.Rbuildignore`
    
    * Revert "Remove `CODEOWNERS`"
    
    This reverts commit 40fa64a0261c35e5c50e03b440310d6f1f814b43.

--- revdep/drake-analyze.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-
-all <- drake::readd(compare_all)
-
-error <- map_chr(map(all, class), 1) == "try-error"
-
-succeeded <- all[!error]
-
-withr::with_output_sink("revdep/new-problems.md", revdepcheck::revdep_report_problems(results = succeeded))
