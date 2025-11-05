05f66434e626252ce5e8e106283606fee857ee1c
Davis Vaughan <davis@posit.co>
2025-11-05 10:37:18 -0500

    Replace `missing` with `na_rm`

--- R/when.R
+++ R/when.R
@@ -3,8 +3,12 @@ NULL
 
 #' @rdname when-any-all
 #' @export
-when_any <- function(..., missing = NULL, size = NULL) {
+when_any <- function(..., na_rm = FALSE, size = NULL) {
   check_dots_unnamed()
+
+  check_bool(na_rm)
+  missing <- if (na_rm) FALSE else NULL
+
   list_pany(
     x = list2(...),
     missing = missing,
@@ -16,8 +20,12 @@ when_any <- function(..., missing = NULL, size = NULL) {
 
 #' @rdname when-any-all
 #' @export
-when_all <- function(..., missing = NULL, size = NULL) {
+when_all <- function(..., na_rm = FALSE, size = NULL) {
   check_dots_unnamed()
+
+  check_bool(na_rm)
+  missing <- if (na_rm) TRUE else NULL
+
   list_pall(
     x = list2(...),
     missing = missing,
