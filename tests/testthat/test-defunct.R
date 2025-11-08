f8aca67e267cb7a133baa1d5fc4a7f71de985a74
Davis Vaughan <davis@posit.co>
2025-11-08 15:11:52 -0500

    Fully remove previously defunct functions

--- tests/testthat/test-defunct.R
+++ tests/testthat/test-defunct.R
@@ -1,23 +1,5 @@
 test_that("generate informative errors", {
   expect_snapshot(error = TRUE, {
-    id()
-
-    failwith()
-
-    select_vars()
-    rename_vars()
-    select_var()
-    current_vars()
-
-    bench_tbls()
-    compare_tbls()
-    compare_tbls2()
-    eval_tbls()
-    eval_tbls2()
-
-    location()
-    changes()
-
     combine()
 
     src_mysql()
