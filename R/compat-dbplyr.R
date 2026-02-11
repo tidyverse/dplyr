e924ab9d950d68d31a50c7c63c4d17eee7882256
Davis Vaughan <davis@posit.co>
2026-02-11 16:08:52 -0500

    Additional updates for dbplyr 2.6.0

--- R/compat-dbplyr.R
+++ R/compat-dbplyr.R
@@ -58,6 +58,14 @@ utils::globalVariables("!<-")
 
 #' @inherit dbplyr::sql
 #' @export
+#' @examplesIf requireNamespace("dbplyr", quietly = TRUE)
+#' # sql() just adds a class
+#' sql("x + 1")
+#' dbplyr::is.sql(sql("x + 1"))
+#'
+#' # You can use it when you need to insert some literal SQL in a query
+#' db <- dbplyr::memdb_frame(x = 1:3)
+#' db |> mutate(y = sql("CAST(x as VARCHAR)"))
 sql <- function(...) {
   check_dbplyr()
   dbplyr::sql(...)
@@ -65,9 +73,8 @@ sql <- function(...) {
 
 #' @inherit dbplyr::ident
 #' @export
-#' @examples
-#' # Identifiers are escaped with "
 #' @examplesIf requireNamespace("dbplyr", quietly = TRUE)
+#' # Identifiers are escaped with "
 #' ident("x")
 ident <- function(...) {
   check_dbplyr()
