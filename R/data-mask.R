tidyverse/dplyr

commit 59d0a78e9c96d4f2cd968b7c77d54d2b85a36b0d

Drop `caller_env` from `DataMask` (#6444)
And remove now unneeded `caller_env` arguments in other functions.

R/data-mask.R
@@ -1,6 +1,6 @@
 DataMask <- R6Class("DataMask",
   public = list(
-    initialize = function(data, caller, verb, error_call) {
+    initialize = function(data, verb, error_call) {
       rows <- group_rows(data)
       if (length(rows) == 0) {
         # Specially handle case of zero groups
@@ -17,7 +17,6 @@ DataMask <- R6Class("DataMask",
       }
       names(data) <- names_bindings
       private$size <- nrow(data)
-      private$caller <- caller
       private$current_data <- unclass(data)
 
       private$chops <- .Call(dplyr_lazy_vec_chop_impl, data, rows)
@@ -185,10 +184,6 @@ DataMask <- R6Class("DataMask",
 
     get_rlang_mask = function() {
       private$mask
-    },
-
-    get_caller_env = function() {
-      private$caller
     }
 
   ),
@@ -225,9 +220,6 @@ DataMask <- R6Class("DataMask",
     # Type of data frame
     grouped_df = NULL,
     rowwise_df = NULL,
-
-    # caller environment of the verb (summarise(), ...)
-    caller = NULL,
 
     verb = character()
   )

R/across.R
@@ -628,7 +628,7 @@ expand_across <- function(quo) {
     !!cols,
     fns = eval_tidy(expr$.fns, mask, env = env),
     names = eval_tidy(expr$.names, mask, env = env),
-    .caller_env = dplyr_mask$get_caller_env(),
+    .caller_env = env,
     mask = dplyr_mask,
     inline = TRUE
   )

R/distinct.R
@@ -88,10 +88,7 @@ distinct_prepare <- function(.data,
   }
 
   # If any calls, use mutate to add new columns, then distinct on those
-  computed_columns <- add_computed_columns(.data, vars,
-    caller_env = caller_env,
-    error_call = error_call
-  )
+  computed_columns <- add_computed_columns(.data, vars, error_call = error_call)
   .data <- computed_columns$data
   distinct_vars <- computed_columns$added_names
 

R/filter.R
@@ -111,17 +111,17 @@ filter <- function(.data, ..., .preserve = FALSE) {
 
 #' @export
 filter.data.frame <- function(.data, ..., .preserve = FALSE) {
-  loc <- filter_rows(.data, ..., caller_env = caller_env())
+  loc <- filter_rows(.data, ...)
   dplyr_row_slice(.data, loc, preserve = .preserve)
 }
 
-filter_rows <- function(.data, ..., caller_env, error_call = caller_env()) {
+filter_rows <- function(.data, ..., error_call = caller_env()) {
   error_call <- dplyr_error_call(error_call)
 
   dots <- dplyr_quosures(...)
   check_filter(dots, error_call = error_call)
 
-  mask <- DataMask$new(.data, caller_env, "filter", error_call = error_call)
+  mask <- DataMask$new(.data, "filter", error_call = error_call)
   on.exit(mask$forget(), add = TRUE)
 
   dots <- filter_expand(dots, mask = mask, error_call = error_call)

R/group-by.r
@@ -141,7 +141,6 @@ group_by.data.frame <- function(.data, ..., .add = FALSE, .drop = group_by_drop_
     .data,
     ...,
     .add = .add,
-    caller_env = caller_env(),
     error_call = current_env()
   )
   grouped_df(groups$data, groups$group_names, .drop)
@@ -192,7 +191,6 @@ ungroup.data.frame <- function(x, ...) {
 #' @keywords internal
 group_by_prepare <- function(.data,
                              ...,
-                             caller_env = caller_env(2),
                              .add = FALSE,
                              .dots = deprecated(),
                              add = deprecated(),
@@ -208,14 +206,11 @@ group_by_prepare <- function(.data,
   if (!missing(.dots)) {
     # Used by dbplyr 1.4.2 so can't aggressively deprecate
     lifecycle::deprecate_warn("1.0.0", "group_by(.dots = )", always = TRUE)
-    new_groups <- c(new_groups, compat_lazy_dots(.dots, env = caller_env))
+    new_groups <- c(new_groups, compat_lazy_dots(.dots, env = caller_env(2)))
   }
 
   # If any calls, use mutate to add new columns, then group by those
-  computed_columns <- add_computed_columns(.data, new_groups,
-    caller_env = caller_env,
-    error_call = error_call
-  )
+  computed_columns <- add_computed_columns(.data, new_groups, error_call = error_call)
 
   out <- computed_columns$data
   group_names <- computed_columns$added_names
@@ -242,7 +237,6 @@ group_by_prepare <- function(.data,
 
 add_computed_columns <- function(.data,
                                  vars,
-                                 caller_env,
                                  error_call = caller_env()) {
   is_symbol <- map_lgl(vars, quo_is_variable_reference)
   needs_mutate <- have_name(vars) | !is_symbol
@@ -253,7 +247,6 @@ add_computed_columns <- function(.data,
       cols <- mutate_cols(
         ungroup(.data),
         dplyr_quosures(!!!vars),
-        caller_env = caller_env,
         error_call = error_call
       )
 

R/mutate.R
@@ -172,7 +172,7 @@ mutate.data.frame <- function(.data,
                               .after = NULL) {
   keep <- arg_match(.keep)
 
-  cols <- mutate_cols(.data, dplyr_quosures(...), caller_env = caller_env())
+  cols <- mutate_cols(.data, dplyr_quosures(...))
   used <- attr(cols, "used")
 
   out <- dplyr_col_modify(.data, cols)
@@ -218,13 +218,13 @@ mutate.data.frame <- function(.data,
 
 # Helpers -----------------------------------------------------------------
 
-mutate_cols <- function(.data, dots, caller_env, error_call = caller_env()) {
+mutate_cols <- function(.data, dots, error_call = caller_env()) {
   # Collect dots before setting up error handlers (#6178)
   force(dots)
 
   error_call <- dplyr_error_call(error_call)
 
-  mask <- DataMask$new(.data, caller_env, "mutate", error_call = error_call)
+  mask <- DataMask$new(.data, "mutate", error_call = error_call)
   old_current_column <- context_peek_bare("column")
 
   on.exit(context_poke("column", old_current_column), add = TRUE)

R/slice.R
@@ -123,7 +123,7 @@ slice <- function(.data, ..., .preserve = FALSE) {
 
 #' @export
 slice.data.frame <- function(.data, ..., .preserve = FALSE) {
-  loc <- slice_rows(.data, ..., caller_env = caller_env(), error_call = current_env())
+  loc <- slice_rows(.data, ..., error_call = current_env())
   dplyr_row_slice(.data, loc, preserve = .preserve)
 }
 
@@ -270,14 +270,14 @@ slice_sample.data.frame <- function(.data, ..., n, prop, weight_by = NULL, repla
 
 # helpers -----------------------------------------------------------------
 
-slice_rows <- function(.data, ..., caller_env, error_call = caller_env()) {
+slice_rows <- function(.data, ..., error_call = caller_env()) {
   error_call <- dplyr_error_call(error_call)
 
   dots <- enquos(...)
   if (is_empty(dots)) {
     return(TRUE)
   }
-  mask <- DataMask$new(.data, caller_env, "slice", error_call = error_call)
+  mask <- DataMask$new(.data, "slice", error_call = error_call)
   on.exit(mask$forget(), add = TRUE)
 
   chunks <- slice_eval(mask, dots, error_call = error_call)

commit dd90e9495008532483561be3b210ce92ca5c2d49

Remove all C level non-API calls (#7819)
* Do 1:1 replacement of `Rf_findVarInFrame()` with `env_get()`

* Use `R_removeVarFromFrame()` not `rlang::env_unbind()`. Closes #7818.

* Remove unused `quo_get_expr()` C callable import

* Remove broken reliance on a `caller` environment that didn't exist!

Bug introduced in https://github.com/tidyverse/dplyr/pull/6444 where `caller` was removed from the DataMask's private environment, not realizing that we referenced it in the C code.

Switching to `env_get()` here revealed the bug, since it errors on unbound values.

This did not cause us any problems before because we always passed a quosure to `rlang::eval_tidy()`, which immediately overwrites the `env` argument with the quosure's environment, so it overwrote our faulty `R_UnboundValue` input. I have now encoded this assumption up front with `check_quosure()`.

* Check for "used" bindings with new `R_GetBindingType()` API

* Use new `R_MakeDelayedBinding()` to remove non-API promise utilities

* We want `inherits = FALSE` in `R_getVar()`

To match `Rf_findVarInFrame3()`

* Remove stray include

* NEWS bullet

* Use `rlang_is_quosure` callable

R/data-mask.R
@@ -224,7 +224,11 @@ DataMask <- R6Class(
     },
 
     get_used = function() {
-      .Call(env_resolved, private$chops, names(private$current_data))
+      .Call(
+        ffi_env_bindings_are_used,
+        private$chops,
+        names(private$current_data)
+      )
     },
 
     unused_vars = function() {

src/chop.cpp
@@ -1,4 +1,5 @@
 #include "dplyr.h"
+#include "utils.h"
 
 SEXP new_environment(int size, SEXP parent)  {
   SEXP call = PROTECT(Rf_lang4(dplyr::symbols::new_env, Rf_ScalarLogical(TRUE), parent, Rf_ScalarInteger(size)));
@@ -13,33 +14,41 @@ void dplyr_lazy_vec_chop_grouped(SEXP chops_env, SEXP rows, SEXP data, bool roww
 
   const SEXP* p_data = VECTOR_PTR_RO(data);
   const SEXP* p_names = STRING_PTR_RO(names);
+
+  SEXP eval_env = R_EmptyEnv;
+
   for (R_xlen_t i = 0; i < n; i++) {
-    SEXP prom = PROTECT(Rf_allocSExp(PROMSXP));
-    SET_PRENV(prom, R_EmptyEnv);
     SEXP column = p_data[i];
+    SEXP sym = PROTECT(rlang::str_as_symbol(p_names[i]));
 
     if (rowwise && vctrs::obj_is_list(column)) {
-      if (Rf_length(column) == 0) {
-        SEXP ptype = PROTECT(Rf_getAttrib(column, Rf_install("ptype")));
-        column = PROTECT(Rf_allocVector(VECSXP, 1));
-        if (ptype != R_NilValue) {
-          SET_VECTOR_ELT(column, 0, ptype);
-        } else {
-          // i.e. `vec_ptype_finalise(unspecified())` (#6369)
-          SET_VECTOR_ELT(column, 0, Rf_allocVector(LGLSXP, 0));
+      // Special handling of list-columns in rowwise mode
+      if (Rf_xlength(column) == 0) {
+        // List-col is empty, bind a length 1 list-col with either its `ptype`
+        // attribute (for list-of) or a fallback to `logical()` (i.e.
+        // `vec_ptype_finalise(unspecified())` as the sole element (#6369)).
+        SEXP ptype = Rf_getAttrib(column, Rf_install("ptype"));
+        if (ptype == R_NilValue) {
+          ptype = Rf_allocVector(LGLSXP, 0);
         }
-        SET_PRCODE(prom, column);
+        PROTECT(ptype);
+
+        column = PROTECT(Rf_allocVector(VECSXP, 1));
+        SET_VECTOR_ELT(column, 0, ptype);
+
+        env_bind_delayed(chops_env, sym, column, eval_env);
         UNPROTECT(2);
       } else {
-        SET_PRCODE(prom, column);
+        // List-col has length, bind it directly
+        env_bind_delayed(chops_env, sym, column, eval_env);
       }
     } else {
-      SET_PRCODE(prom, Rf_lang3(dplyr::functions::vec_chop, column, rows));
+      // Standard grouped case is to bind a delayed call to `vec_chop(column, rows)`
+      SEXP expr = PROTECT(Rf_lang3(dplyr::functions::vec_chop, column, rows));
+      env_bind_delayed(chops_env, sym, expr, eval_env);
+      UNPROTECT(1);
     }
 
-    SET_PRVALUE(prom, R_UnboundValue);
-
-    Rf_defineVar(rlang::str_as_symbol(p_names[i]), prom, chops_env);
     UNPROTECT(1);
   }
 
@@ -52,15 +61,16 @@ void dplyr_lazy_vec_chop_ungrouped(SEXP chops_env, SEXP data) {
 
   const SEXP* p_data = VECTOR_PTR_RO(data);
   const SEXP* p_names = STRING_PTR_RO(names);
-  for (R_xlen_t i = 0; i < n; i++) {
-    SEXP prom = PROTECT(Rf_allocSExp(PROMSXP));
-    SET_PRENV(prom, R_EmptyEnv);
-    SET_PRCODE(prom, Rf_lang2(dplyr::functions::list, p_data[i]));
-    SET_PRVALUE(prom, R_UnboundValue);
 
-    SEXP symb = rlang::str_as_symbol(p_names[i]);
-    Rf_defineVar(symb, prom, chops_env);
-    UNPROTECT(1);
+  SEXP eval_env = R_EmptyEnv;
+
+  // Bind a delayed call to `list(column)`, i.e. 1 group containing the whole column
+  for (R_xlen_t i = 0; i < n; i++) {
+    SEXP column = p_data[i];
+    SEXP sym = PROTECT(rlang::str_as_symbol(p_names[i]));
+    SEXP expr = PROTECT(Rf_lang2(dplyr::functions::list, column));
+    env_bind_delayed(chops_env, sym, expr, eval_env);
+    UNPROTECT(2);
   }
 
   UNPROTECT(1);
@@ -121,23 +131,48 @@ SEXP dplyr_make_mask_bindings(SEXP env_chops, SEXP data) {
   return env_mask_bindings;
 }
 
-SEXP env_resolved(SEXP env, SEXP names) {
+static inline
+bool env_binding_is_delayed(SEXP env, SEXP sym) {
+#if (R_VERSION >= R_Version(4, 6, 0))
+  R_BindingType_t type = R_GetBindingType(sym, env);
+  if (type == R_BindingTypeUnbound) {
+    Rf_errorcall(R_NilValue, "Internal error: Unbound chops binding.");
+  }
+  return type == R_BindingTypeDelayed;
+#else
+  SEXP value = Rf_findVarInFrame3(env, sym, FALSE);
+  if (value == R_UnboundValue) {
+    Rf_errorcall(R_NilValue, "Internal error: Unbound chops binding.");
+  }
+  return TYPEOF(value) == PROMSXP && PRVALUE(value) == R_UnboundValue;
+#endif
+}
+
+// Detect "used" chops bindings
+//
+// We first require that the binding exists, otherwise that would
+// be an internal error on our part.
+//
+// Then we check to see if the binding is delayed. If it is, then it is
+// considered "unused".
+//
+// Used bindings include:
+// - Forced chop promises (i.e. if the user referenced an original column in an expression)
+// - Newly installed columns from a previous expression
+SEXP ffi_env_bindings_are_used(SEXP chops_env, SEXP names) {
   R_xlen_t n = XLENGTH(names);
   SEXP res = PROTECT(Rf_allocVector(LGLSXP, n));
 
   int* p_res = LOGICAL(res);
   const SEXP* p_names = STRING_PTR_RO(names);
 
-  for(R_xlen_t i = 0; i < n; i++) {
-    SEXP name = PROTECT(rlang::str_as_symbol(p_names[i]));
-    SEXP prom = PROTECT(Rf_findVarInFrame(env, name));
-    SEXP val = TYPEOF(prom) == PROMSXP ? PRVALUE(prom) : prom;
-    p_res[i] = val != R_UnboundValue;
-    UNPROTECT(2);
+  for (R_xlen_t i = 0; i < n; i++) {
+    SEXP sym = PROTECT(rlang::str_as_symbol(p_names[i]));
+    p_res[i] = !env_binding_is_delayed(chops_env, sym);
+    UNPROTECT(1);
   }
 
   Rf_namesgets(res, names);
   UNPROTECT(1);
   return res;
 }
-

src/dplyr.h
@@ -36,6 +36,23 @@ SEXP R_mapAttrib(SEXP x, SEXP (*FUN)(SEXP, SEXP, void *), void *data) {
 }
 #endif
 
+// Poorman's version of rlang's `r_env_get()`, which more faithfully handles
+// edge cases like active bindings, promises, and missing args. Only use this
+// for extracting a simple variable that you KNOW exists in the env, like
+// anything in our `private` envs.
+static inline
+SEXP env_get(SEXP env, SEXP sym) {
+#if R_VERSION >= R_Version(4, 5, 0)
+  return R_getVar(sym, env, FALSE);
+#else
+  SEXP value = Rf_findVarInFrame3(env, sym, FALSE);
+  if (value == R_UnboundValue) {
+    Rf_errorcall(R_NilValue, "object '%s' not found", CHAR(PRINTNAME(sym)));
+  }
+  return value;
+#endif
+}
+
 namespace dplyr {
 
 struct envs {
@@ -52,7 +69,6 @@ struct symbols {
   static SEXP current_group_size;
   static SEXP current_expression;
   static SEXP rows;
-  static SEXP caller;
   static SEXP current_data;
   static SEXP dot_drop;
   static SEXP dplyr_internal_error;
@@ -88,8 +104,8 @@ namespace rlang {
 SEXP eval_tidy(SEXP expr, SEXP data, SEXP env);
 SEXP as_data_pronoun(SEXP x);
 SEXP new_data_mask(SEXP bottom, SEXP top);
-SEXP str_as_symbol(SEXP);
-void env_unbind(SEXP, SEXP);
+SEXP str_as_symbol(SEXP str);
+bool is_quosure(SEXP x);
 }
 
 namespace vctrs {
@@ -137,28 +153,27 @@ SEXP dplyr_mask_binding_add(SEXP env_private, SEXP s_name, SEXP ptype, SEXP chun
 
 SEXP dplyr_lazy_vec_chop(SEXP data, SEXP rows, SEXP env_current_group_info, SEXP ffi_grouped, SEXP ffi_rowwise);
 SEXP dplyr_make_mask_bindings(SEXP chops, SEXP data);
-SEXP env_resolved(SEXP env, SEXP names);
+SEXP ffi_env_bindings_are_used(SEXP env_chops, SEXP names);
 void add_mask_binding(SEXP name, SEXP env_mask_bindings, SEXP env_chops);
 
 SEXP dplyr_extract_chunks(SEXP df_list, SEXP df_ptype);
 
 #define DPLYR_MASK_INIT()                                                                                            \
-  SEXP rows = PROTECT(Rf_findVarInFrame(env_private, dplyr::symbols::rows));                                         \
+  SEXP rows = PROTECT(env_get(env_private, dplyr::symbols::rows));                                                   \
   const SEXP* v_rows = VECTOR_PTR_RO(rows);                                                                          \
   R_xlen_t ngroups = XLENGTH(rows);                                                                                  \
-  SEXP caller = PROTECT(Rf_findVarInFrame(env_private, dplyr::symbols::caller));                                     \
-  SEXP env_mask_bindings = PROTECT(Rf_findVarInFrame(env_private, dplyr::symbols::env_mask_bindings));               \
+  SEXP env_mask_bindings = PROTECT(env_get(env_private, dplyr::symbols::env_mask_bindings));                         \
   SEXP pronoun = PROTECT(rlang::as_data_pronoun(env_mask_bindings));                                                 \
-  SEXP env_current_group_info = PROTECT(Rf_findVarInFrame(env_private, dplyr::symbols::env_current_group_info));     \
-  SEXP current_group_id = PROTECT(Rf_findVarInFrame(env_current_group_info, dplyr::symbols::current_group_id));      \
+  SEXP env_current_group_info = PROTECT(env_get(env_private, dplyr::symbols::env_current_group_info));               \
+  SEXP current_group_id = PROTECT(env_get(env_current_group_info, dplyr::symbols::current_group_id));                \
   int* p_current_group_id = INTEGER(current_group_id);                                                               \
   *p_current_group_id = 0;                                                                                           \
-  SEXP current_group_size = PROTECT(Rf_findVarInFrame(env_current_group_info, dplyr::symbols::current_group_size));  \
+  SEXP current_group_size = PROTECT(env_get(env_current_group_info, dplyr::symbols::current_group_size));            \
   int* p_current_group_size = INTEGER(current_group_size);                                                           \
   *p_current_group_size = 0
 
 #define DPLYR_MASK_FINALISE()                                  \
-  UNPROTECT(7);                                                \
+  UNPROTECT(6);                                                \
   *p_current_group_id = 0;                                     \
   *p_current_group_size = 0
 
@@ -175,8 +190,8 @@ SEXP dplyr_extract_chunks(SEXP df_list, SEXP df_ptype);
   *p_current_group_id = INDEX + 1;                             \
   *p_current_group_size = Rf_xlength(v_rows[INDEX])
 
-#define DPLYR_MASK_EVAL(quo)                                   \
-  rlang::eval_tidy(quo, mask, caller)
+#define DPLYR_MASK_EVAL(quo, env)                              \
+  rlang::eval_tidy(quo, mask, env)
 
 #define DPLYR_ERROR_INIT(n)                                    \
   SEXP error_data = PROTECT(Rf_allocVector(VECSXP, n));              \

src/filter.cpp
@@ -1,4 +1,5 @@
 #include "dplyr.h"
+#include "utils.h"
 
 namespace dplyr {
 
@@ -98,7 +99,7 @@ bool filter_is_valid_lgl(SEXP x, bool first) {
 static
 SEXP eval_filter_one(SEXP quos,
                      SEXP mask,
-                     SEXP caller,
+                     SEXP env,
                      R_xlen_t n,
                      SEXP env_filter,
                      bool first) {
@@ -120,7 +121,7 @@ SEXP eval_filter_one(SEXP quos,
     SEXP current_expression = PROTECT(Rf_ScalarInteger(i + 1));
     Rf_defineVar(dplyr::symbols::current_expression, current_expression, env_filter);
 
-    SEXP res = PROTECT(rlang::eval_tidy(p_quos[i], mask, caller));
+    SEXP res = PROTECT(rlang::eval_tidy(p_quos[i], mask, env));
 
     const R_xlen_t res_size = vctrs::short_vec_size(res);
     if (res_size != n && res_size != 1) {
@@ -146,6 +147,13 @@ SEXP dplyr_mask_eval_all_filter(SEXP quos,
                                 SEXP s_n,
                                 SEXP env_filter) {
   DPLYR_MASK_INIT();
+
+  // Ensure we pass a list of quosures, which forces `rlang::eval_tidy()` to
+  // extract the environment from the quosure rather than using `env`, so what
+  // we pass as `env` doesn't actually matter.
+  check_list_of_quosures(quos);
+  SEXP env = R_EmptyEnv;
+
   const SEXP* p_rows = VECTOR_PTR_RO(rows);
 
   const R_xlen_t n = Rf_asInteger(s_n);
@@ -165,7 +173,7 @@ SEXP dplyr_mask_eval_all_filter(SEXP quos,
     SEXP result_i = PROTECT(eval_filter_one(
       quos,
       mask,
-      caller,
+      env,
       n_i,
       env_filter,
       first

src/imports.cpp
@@ -8,16 +8,14 @@ struct rlang_api_ptrs_t {
   SEXP (*as_data_pronoun)(SEXP x);
   SEXP (*new_data_mask)(SEXP bottom, SEXP top);
   SEXP (*str_as_symbol)(SEXP str);
-  SEXP (*quo_get_expr)(SEXP quo);
-  void (*env_unbind)(SEXP, SEXP);
+  bool (*is_quosure)(SEXP x);
 
   rlang_api_ptrs_t() {
     eval_tidy       = (SEXP (*)(SEXP, SEXP, SEXP)) R_GetCCallable("rlang", "rlang_eval_tidy");
     as_data_pronoun = (SEXP (*)(SEXP))             R_GetCCallable("rlang", "rlang_as_data_pronoun");
     new_data_mask   = (SEXP (*)(SEXP, SEXP))       R_GetCCallable("rlang", "rlang_new_data_mask_3.0.0");
     str_as_symbol   = (SEXP (*)(SEXP))             R_GetCCallable("rlang", "rlang_str_as_symbol");
-    quo_get_expr    = (SEXP (*)(SEXP))             R_GetCCallable("rlang", "rlang_quo_get_expr");
-    env_unbind      = (void (*)(SEXP, SEXP))       R_GetCCallable("rlang", "rlang_env_unbind");
+    is_quosure      = (bool (*)(SEXP))             R_GetCCallable("rlang", "rlang_is_quosure");
   }
 };
 // *INDENT-ON*
@@ -43,8 +41,8 @@ SEXP str_as_symbol(SEXP str) {
   return rlang_api().str_as_symbol(str);
 }
 
-void env_unbind(SEXP env, SEXP sym) {
-  rlang_api().env_unbind(env, sym);
+bool is_quosure(SEXP x) {
+  return rlang_api().is_quosure(x);
 }
 
 }

src/init.cpp
@@ -46,7 +46,6 @@ SEXP symbols::current_group_id = Rf_install("dplyr:::current_group_id");
 SEXP symbols::current_group_size = Rf_install("dplyr:::current_group_size");
 SEXP symbols::current_expression = Rf_install("current_expression");
 SEXP symbols::rows = Rf_install("rows");
-SEXP symbols::caller = Rf_install("caller");
 SEXP symbols::current_data = Rf_install("current_data");
 SEXP symbols::dot_drop = Rf_install(".drop");
 SEXP symbols::dplyr_internal_error = Rf_install("dplyr_internal_error");
@@ -77,9 +76,9 @@ SEXP dplyr_init_library(SEXP ns_dplyr, SEXP ns_vctrs, SEXP ns_rlang) {
   dplyr::envs::ns_dplyr = ns_dplyr;
   dplyr::envs::ns_vctrs = ns_vctrs;
   dplyr::envs::ns_rlang = ns_rlang;
-  dplyr::functions::vec_chop = PROTECT(Rf_findVarInFrame(ns_vctrs, Rf_install("vec_chop")));
-  dplyr::functions::dot_subset2 = PROTECT(Rf_findVarInFrame(R_BaseEnv, Rf_install(".subset2")));
-  dplyr::functions::list = PROTECT(Rf_findVarInFrame(R_BaseEnv, Rf_install("list")));
+  dplyr::functions::vec_chop = PROTECT(env_get(ns_vctrs, Rf_install("vec_chop")));
+  dplyr::functions::dot_subset2 = PROTECT(env_get(R_BaseEnv, Rf_install(".subset2")));
+  dplyr::functions::list = PROTECT(env_get(R_BaseEnv, Rf_install("list")));
   dplyr::functions::function = PROTECT(Rf_eval(Rf_install("function"), R_BaseEnv));
 
   R_PreserveObject(dplyr::functions::vec_chop);
@@ -122,7 +121,7 @@ static const R_CallMethodDef CallEntries[] = {
 
   {"dplyr_lazy_vec_chop_impl", (DL_FUNC)& dplyr_lazy_vec_chop, 5},
   {"dplyr_make_mask_bindings", (DL_FUNC)& dplyr_make_mask_bindings, 2},
-  {"env_resolved", (DL_FUNC)& env_resolved, 2},
+  {"ffi_env_bindings_are_used", (DL_FUNC)& ffi_env_bindings_are_used, 2},
 
   {"dplyr_extract_chunks", (DL_FUNC)& dplyr_extract_chunks, 2},
 

src/mask.cpp
@@ -45,7 +45,7 @@ SEXP dplyr_mask_binding_add(SEXP env_private, SEXP s_name, SEXP ptype, SEXP chun
   SEXP name = STRING_ELT(s_name, 0);
 
   // we assume control over these
-  SEXP current_data = PROTECT(Rf_findVarInFrame(env_private, dplyr::symbols::current_data));
+  SEXP current_data = PROTECT(env_get(env_private, dplyr::symbols::current_data));
   SEXP current_vars = PROTECT(Rf_getAttrib(current_data, R_NamesSymbol));
 
   // search for position of name
@@ -73,10 +73,10 @@ SEXP dplyr_mask_binding_add(SEXP env_private, SEXP s_name, SEXP ptype, SEXP chun
   }
 
   SEXP sym_name = PROTECT(rlang::str_as_symbol(name));
-  SEXP chops = PROTECT(Rf_findVarInFrame(env_private, dplyr::symbols::chops));
+  SEXP chops = PROTECT(env_get(env_private, dplyr::symbols::chops));
   Rf_defineVar(sym_name, chunks, chops);
 
-  SEXP env_mask_bindings = PROTECT(Rf_findVarInFrame(env_private, dplyr::symbols::env_mask_bindings));
+  SEXP env_mask_bindings = PROTECT(env_get(env_private, dplyr::symbols::env_mask_bindings));
   add_mask_binding(sym_name, env_mask_bindings, chops);
 
   UNPROTECT(5);
@@ -86,7 +86,7 @@ SEXP dplyr_mask_binding_add(SEXP env_private, SEXP s_name, SEXP ptype, SEXP chun
 SEXP dplyr_mask_binding_remove(SEXP env_private, SEXP s_name) {
   SEXP name = STRING_ELT(s_name, 0);
 
-  SEXP current_data = PROTECT(Rf_findVarInFrame(env_private, dplyr::symbols::current_data));
+  SEXP current_data = PROTECT(env_get(env_private, dplyr::symbols::current_data));
   SEXP current_vars = PROTECT(Rf_getAttrib(current_data, R_NamesSymbol));
 
   // search for position of name
@@ -108,11 +108,11 @@ SEXP dplyr_mask_binding_remove(SEXP env_private, SEXP s_name) {
 
     SEXP sym_name = PROTECT(rlang::str_as_symbol(name));
 
-    SEXP chops = PROTECT(Rf_findVarInFrame(env_private, dplyr::symbols::chops));
-    SEXP env_mask_bindings = PROTECT(Rf_findVarInFrame(env_private, dplyr::symbols::env_mask_bindings));
+    SEXP chops = PROTECT(env_get(env_private, dplyr::symbols::chops));
+    SEXP env_mask_bindings = PROTECT(env_get(env_private, dplyr::symbols::env_mask_bindings));
 
-    rlang::env_unbind(env_mask_bindings, sym_name);
-    rlang::env_unbind(chops, sym_name);
+    R_removeVarFromFrame(sym_name, env_mask_bindings);
+    R_removeVarFromFrame(sym_name, chops);
 
     UNPROTECT(5);
   }

src/mutate.cpp
@@ -1,4 +1,5 @@
 #include "dplyr.h"
+#include "utils.h"
 
 namespace dplyr {
 void stop_mutate_recycle_incompatible_size(R_len_t n_result_i, R_xlen_t n_expected_i) {
@@ -24,6 +25,12 @@ void stop_mutate_not_vector(SEXP result) {
 SEXP dplyr_mask_eval_all_mutate(SEXP quo, SEXP env_private) {
   DPLYR_MASK_INIT();
 
+  // Ensure we pass a quosure, which forces `rlang::eval_tidy()` to extract the
+  // environment from the quosure rather than using `env`, so what we pass as
+  // `env` doesn't matter.
+  check_quosure(quo);
+  SEXP env = R_EmptyEnv;
+
   SEXP chunks = PROTECT(Rf_allocVector(VECSXP, ngroups));
   bool seen_vec = false;
   bool seen_null = false;
@@ -33,7 +40,7 @@ SEXP dplyr_mask_eval_all_mutate(SEXP quo, SEXP env_private) {
     DPLYR_MASK_ITERATION_INIT();
     DPLYR_MASK_SET_GROUP(i);
     R_xlen_t n_i = XLENGTH(p_rows[i]);
-    SEXP result_i = PROTECT(DPLYR_MASK_EVAL(quo));
+    SEXP result_i = PROTECT(DPLYR_MASK_EVAL(quo, env));
     SET_VECTOR_ELT(chunks, i, result_i);
 
     if (Rf_isNull(result_i)) {

src/slice.cpp
@@ -1,14 +1,21 @@
 #include "dplyr.h"
+#include "utils.h"
 
 SEXP dplyr_mask_eval_all(SEXP quo, SEXP env_private) {
   DPLYR_MASK_INIT();
+
+  // Ensure we pass a quosure, which forces `rlang::eval_tidy()` to extract the
+  // environment from the quosure rather than using `env`, so what we pass as
+  // `env` doesn't matter.
+  check_quosure(quo);
+  SEXP env = R_EmptyEnv;
 
   SEXP chunks = PROTECT(Rf_allocVector(VECSXP, ngroups));
 
   for (R_xlen_t i = 0; i < ngroups; i++) {
     DPLYR_MASK_ITERATION_INIT();
     DPLYR_MASK_SET_GROUP(i);
-    SET_VECTOR_ELT(chunks, i, DPLYR_MASK_EVAL(quo));
+    SET_VECTOR_ELT(chunks, i, DPLYR_MASK_EVAL(quo, env));
     DPLYR_MASK_ITERATION_FINALISE();
   }
 

src/summarise.cpp
@@ -1,4 +1,5 @@
 #include "dplyr.h"
+#include "utils.h"
 
 namespace dplyr {
 
@@ -45,13 +46,19 @@ void stop_reframe_incompatible_size(
 SEXP dplyr_mask_eval_all_summarise(SEXP quo, SEXP env_private) {
   DPLYR_MASK_INIT();
 
+  // Ensure we pass a quosure, which forces `rlang::eval_tidy()` to extract the
+  // environment from the quosure rather than using `env`, so what we pass as
+  // `env` doesn't matter.
+  check_quosure(quo);
+  SEXP env = R_EmptyEnv;
+
   R_xlen_t n_null = 0;
   SEXP chunks = PROTECT(Rf_allocVector(VECSXP, ngroups));
   for (R_xlen_t i = 0; i < ngroups; i++) {
     DPLYR_MASK_ITERATION_INIT();
     DPLYR_MASK_SET_GROUP(i);
 
-    SEXP result_i = PROTECT(DPLYR_MASK_EVAL(quo));
+    SEXP result_i = PROTECT(DPLYR_MASK_EVAL(quo, env));
     SET_VECTOR_ELT(chunks, i, result_i);
 
     if (result_i == R_NilValue) {

src/utils.h
@@ -1,10 +1,7 @@
 #ifndef DPLYR_UTILS_H
 #define DPLYR_UTILS_H
 
-#define R_NO_REMAP
-#include <R.h>
-#include <Rinternals.h>
-#include <Rversion.h>
+#include "dplyr.h"
 
 // String encoding normalization
 // From https://github.com/r-lib/vctrs/pull/2085
@@ -21,6 +18,37 @@ static inline bool string_is_ascii_or_utf8(SEXP x) {
 
 static inline SEXP string_as_utf8(SEXP x) {
   return Rf_mkCharCE(Rf_translateCharUTF8(x), CE_UTF8);
+}
+
+static inline void check_quosure(SEXP x) {
+  if (!rlang::is_quosure(x)) {
+    Rf_errorcall(R_NilValue, "Internal error: `x` must be a quosure.");
+  }
+}
+
+static inline void check_list_of_quosures(SEXP x) {
+  const SEXP* v_x = VECTOR_PTR_RO(x);
+  const R_xlen_t size = Rf_xlength(x);
+
+  for (R_xlen_t i = 0; i < size; ++i) {
+    if (!rlang::is_quosure(v_x[i])) {
+      Rf_errorcall(R_NilValue, "Internal error: `x[[%i]]` must be a quosure.", (int) i + 1);
+    }
+  }
+}
+
+static inline
+void env_bind_delayed(SEXP env, SEXP sym, SEXP expr, SEXP eval_env) {
+#if (R_VERSION >= R_Version(4, 6, 0))
+  R_MakeDelayedBinding(sym, expr, eval_env, env);
+#else
+  SEXP promise = PROTECT(Rf_allocSExp(PROMSXP));
+  SET_PRCODE(promise, expr);
+  SET_PRENV(promise, eval_env);
+  SET_PRVALUE(promise, R_UnboundValue);
+  Rf_defineVar(sym, promise, env);
+  UNPROTECT(1);
+#endif
 }
 
 #endif